cbd49fd9b0e4765d4f7bdce206a7f3c4f800cfc6
Petar Belchev <petar9004@gmail.com>
2022-04-19 21:34:27 +0300

    Add a Yard Greening task

---
+++ Programing Basics/First Steps In Coding - Lab/09. Yard Greening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareМeters = double.Parse(Console.ReadLine());
+            double price = squareМeters * 7.61;
+            double discountPrice = price * 0.18;
+            double finalPrice = price - discountPrice;
+            
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discountPrice} lv.");
+        }
+    }
+}
