20137f1080b65b9831e6efc4e0b6c091bbc1eceb
Petar Belchev <petar9004@gmail.com>
2022-04-19 21:31:02 +0300

    Add a Projects Creation task

---
+++ Programing Basics/First Steps In Coding - Lab/07. Projects Creation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numberprojects = int.Parse(Console.ReadLine());
+            int needHours = numberprojects * 3;
+            Console.WriteLine($"The architect {name} will need {needHours} hours to complete {numberprojects} project/s.");
+        }
+    }
+}
