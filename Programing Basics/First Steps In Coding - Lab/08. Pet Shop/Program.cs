3527b402c7979b9fe81b1e029021ad0c22ab213b
Petar Belchev <petar9004@gmail.com>
2022-04-19 21:33:03 +0300

    Add a Pet Shop task

---
+++ Programing Basics/First Steps In Coding - Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberDogFood = int.Parse(Console.ReadLine());
+            int numberCatFood = int.Parse(Console.ReadLine());
+            double priceDogFood = numberDogFood * 2.50;
+            int priceCatFood = numberCatFood * 4;
+            double priceAll = priceDogFood + priceCatFood;
+            Console.WriteLine(priceAll + " lv.");
+
+        }
+    }
+}
