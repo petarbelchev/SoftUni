d47ac02a08670d062366eb09de75119d6cf84470
Petar Belchev <petar9004@gmail.com>
2022-04-19 20:32:53 +0300

    Add a Inches To Centimeters task

---
+++ Programing Basics/First Steps In Coding - Lab/04. InchesToCentimeters/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cm = double.Parse(Console.ReadLine());
+            Console.WriteLine(cm * 2.54);
+        }
+    }
+}
