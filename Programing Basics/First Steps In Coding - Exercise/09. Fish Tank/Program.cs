fa27a3efb5bb6ab84cef729993a4e2c8e246292e
Petar Belchev <petar9004@gmail.com>
2022-04-20 08:39:48 +0300

    Add a Fish Tank task

---
+++ Programing Basics/First Steps In Coding - Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09._Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int widgh = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentValue = double.Parse(Console.ReadLine());
+
+            double percent = percentValue / 100.0;
+            double capacityTankLitres = (lenght * widgh * height) / 1000.0;
+            double capacityTankForWater = capacityTankLitres - (capacityTankLitres * percent);
+
+            Console.WriteLine(capacityTankForWater);
+        }
+    }
+}
