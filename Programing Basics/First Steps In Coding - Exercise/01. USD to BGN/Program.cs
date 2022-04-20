3a379900c14e8e4305e8b6d534a1d334a4ed1c0e
Petar Belchev <petar9004@gmail.com>
2022-04-20 08:32:28 +0300

    Add a USD to BGN task

---
+++ Programing Basics/First Steps In Coding - Exercise/01. USD to BGN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usdCount = double.Parse(Console.ReadLine());
+            double bgnCount = usdCount * 1.79549;
+            Console.WriteLine(bgnCount);
+        }
+    }
+}
