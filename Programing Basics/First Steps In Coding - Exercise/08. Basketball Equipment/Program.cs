f7fbf40d954717c29065d44f781ef9be52f344c5
Petar Belchev <petar9004@gmail.com>
2022-04-20 08:39:20 +0300

    Add a Basketball Equipment task

---
+++ Programing Basics/First Steps In Coding - Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int feePerYear = int.Parse(Console.ReadLine());
+
+            double sneakersPrice = feePerYear - (feePerYear * 0.4);
+            double outfitPrice = sneakersPrice - (sneakersPrice * 0.2);
+            double ballPrice = outfitPrice / 4;
+            double accessories = ballPrice / 5;
+            double priceAllPerYear = feePerYear + sneakersPrice + outfitPrice + ballPrice + accessories;
+
+            Console.WriteLine(priceAllPerYear);
+        }
+    }
+}
