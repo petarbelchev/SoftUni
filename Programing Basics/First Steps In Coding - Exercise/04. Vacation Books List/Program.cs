c8808d2dac0e7906ee1428c6f24300b520eda0c4
Petar Belchev <petar9004@gmail.com>
2022-04-20 08:37:00 +0300

    Add a Vacantion Books List task

---
+++ Programing Basics/First Steps In Coding - Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pagesCount = int.Parse(Console.ReadLine());
+            int readedPagesPerHour = int.Parse(Console.ReadLine());
+            int daysCount = int.Parse(Console.ReadLine());
+            int neededHoursPerDay = (pagesCount / readedPagesPerHour) / daysCount;
+            Console.WriteLine(neededHoursPerDay);
+        }
+    }
+}
