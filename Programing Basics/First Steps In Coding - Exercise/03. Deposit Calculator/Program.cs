fb0bb3ef84808649c9103a850bc3c24dd2acf355
Petar Belchev <petar9004@gmail.com>
2022-04-20 08:34:30 +0300

    Add a Deposit Calculator task

---
+++ Programing Basics/First Steps In Coding - Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositValue = double.Parse(Console.ReadLine());
+            int termOfTheDeposit = int.Parse(Console.ReadLine());
+            double annualInterestRate = double.Parse(Console.ReadLine());
+
+            double totalAmountDepositPeriod = depositValue + termOfTheDeposit * ((depositValue * annualInterestRate / 100) / 12);
+
+            Console.WriteLine(totalAmountDepositPeriod);
+        }
+    }
+}
