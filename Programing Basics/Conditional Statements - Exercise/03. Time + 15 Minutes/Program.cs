362d02b4e0bae69292d14d6454e061fc082364f8
Petar Belchev <petar9004@gmail.com>
2022-04-22 18:48:34 +0300

    Add a Time + 15 Minutes task

---
+++ Programing Basics/Conditional Statements - Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _03._Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int inMinutes = (hours * 60) + minutes;
+
+            int inMinutesAfter = inMinutes + 15;
+            int hoursAfter = inMinutesAfter / 60;
+            int minutesAfter = inMinutesAfter % 60;
+
+            if (hoursAfter > 23)
+            {
+                hoursAfter = 0;
+            }
+
+            if (minutesAfter < 10)
+            {
+                Console.WriteLine($"{hoursAfter}:0{minutesAfter}");
+            }
+            else
+            {
+                Console.WriteLine($"{hoursAfter}:{minutesAfter}");
+            }
+        }
+    }
+}
