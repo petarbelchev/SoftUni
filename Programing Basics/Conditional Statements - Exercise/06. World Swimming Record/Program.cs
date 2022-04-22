4f23e4042dfe25341533a5b336116d1678812e5c
Petar Belchev <petar9004@gmail.com>
2022-04-22 18:50:55 +0300

    Add a World Swimming Record task

---
+++ Programing Basics/Conditional Statements - Exercise/06. World Swimming Record/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06._World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double resistanceDelay = (distanceInMeters / 15);
+
+            double roundedResistanceDelay = Math.Floor(resistanceDelay);
+
+            double delayInSeconds = roundedResistanceDelay * 12.5;
+
+            double ivanRecord = distanceInMeters * timeForOneMeter + delayInSeconds;
+
+            if (ivanRecord < recordInSeconds)
+            {
+                Console.WriteLine($" Yes, he succeeded! The new world record is {ivanRecord:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {ivanRecord - recordInSeconds:f2} seconds slower.");
+            }
+
+        }
+    }
+}
