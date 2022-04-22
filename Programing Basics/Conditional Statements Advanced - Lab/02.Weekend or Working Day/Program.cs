3b633f3dd3ef0b31a2104ba9de66c8edb22dfd56
Petar Belchev <petar9004@gmail.com>
2022-04-23 00:30:20 +0300

    Add a Weekend or Working Day task

---
+++ Programing Basics/Conditional Statements Advanced - Lab/02.Weekend or Working Day/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02.Weekend_or_Working_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+
+            bool itsWorkingDay = dayOfWeek == "Monday" || dayOfWeek == "Tuesday" ||
+                dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday";
+
+            bool itsWeekend = dayOfWeek == "Saturday" || dayOfWeek == "Sunday";
+
+            if (itsWorkingDay == true)
+            {
+                Console.WriteLine("Working day");
+            }
+            else if (itsWeekend == true)
+            {
+                Console.WriteLine("Weekend");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
