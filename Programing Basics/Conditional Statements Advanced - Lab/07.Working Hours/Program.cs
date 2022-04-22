a76e708f71b22bdd6dbc1af408901fa433218727
Petar Belchev <petar9004@gmail.com>
2022-04-23 00:33:26 +0300

    Add a Working Hours task

---
+++ Programing Basics/Conditional Statements Advanced - Lab/07.Working Hours/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            bool inWorkingDay = day != "Sunday";
+            bool inWorkingTime = hour >= 10 && hour <= 18;
+
+            if (inWorkingDay == true && inWorkingTime == true)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
