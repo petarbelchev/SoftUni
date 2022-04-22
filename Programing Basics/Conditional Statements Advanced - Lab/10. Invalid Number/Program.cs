52fe64fc3ddd8d4ce83b45f29d7089509fdb67c8
Petar Belchev <petar9004@gmail.com>
2022-04-23 00:35:13 +0300

    Add a Invalid Number task

---
+++ Programing Basics/Conditional Statements Advanced - Lab/10. Invalid Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10._Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            bool numIsValid = number >= 100 && number <= 200 || number == 0;
+
+            if (numIsValid == false)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
