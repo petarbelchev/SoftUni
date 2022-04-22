8b59c1734b949d7175498311da8a9a248f360940
Petar Belchev <petar9004@gmail.com>
2022-04-23 00:30:53 +0300

    Add a Animal Type task

---
+++ Programing Basics/Conditional Statements Advanced - Lab/03. Animal Type/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Animal_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animalName = Console.ReadLine();
+            switch (animalName)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
