270ba8df3df4be7c8f0a695a6966ffbd65744482
Petar Belchev <petar9004@gmail.com>
2022-04-25 12:55:19 +0300

    Add a Operations Between Numbers task

---
+++ Programing Basics/Conditional Statements Advanced - Exercise/06. Operations Between Numbers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _06._Operations_Between_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = int.Parse(Console.ReadLine());
+            double num2 = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    double result = num1 + num2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{num1} + {num2} = {result} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num1} + {num2} = {result} - odd");
+                    }
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{num1} - {num2} = {result} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num1} - {num2} = {result} - odd");
+                    }
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{num1} * {num2} = {result} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num1} * {num2} = {result} - odd");
+                    }
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {num1} by zero");
+                    }
+                    else
+                    {
+                    result = num1 / num2;
+                    Console.WriteLine($"{num1} / {num2} = {result:f2}");
+                    }
+                    break;
+                case '%':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {num1} by zero");
+                    }
+                    else
+                    {
+                        result = num1 % num2;
+                        Console.WriteLine($"{num1} % {num2} = {result}");
+                    }
+                    break;
+            }
+        }
+    }
+}
