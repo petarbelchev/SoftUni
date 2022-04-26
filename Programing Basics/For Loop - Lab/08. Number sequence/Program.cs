356a098669e6b3426ab2bda5df9ddb8c24d00e69
Petar Belchev <petar9004@gmail.com>
2022-04-26 08:55:55 +0300

    Add a Number Sequence task

---
+++ Programing Basics/For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > maxNumber)
+                {
+                    maxNumber = num;
+                }
+                if (num < minNumber)
+                {
+                    minNumber = num;
+                }
+
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
