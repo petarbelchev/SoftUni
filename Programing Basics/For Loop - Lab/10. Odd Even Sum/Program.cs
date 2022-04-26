efa4218fce7643e63da2c4a3100067b6116bdadc
Petar Belchev <petar9004@gmail.com>
2022-04-26 08:57:02 +0300

    Add a Odd Even Sum task

---
+++ Programing Basics/For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNumbers = int.Parse(Console.ReadLine());
+            int odd = 0;
+            int even = 0;
+
+            for (int i = 1; i <= nNumbers; i += 1)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                if (i % 2 != 0)
+                {
+                    odd += numbers;
+                }
+                else
+                {
+                    even += numbers;
+                }
+            }
+
+            if (odd == even)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {odd}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(odd - even)}");
+            }
+        }
+    }
+}
