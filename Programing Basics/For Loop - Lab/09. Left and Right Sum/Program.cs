eeebba04aef9102f3af756922b7bee89a160b55d
Petar Belchev <petar9004@gmail.com>
2022-04-26 08:56:33 +0300

    Add a Left and Right Sum task

---
+++ Programing Basics/For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNumbers = int.Parse(Console.ReadLine());
+            int leftNumber = 0;
+            int rightNumber = 0;
+
+            for (int i = 1; i <= nNumbers; i += 1)
+            {
+                leftNumber += int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i <= nNumbers; i += 1)
+            {
+                rightNumber += int.Parse(Console.ReadLine());
+            }
+
+            if (leftNumber == rightNumber)
+            {
+                Console.WriteLine($"Yes, sum = {leftNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftNumber - rightNumber)}");
+            }
+        }
+    }
+}
