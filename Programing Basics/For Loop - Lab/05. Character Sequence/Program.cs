4a32a7fdaaa57256a4092517269117f4b35ce366
Petar Belchev <petar9004@gmail.com>
2022-04-26 08:53:57 +0300

    Add a Character Sequence task

---
+++ Programing Basics/For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05._Character_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int lengthText = text.Length;
+            for (int i = 0; i < lengthText; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+    }
+}
