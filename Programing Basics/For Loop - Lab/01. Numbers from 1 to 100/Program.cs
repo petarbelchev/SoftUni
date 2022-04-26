1a32b6db4f474a31f9b5c45545d8d73cc65772e9
Petar Belchev <petar9004@gmail.com>
2022-04-26 08:51:01 +0300

    Add a Numbers from 1 to 100 task

---
+++ Programing Basics/For Loop - Lab/01. Numbers from 1 to 100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Numbers_from_1_to_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 100; a++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
