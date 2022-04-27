a99d3da44c922cf787599f396808ae98ef2640cc
Petar Belchev <petar9004@gmail.com>
2022-04-27 09:14:27 +0300

    Add a Half Sum Element task

---
+++ Programing Basics/For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int biggerNumber = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num >= biggerNumber)
+                {
+                    biggerNumber = num;
+                }
+            }
+
+            if (biggerNumber == sum - biggerNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {biggerNumber}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(biggerNumber - (sum - biggerNumber))}");
+            }
+        }
+    }
+}
