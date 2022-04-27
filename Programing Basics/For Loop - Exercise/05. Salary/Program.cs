8e8acc41b59c01a9d9012556199f015e6d9d2c22
Petar Belchev <petar9004@gmail.com>
2022-04-27 09:15:43 +0300

    Add a Salary task

---
+++ Programing Basics/For Loop - Exercise/05. Salary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05._Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int openTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < openTabs && salary > 0; i++)
+            {
+                string websiteName = Console.ReadLine();
+                switch (websiteName)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+                }
+            }
+
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+
+        }
+    }
+}
