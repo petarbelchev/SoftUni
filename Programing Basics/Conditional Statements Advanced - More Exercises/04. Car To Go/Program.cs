435694e91e3d58584e9eeff434b2ec8a95b844dd
Petar Belchev <petar9004@gmail.com>
2022-04-28 13:16:17 +0300

    Add a Car To Go task

---
+++ Programing Basics/Conditional Statements Advanced - More Exercises/04. Car To Go/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _04._Car_To_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string typeCar = null;
+            string classType = null;
+            double carPrice = 0;
+
+            switch (season)
+            {
+                case "Summer":
+
+                    if (budget <= 100)
+                    {
+                        typeCar = "Cabrio";
+                        classType = "Economy class";
+                        carPrice = budget * 0.35;
+                    }
+                    else if (budget > 100 && budget <= 500)
+                    {
+                        typeCar = "Cabrio";
+                        classType = "Compact class";
+                        carPrice = budget * 0.45;
+                    }
+                    else if (budget > 500)
+                    {
+                        typeCar = "Jeep";
+                        classType = "Luxury class";
+                        carPrice = budget * 0.9;
+                    }
+
+                    break;
+
+
+                case "Winter":
+
+                    if (budget <= 100)
+                    {
+                        typeCar = "Jeep";
+                        classType = "Economy class";
+                        carPrice = budget * 0.65;
+                    }
+                    else if (budget > 100 && budget <= 500)
+                    {
+                        typeCar = "Jeep";
+                        classType = "Compact class";
+                        carPrice = budget * 0.8;
+                    }
+                    else if (budget > 500)
+                    {
+                        typeCar = "Jeep";
+                        classType = "Luxury class";
+                        carPrice = budget * 0.9;
+                    }
+
+                    break;
+            }
+
+            Console.WriteLine(classType);
+            Console.WriteLine($"{typeCar} - {carPrice:f2}");
+        }
+    }
+}
