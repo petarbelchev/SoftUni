c42301c62ca07a7b568c7d31c4c7f9979ffeb6a1
Petar Belchev <petar9004@gmail.com>
2022-04-28 13:18:54 +0300

    Add a Multiply by 2 task

---
+++ Programing Basics/Conditional Statements Advanced - More Exercises/10. Multiply by 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10._Multiply_by_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double a = double.Parse(Console.ReadLine());
+
+                if (a >= 0)
+                {
+                    Console.WriteLine($"Result: {a * 2:f2}");
+                }
+                else
+                {
+                    Console.WriteLine("Negative number!");
+                    break;
+                }
+            }
+        }
+    }
+}
