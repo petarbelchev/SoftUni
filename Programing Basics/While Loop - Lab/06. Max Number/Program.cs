efc9ac260a760560b235e3b220d70c995c1270f6
Petar Belchev <petar9004@gmail.com>
2022-04-29 08:35:18 +0300

    Add a Max Number task

---
+++ Programing Basics/While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int biggerNum = int.MinValue;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Stop")
+                {
+                    break;
+                }
+
+                int number = int.Parse(input);
+
+                if (number > biggerNum)
+                {
+                    biggerNum = number;
+                }
+            }
+
+            Console.WriteLine($"{biggerNum}");
+        }
+    }
+}
