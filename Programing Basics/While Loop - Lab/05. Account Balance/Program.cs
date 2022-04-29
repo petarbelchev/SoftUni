6c579ebe034a9ad0f35c9281db5a8297b104ab71
Petar Belchev <petar9004@gmail.com>
2022-04-29 08:34:55 +0300

    Add a Account Balance task

---
+++ Programing Basics/While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "NoMoreMoney")
+                {
+                    break;
+                }
+
+                double deposit = double.Parse(input);
+
+                if (deposit < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                Console.WriteLine($"Increase: {deposit:f2}");
+                balance += deposit;
+            }
+
+            Console.WriteLine($"Total: {balance:f2}");
+        }
+    }
+}
