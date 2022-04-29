0c664606850f0577abab9081a41a55ebe18a38a2
Petar Belchev <petar9004@gmail.com>
2022-04-29 08:33:00 +0300

    Add a Read Text task

---
+++ Programing Basics/While Loop - Lab/01. Read Text/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+
+            while (name != "Stop")
+            {
+                name = Console.ReadLine();
+                if (name != "Stop")
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
