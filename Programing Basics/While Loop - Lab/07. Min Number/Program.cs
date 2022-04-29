47bb6fb26761fa62d4131405e08a624744fbc07d
Petar Belchev <petar9004@gmail.com>
2022-04-29 08:35:46 +0300

    Add a Min Number task

---
+++ Programing Basics/While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallerNum = int.MaxValue;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Stop")
+                {
+                    break;
+                }
+
+                int number = int.Parse(input);
+
+                if (number < smallerNum)
+                {
+                    smallerNum = number;
+                }
+
+            }
+
+            Console.WriteLine($"{smallerNum}");
+        }
+    }
+}
