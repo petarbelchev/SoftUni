e0fa183fd25802d3803f167c4eea2ac588c22d2a
Petar Belchev <petar9004@gmail.com>
2022-04-30 10:32:59 +0300

    Add a Cake task

---
+++ Programing Basics/While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int visochina = int.Parse(Console.ReadLine());
+            int daljina = int.Parse(Console.ReadLine());
+
+            int brParcheta = visochina * daljina;
+            int brVzetiParcheta = 0;
+
+            while (brVzetiParcheta < brParcheta)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "STOP")
+                {
+                    Console.WriteLine($"{brParcheta - brVzetiParcheta} pieces are left.");
+                    break;
+                }
+                else
+                {
+                    brVzetiParcheta += int.Parse(input);
+
+                    if (brParcheta <= brVzetiParcheta)
+                    {
+                        Console.WriteLine($"No more cake left! You need {brVzetiParcheta - brParcheta} pieces more.");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
