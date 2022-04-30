7134302a7a8ed2b33141c1b9cc551981f4d675f8
Petar Belchev <petar9004@gmail.com>
2022-04-30 10:33:41 +0300

    Add a Moving task

---
+++ Programing Basics/While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shirochina = int.Parse(Console.ReadLine());
+            int daljina = int.Parse(Console.ReadLine());
+            int visochina = int.Parse(Console.ReadLine());
+
+            int obemKvartira = shirochina * daljina * visochina;
+            int brKashoni = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Done")
+                {
+                    Console.WriteLine($"{obemKvartira - brKashoni} Cubic meters left.");
+                    break;
+                }
+                else
+                {
+                    brKashoni += int.Parse(input);
+
+                    if (brKashoni >= obemKvartira)
+                    {
+                        Console.WriteLine($"No more free space! You need {brKashoni - obemKvartira} Cubic meters more.");
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
