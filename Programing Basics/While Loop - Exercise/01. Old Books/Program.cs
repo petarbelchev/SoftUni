1c028972f11ada66824fe5c512ed0867f31e9c81
Petar Belchev <petar9004@gmail.com>
2022-04-30 10:29:28 +0300

    Add a Old Books task

---
+++ Programing Basics/While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookForSearch = Console.ReadLine();
+            string nextBook = null;
+            int checkedBooksQuantity = 0;
+
+            while (bookForSearch != nextBook)
+            {
+                nextBook = Console.ReadLine();
+
+                if (nextBook == "No More Books")
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {checkedBooksQuantity} books.");
+                    break;
+                }
+
+                if (nextBook == bookForSearch)
+                {
+                    Console.WriteLine($"You checked {checkedBooksQuantity} books and found it.");
+                    break;
+                }
+
+                checkedBooksQuantity++;
+            }
+        }
+    }
+}
