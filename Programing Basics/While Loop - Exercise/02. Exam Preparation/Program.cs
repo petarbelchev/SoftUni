cf5ad8fd82930e4e7317e40c0450944768fbed4e
Petar Belchev <petar9004@gmail.com>
2022-04-30 10:30:07 +0300

    Add a Exam Preparation task

---
+++ Programing Basics/While Loop - Exercise/02. Exam Preparation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _02._Exam_Preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int unsatLimit = int.Parse(Console.ReadLine());
+            int unsatQuantity = 0;
+            int gradeSum = 0;
+            int taskQuantity = 0;
+            string lastTask = null;
+
+            while (unsatQuantity != unsatLimit)
+            {
+                string taskName = Console.ReadLine();
+
+                if (taskName == "Enough")
+                {
+                    Console.WriteLine($"Average score: {gradeSum / (double)taskQuantity:f2}");
+                    Console.WriteLine($"Number of problems: {taskQuantity}");
+                    Console.WriteLine($"Last problem: {lastTask}");
+                    break;
+                }
+
+                int grade = int.Parse(Console.ReadLine());
+
+                if (grade <= 4)
+                {
+                    unsatQuantity++;
+                }
+
+                if (taskName != "Enough")
+                {
+                    lastTask = taskName;
+                }
+
+                taskQuantity++;
+                gradeSum += grade;
+            }
+
+            if (unsatQuantity >= unsatLimit)
+            {
+                Console.WriteLine($"You need a break, {unsatQuantity} poor grades.");
+            }
+        }
+    }
+}
