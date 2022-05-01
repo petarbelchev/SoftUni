f8710fd8d2f205d5123b88d843e7b194e444d16b
Petar Belchev <petar9004@gmail.com>
2022-05-01 14:36:52 +0300

    Add a Back To The Past task

---
+++ Programing Basics/For-Loop - More Exercises/01. Back To The Past/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Back_To_The_Past
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+
+            for (int i = 1800; i <= year; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money -= 12000;
+                }
+                else
+                {
+                    money -= 12000 + (50 * (i - 1800 + 18));
+                }
+            }
+
+            if (money >= 0)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {money:f2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {Math.Abs(money):f2} dollars to survive.");
+            }
+        }
+    }
+}
