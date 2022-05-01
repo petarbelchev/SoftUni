22fa06d881841cbd212794ea2531b6748479dfeb
Petar Belchev <petar9004@gmail.com>
2022-05-01 14:39:32 +0300

    Add a Bills task

---
+++ Programing Basics/For-Loop - More Exercises/06. Bills/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Bills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthQuantity = int.Parse(Console.ReadLine());
+            double electricityBills = 0;
+            double waterBills = 20 * monthQuantity;
+            double internetBills = 15 * monthQuantity;
+            double otherBills = 0;
+
+            for (int i = 0; i < monthQuantity; i++)
+            {
+                electricityBills += double.Parse(Console.ReadLine());
+                otherBills = (electricityBills + waterBills + internetBills) + ((electricityBills + waterBills + internetBills) * 0.2);
+            }
+
+            Console.WriteLine($"Electricity: {electricityBills:f2} lv");
+            Console.WriteLine($"Water: {waterBills:f2} lv");
+            Console.WriteLine($"Internet: {internetBills:f2} lv");
+            Console.WriteLine($"Other: {otherBills:f2} lv");
+            Console.WriteLine($"Average: {(electricityBills + waterBills + internetBills + otherBills) / monthQuantity:f2} lv");
+        }
+    }
+}
