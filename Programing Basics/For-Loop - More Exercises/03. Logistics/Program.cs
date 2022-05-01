8dbe0e32b9731f66cdd7d8ea0c654d8cd5c2e073
Petar Belchev <petar9004@gmail.com>
2022-05-01 14:38:06 +0300

    Add a Logistics task

---
+++ Programing Basics/For-Loop - More Exercises/03. Logistics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _03._Logistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cargoCount = int.Parse(Console.ReadLine());
+            double tonnageAll = 0;
+            int tonnageMinibus = 0;
+            int tonnageTruck = 0;
+            int tonnageTrain = 0;
+
+            for (int i = 1; i <= cargoCount; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                tonnageAll += input;
+
+                if (input <= 3)
+                {
+                    tonnageMinibus += input;
+                }
+                else if (input >= 4 && input <= 11)
+                {
+                    tonnageTruck += input;
+                }
+                else
+                {
+                    tonnageTrain += input;
+                }
+            }
+
+            double minibusPrice = tonnageMinibus * 200;
+            double truckPrice = tonnageTruck * 175;
+            double trainPrice = tonnageTrain * 120;
+
+            double averPricePerTone = (minibusPrice + truckPrice + trainPrice) / tonnageAll;
+            double percentMinibus = (tonnageMinibus / tonnageAll) * 100;
+            double percentTruck = (tonnageTruck / tonnageAll) * 100;
+            double percentTrain = (tonnageTrain / tonnageAll) * 100;
+
+            Console.WriteLine($"{averPricePerTone:f2}");
+            Console.WriteLine($"{percentMinibus:f2}%");
+            Console.WriteLine($"{percentTruck:f2}%");
+            Console.WriteLine($"{percentTrain:f2}%");
+        }
+    }
+}
