0d2200a4b894d50808c8aceee32eef6899f964dd
Petar Belchev <petar9004@gmail.com>
2022-05-01 14:37:38 +0300

    Add a Hospital task

---
+++ Programing Basics/For-Loop - More Exercises/02. Hospital/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02._Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+            int patientsCounter = 0;
+            int forOtherHospital = 0;
+            int doctorsQuantity = 7;
+            int day3Counter = 1;
+
+            for (int i = 1; i <= period; i++)
+            {
+                int patientsPerDay = int.Parse(Console.ReadLine());
+
+                if (day3Counter == 3 && forOtherHospital > patientsCounter)
+                {
+                    doctorsQuantity++;
+                    day3Counter = 0;
+                }
+                else if (day3Counter == 3)
+                {
+                    day3Counter = 0;
+                }
+
+                day3Counter++;
+
+                if (patientsPerDay <= doctorsQuantity)
+                {
+                    patientsCounter += patientsPerDay;
+                }
+                else
+                {
+                    patientsCounter += doctorsQuantity;
+                    forOtherHospital += patientsPerDay - doctorsQuantity;
+                }
+            }
+
+            Console.WriteLine($"Treated patients: {patientsCounter}.");
+            Console.WriteLine($"Untreated patients: {forOtherHospital}.");
+        }
+    }
+}
