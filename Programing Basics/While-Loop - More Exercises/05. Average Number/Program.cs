e52ef8246653434a68996ce9f091fe9f7de56efe
Petar Belchev <petar9004@gmail.com>
2022-05-02 15:20:39 +0300

    Add a Average Number task

---
+++ Programing Basics/While-Loop - More Exercises/05. Average Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05._Average_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"{(double)sum / n:f2}");
+        }
+    }
+}
