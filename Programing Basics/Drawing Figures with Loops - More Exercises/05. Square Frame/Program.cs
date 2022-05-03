e584adb158a478fc817c54ec8522202ca9ad2af4
Petar Belchev <petar9004@gmail.com>
2022-05-03 08:36:24 +0300

    Add a Square Frame task

---
+++ Programing Basics/Drawing Figures with Loops - More Exercises/05. Square Frame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _05._Square_Frame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string plus = "+";
+            string minus = "-";
+            string line = "|";
+
+            Console.Write(plus + " ");
+
+            for (int i = 1; i <= n-2; i++)
+            {
+                Console.Write(minus + " ");
+            }
+
+            Console.WriteLine(plus + " ");
+
+            for (int i = 1; i <= n-2; i++)
+            {
+                Console.Write(line + " ");
+
+                for (int k = 1; k <= n-2; k++)
+                {
+                    Console.Write(minus + " ");
+                }
+
+                Console.WriteLine(line + " ");
+            }
+
+            Console.Write(plus + " ");
+
+            for (int i = 1; i <= n - 2; i++)
+            {
+                Console.Write(minus + " ");
+            }
+
+            Console.WriteLine(plus + " ");
+        }
+    }
+}
