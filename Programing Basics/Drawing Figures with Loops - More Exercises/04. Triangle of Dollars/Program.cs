e796020a79b80ed4f9db754d6ec7d5de4c5ded20
Petar Belchev <petar9004@gmail.com>
2022-05-03 08:35:47 +0300

    Add a Triangle of Dollars task

---
+++ Programing Basics/Drawing Figures with Loops - More Exercises/04. Triangle of Dollars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Triangle_of_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char symbol = '$';
+            string forPrint = null;
+
+            for (int i = 1; i <= n; i++)
+            {
+                forPrint += symbol + " ";
+                Console.WriteLine(forPrint);
+            }
+        }
+    }
+}
