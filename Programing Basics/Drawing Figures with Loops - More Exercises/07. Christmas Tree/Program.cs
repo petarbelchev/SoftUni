25693639b78100a19faafa2c8bea98b87ece5a47
Petar Belchev <petar9004@gmail.com>
2022-05-03 08:37:20 +0300

    Add a Christmas Tree task

---
+++ Programing Basics/Drawing Figures with Loops - More Exercises/07. Christmas Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07._Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row <= n; row++)
+            {
+                for (int spacers = row; spacers < n; spacers++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int stars = row; stars > 0; stars--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write(" | ");
+
+                for (int stars = row; stars > 0; stars--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
