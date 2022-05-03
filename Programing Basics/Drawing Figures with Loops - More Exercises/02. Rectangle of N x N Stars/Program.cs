1adf534060b666c6823b0c01414fefaaaac0543f
Petar Belchev <petar9004@gmail.com>
2022-05-03 08:34:53 +0300

    Add a Rectangle of N x N Stars task

---
+++ Programing Basics/Drawing Figures with Loops - More Exercises/02. Rectangle of N x N Stars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Rectangle_of_N_x_N_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char symbol = '*';
+            string forPrint = null;
+
+            for (int i = 1; i <= n; i++)
+            {
+                forPrint += symbol;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(forPrint);
+            }
+        }
+    }
+}
