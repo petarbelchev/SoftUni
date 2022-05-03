3ba9d8d93a29cf5cd23b74f4c4d485a3df01a494
Petar Belchev <petar9004@gmail.com>
2022-05-03 08:35:18 +0300

    Add a Square of Stars task

---
+++ Programing Basics/Drawing Figures with Loops - More Exercises/03. Square of Stars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Square_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char symbol = '*';
+            string forPrint = null;
+
+            for (int i = 1; i <= n; i++)
+            {
+                forPrint += symbol + " ";
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(forPrint);
+            }
+        }
+    }
+}
