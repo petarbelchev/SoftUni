c5b11d035b2363f5625f531dacf53fda08b5e410
Petar Belchev <petar9004@gmail.com>
2022-05-04 08:22:02 +0300

    Add a Training Lab task

---
+++ Programing Basics/First Steps in Coding - More Exercises/05. Training Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05._Training_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine())*100;
+            double h = double.Parse(Console.ReadLine())*100;
+
+            int placesByH = ((int)h - 100)/70;
+            int placesByW = (int)w/120;
+
+            int totalPlaces = (placesByH * placesByW) - 3;
+            Console.WriteLine(totalPlaces);
+        }
+    }
+}
