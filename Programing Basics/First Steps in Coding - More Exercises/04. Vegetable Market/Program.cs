2e4cd5b745a143d05a27f13564538b6eab2d133b
Petar Belchev <petar9004@gmail.com>
2022-05-04 08:21:27 +0300

    Add a Vegetable Market task

---
+++ Programing Basics/First Steps in Coding - More Exercises/04. Vegetable Market/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Vegetable_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetablesPerKilo = double.Parse(Console.ReadLine());
+            double fruitsPerKilo = double.Parse(Console.ReadLine());
+            int vegetablesKilograms = int.Parse(Console.ReadLine());
+            int fruitsKilograms = int.Parse(Console.ReadLine());
+
+            double income = ((vegetablesPerKilo * vegetablesKilograms) + (fruitsPerKilo * fruitsKilograms)) / 1.94;
+            Console.WriteLine($"{income:f2}");
+        }
+    }
+}
