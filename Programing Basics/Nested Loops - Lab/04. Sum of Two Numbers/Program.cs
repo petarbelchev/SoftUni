dd646f91cb9ee7829ade3aec5ce6807137d2a75c
Petar Belchev <petar9004@gmail.com>
2022-05-05 08:13:10 +0300

    Add a Sum of Two Numbers task

---
+++ Programing Basics/Nested Loops - Lab/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int finishNum = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool check = false;
+
+            for (int firstNum = startNum; firstNum <= finishNum; firstNum++)
+            {
+                for (int secondNum = startNum; secondNum <= finishNum; secondNum++)
+                {
+                    counter++;
+
+                    if (firstNum+secondNum == magicNum && check == false)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({firstNum} + {secondNum} = {magicNum})");
+                        check = true;
+                    }
+                }
+            }
+
+            if (check == false)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNum}");
+            }
+        }
+    }
+}
