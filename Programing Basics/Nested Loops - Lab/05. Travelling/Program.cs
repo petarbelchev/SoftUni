cdb2517ec19ff6eae15c6b420da2e1abba013d50
Petar Belchev <petar9004@gmail.com>
2022-05-05 08:14:19 +0300

    Add a Traveling task

---
+++ Programing Basics/Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+
+                if (destination == "End")
+                {
+                    break;
+                }
+
+                double budget = double.Parse(Console.ReadLine());
+
+                while (budget > 0)
+                {
+                    budget -= double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
