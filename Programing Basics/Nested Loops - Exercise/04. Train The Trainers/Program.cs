b03bdb47891497c7c3f36cbda9c3baa3d8af6a92
Petar Belchev <petar9004@gmail.com>
2022-05-06 08:57:24 +0300

    Add a Train The Trainers task

---
+++ Programing Basics/Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryNumber = int.Parse(Console.ReadLine()); // 1 ... 20
+            string presentationName = null;
+            double averGradeSum = 0;
+            int presentationsCounter = 0;
+
+            while (true)
+            {
+                presentationName = Console.ReadLine();
+
+                if (presentationName == "Finish")
+                {
+                    break;
+                }
+
+                double grades = 0;
+
+                for (int i = 1; i <= juryNumber; i++) // chetat se ocenkite na jurito
+                {
+                    grades += double.Parse(Console.ReadLine()); // ot 2.00 do 6.00
+                }
+
+                double averGrade = grades / juryNumber;
+                averGradeSum += averGrade;
+                presentationsCounter++;
+                Console.WriteLine($"{presentationName} - {averGrade:f2}.");
+            }
+
+            Console.WriteLine($"Student's final assessment is {averGradeSum / presentationsCounter:f2}.");
+        }
+    }
+}
