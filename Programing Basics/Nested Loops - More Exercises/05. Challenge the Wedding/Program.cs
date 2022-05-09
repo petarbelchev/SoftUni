cc86c01ba5bf1dcb18f314715b121f45ded73fea
Petar Belchev <petar9004@gmail.com>
2022-05-09 08:36:21 +0300

    Add a Challenge the Wedding task

---
+++ Programing Basics/Nested Loops - More Exercises/05. Challenge the Wedding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Challenge_the_Wedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mans = int.Parse(Console.ReadLine());
+            int womans = int.Parse(Console.ReadLine());
+            int tables = int.Parse(Console.ReadLine());
+            bool isTablesOver = false;
+
+            for (int currentMan = 1; currentMan <= mans; currentMan++)
+            {
+                for (int currentWoman = 1; currentWoman <= womans; currentWoman++)
+                {
+                    Console.Write($"({currentMan} <-> {currentWoman}) ");
+                    tables--;
+
+                    if (tables == 0)
+                    {
+                        isTablesOver = true;
+                        break;
+                    }
+                }
+
+                if (isTablesOver)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
