51f935ecaefe184ecc426288be19d98d0a3d437b
Petar Belchev <petar9004@gmail.com>
2022-05-10 08:42:28 +0300

    Add a Secret Door's Lock task

---
+++ Programing Basics/Nested Loops - More Exercises/08. Secret Door's Lock/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _08._Secret_Door_s_Lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fMax = int.Parse(Console.ReadLine());
+            int sMax = int.Parse(Console.ReadLine());
+            int tMax = int.Parse(Console.ReadLine());
+
+            for (int first = 1; first <= fMax; first++)
+            {
+                if (first % 2 == 0)
+                {
+                    for (int second = 1; second <= sMax; second++)
+                    {
+                        bool isPrime = true;
+
+                        for (int divider = 2; divider < second; divider++)
+                        {
+                            if (second % divider == 0)
+                            {
+                                isPrime = false;
+                            }
+                        }
+
+                        if (isPrime && second >= 2 && second <= 7)
+                        {
+                            for (int third = 1; third <= tMax; third++)
+                            {
+                                if (third % 2 == 0)
+                                {
+                                    Console.WriteLine($"{first} {second} {third}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
