e83c36cf812e7d6dfc67712ba9a79142ef12893c
Petar Belchev <petar9004@gmail.com>
2022-05-11 08:34:31 +0300

    Add a Oscars ceremony task

---
+++ Programing Basics/Programming Basics Online Exam - 6 and 7 April 2019/01. Oscars ceremony/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Oscars_ceremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+            double statuettePrice = rent - (rent * 0.3);
+            double catering = statuettePrice - (statuettePrice * 0.15);
+            double sound = catering / 2;
+            double totalPrice = rent + statuettePrice + catering + sound;
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
