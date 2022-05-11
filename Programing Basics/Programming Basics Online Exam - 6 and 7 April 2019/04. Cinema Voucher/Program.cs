1048f19d8a1ce8419ba427dae3bbc33ecd394fd9
Petar Belchev <petar9004@gmail.com>
2022-05-11 08:36:01 +0300

    Add a Cinema Voucher task

---
+++ Programing Basics/Programming Basics Online Exam - 6 and 7 April 2019/04. Cinema Voucher/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _04._Cinema_Voucher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vaucherPrice = int.Parse(Console.ReadLine());
+            int movieTickets = 0;
+            int productsCounter = 0;
+            //int moviePrice = 0;
+            //int productPrice = 0;
+
+            while (vaucherPrice > 0)
+            {
+                string purchase = Console.ReadLine();
+
+                if (purchase == "End")
+                {
+                    break;
+                }
+                else
+                {
+                    if (purchase.Length > 8)
+                    {
+                        vaucherPrice -= purchase[0] + purchase[1];
+
+                        if (vaucherPrice >= 0)
+                        {
+                            movieTickets++;
+                        }
+                    }
+                    else
+                    {
+                        vaucherPrice -= purchase[0];
+
+                        if (vaucherPrice >= 0)
+                        {
+                            productsCounter++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"{movieTickets}");
+            Console.WriteLine($"{productsCounter}");
+        }
+    }
+}
