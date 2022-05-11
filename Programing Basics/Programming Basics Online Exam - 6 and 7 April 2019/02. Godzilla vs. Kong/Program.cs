3fb15a76699490533e92edcec10a5286fe6171ca
Petar Belchev <petar9004@gmail.com>
2022-05-11 08:35:02 +0300

    Add a Godzilla vs. Kong task

---
+++ Programing Basics/Programming Basics Online Exam - 6 and 7 April 2019/02. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+            double clothingPricePerMan = double.Parse(Console.ReadLine());
+
+            double clothingPrice = clothingPricePerMan * peopleCount;
+            double decorPrice = budget * 0.1;
+
+            if (peopleCount > 150)
+            {
+                clothingPrice = clothingPrice - (clothingPrice * 0.1);
+            }
+
+            if ((decorPrice + clothingPrice) > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {(decorPrice + clothingPrice) - budget:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - (decorPrice + clothingPrice):f2} leva left.");
+            }
+        }
+    }
+}
