d6f56176997aa52d96fab9acd56273d76fb455fb
Petar Belchev <petar9004@gmail.com>
2022-05-11 08:36:24 +0300

    Add a Movie Ratings task

---
+++ Programing Basics/Programming Basics Online Exam - 6 and 7 April 2019/05. Movie Ratings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Movie_Ratings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int moviesQuantity = int.Parse(Console.ReadLine());
+            string highestName = null;
+            string lowestName = null;
+            double highestRating = double.MinValue;
+            double lowestRating = double.MaxValue;
+            double ratingCounter = 0;
+
+            for (int i = 1; i <= moviesQuantity; i++)
+            {
+                string inputName = Console.ReadLine();
+                double inputRating = double.Parse(Console.ReadLine());
+
+                if (inputRating > highestRating)
+                {
+                    highestRating = inputRating;
+                    highestName = inputName;
+                    ratingCounter += inputRating;
+                }
+                else if (inputRating < lowestRating)
+                {
+                    lowestRating = inputRating;
+                    lowestName = inputName;
+                    ratingCounter += inputRating;
+                }
+                else
+                {
+                    ratingCounter += inputRating;
+                }
+            }
+
+            Console.WriteLine($"{highestName} is with highest rating: {highestRating:f1}");
+            Console.WriteLine($"{lowestName} is with lowest rating: {lowestRating:f1}");
+            Console.WriteLine($"Average rating: {ratingCounter / moviesQuantity:f1}");
+        }
+    }
+}
