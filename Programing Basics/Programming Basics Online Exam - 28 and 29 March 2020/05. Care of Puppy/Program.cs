873faf7bab28ab87dfa350cb8b712240efa18064
Petar Belchev <petar9004@gmail.com>
2022-05-12 16:00:01 +0300

    Add a Care of Puppy task

---
+++ Programing Basics/Programming Basics Online Exam - 28 and 29 March 2020/05. Care of Puppy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Care_of_Puppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodTotal = int.Parse(Console.ReadLine()) * 1000;
+            int eatedFood = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Adopted")
+                {
+                    break;
+                }
+
+                eatedFood += int.Parse(input);
+            }
+
+            if (eatedFood <= foodTotal)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodTotal - eatedFood} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {eatedFood - foodTotal} grams more.");
+            }
+        }
+    }
+}
