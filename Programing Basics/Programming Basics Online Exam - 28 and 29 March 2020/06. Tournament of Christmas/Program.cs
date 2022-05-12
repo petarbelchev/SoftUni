48da467d874645fce72e29624d82ea88c47bdff7
Petar Belchev <petar9004@gmail.com>
2022-05-12 16:00:33 +0300

    Add a Tournament of Christmas task

---
+++ Programing Basics/Programming Basics Online Exam - 28 and 29 March 2020/06. Tournament of Christmas/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _06._Tournament_of_Christmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int daysWinner = 0;
+            int daysLoser = 0;
+            double totalMoney = 0;
+
+            for (int currentDay = 1; currentDay <= days; currentDay++)
+            {
+                int currentDayWins = 0;
+                int currentDayLoses = 0;
+                int currentDayMoney = 0;
+
+                while (true)
+                {
+                    string sport = Console.ReadLine();
+
+                    if (sport == "Finish")
+                    {
+                        break;
+                    }
+
+                    string winOrLose = Console.ReadLine();
+
+                    if (winOrLose == "win")
+                    {
+                        currentDayMoney += 20;
+                        currentDayWins++;
+                    }
+                    else
+                    {
+                        currentDayLoses++;
+                    }
+
+                }
+
+                if (currentDayWins >= currentDayLoses)
+                {
+                    currentDayMoney += currentDayMoney / 10;
+                    daysWinner++;
+                }
+                else
+                {
+                    daysLoser++;
+                }
+
+                totalMoney += currentDayMoney;
+            }
+
+            if (daysWinner >= daysLoser)
+            {
+                totalMoney += totalMoney * 0.2;
+                Console.WriteLine($"You won the tournament! Total raised money: {totalMoney:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {totalMoney:f2}");
+            }
+        }
+    }
+}
