ac5ab95c8bc2ce68c91acfa9e817fb92ba672a7d
Petar Belchev <petar9004@gmail.com>
2022-05-12 15:56:43 +0300

    Add a Football Tournament task

---
+++ Programing Basics/Programming Basics Online Exam - 6 and 7 July 2019/05. Football Tournament/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05._Football_Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameClub = Console.ReadLine();
+            int playedGames = int.Parse(Console.ReadLine());
+            int wins = 0;
+            int loses = 0;
+            int draws = 0;
+            for (int currentGame = 1; currentGame <= playedGames; currentGame++)
+            {
+                char gameResult = char.Parse(Console.ReadLine());
+                if (gameResult == 'W')
+                {
+                    wins++;
+                }
+                else if (gameResult == 'D')
+                {
+                    draws++;
+                }
+                else if (gameResult == 'L')
+                {
+                    loses++;
+                }
+            }
+            int wonPoinds = wins * 3 + draws;
+            if (playedGames == 0)
+            {
+                Console.WriteLine($"{nameClub} hasn't played any games during this season.");
+            }
+            else
+            {
+                Console.WriteLine($"{nameClub} has won {wonPoinds} points during this season.");
+                Console.WriteLine("Total stats:");
+                Console.WriteLine($"## W: {wins}");
+                Console.WriteLine($"## D: {draws}");
+                Console.WriteLine($"## L: {loses}");
+                Console.WriteLine($"Win rate: {((double)wins / playedGames) * 100:f2}%");
+            }
+        }
+    }
+}
