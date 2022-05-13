f2a2ca551e21f3c287b750ca5bc556f8ffd15ca5
Petar Belchev <petar9004@gmail.com>
2022-05-13 08:32:04 +0300

    Add a Movie Profit task

---
+++ Programing Basics/Programming Basics Online Exam - 15 and 16 June 2019/01. Movie Profit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Movie_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameMovie = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int tickets = int.Parse(Console.ReadLine());
+            double priceTicket = double.Parse(Console.ReadLine());
+            double percent = int.Parse(Console.ReadLine());
+
+            double totalIncome = (days * tickets * priceTicket);
+            double totalProfit = totalIncome - (totalIncome * (percent / 100));
+
+            Console.WriteLine($"The profit from the movie {nameMovie} is {totalProfit:f2} lv.");
+        }
+    }
+}
