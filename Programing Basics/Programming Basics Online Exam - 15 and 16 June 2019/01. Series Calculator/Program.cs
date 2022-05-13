888a0d7140c124268c5056ed5e6a8e523029da88
Petar Belchev <petar9004@gmail.com>
2022-05-13 08:32:41 +0300

    Add a Series Calculator task

---
+++ Programing Basics/Programming Basics Online Exam - 15 and 16 June 2019/01. Series Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Series_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameSerial = Console.ReadLine();
+            int seasons = int.Parse(Console.ReadLine());
+            int episodes = int.Parse(Console.ReadLine());
+            double timeEpisode = double.Parse(Console.ReadLine());
+
+            double ads = timeEpisode * 0.2;
+            double lastEpisode = seasons * 10;
+
+            double totalTime = (seasons * episodes) * (timeEpisode + ads) + lastEpisode;
+
+            Console.WriteLine($"Total time needed to watch the {nameSerial} series is {Math.Floor(totalTime)} minutes.");
+        }
+    }
+}
