a868d35b0e111562aab4b579e869ce51f2250ae1
Petar Belchev <petar9004@gmail.com>
2022-05-13 08:33:05 +0300

    Add a Cinema task

---
+++ Programing Basics/Programming Basics Online Exam - 15 and 16 June 2019/04. Cinema/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _04._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hallCapacity = int.Parse(Console.ReadLine());
+            int income = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int currentIncome = 0;
+
+                if (input == "Movie time!")
+                {
+                    Console.WriteLine($"There are {hallCapacity} seats left in the cinema.");
+                    break;
+                }
+
+                int currentAudience = int.Parse(input);
+
+                if (hallCapacity >= currentAudience)
+                {
+                    hallCapacity -= currentAudience;
+                    currentIncome = currentAudience * 5;
+
+                    if (currentAudience % 3 == 0)
+                    {
+                        currentIncome -= 5;
+                        income += currentIncome;
+                    }
+                    else
+                    {
+                        income += currentIncome;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The cinema is full.");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Cinema income - {income} lv.");
+        }
+    }
+}
