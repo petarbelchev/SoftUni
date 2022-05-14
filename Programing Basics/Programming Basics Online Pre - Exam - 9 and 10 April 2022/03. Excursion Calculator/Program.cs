fa5a3126d642587bc7a1edbad5ae6b2a4bddda7f
Petar Belchev <petar9004@gmail.com>
2022-05-14 09:36:08 +0300

    Add a Excursion Calculator task

---
+++ Programing Basics/Programming Basics Online Pre - Exam - 9 and 10 April 2022/03. Excursion Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _03._Excursion_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleQuantity = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double pricePerPerson = 0;
+
+            if (peopleQuantity <= 5)
+            {
+                if (season == "spring")
+                {
+                    pricePerPerson = 50;
+                }
+                else if (season == "summer")
+                {
+                    pricePerPerson = 48.5;
+                }
+                else if (season == "autumn")
+                {
+                    pricePerPerson = 60;
+                }
+                else if (season == "winter")
+                {
+                    pricePerPerson = 86;
+                }
+            }
+            else
+            {
+                if (season == "spring")
+                {
+                    pricePerPerson = 48;
+                }
+                else if (season == "summer")
+                {
+                    pricePerPerson = 45;
+                }
+                else if (season == "autumn")
+                {
+                    pricePerPerson = 49.5;
+                }
+                else if (season == "winter")
+                {
+                    pricePerPerson = 85;
+                }
+            }
+
+            double totalPrice = peopleQuantity * pricePerPerson;
+
+            if (season == "summer")
+            {
+                totalPrice -= totalPrice * 0.15;
+            }
+            else if (season == "winter")
+            {
+                totalPrice += totalPrice * 0.08;
+            }
+
+            Console.WriteLine($"{totalPrice:f2} leva.");
+        }
+    }
+}
