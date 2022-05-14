24cf6fde5fafd58de91572e96409fa526c8667f5
Petar Belchev <petar9004@gmail.com>
2022-05-14 09:36:37 +0300

    Add a Computer Firm task

---
+++ Programing Basics/Programming Basics Online Pre - Exam - 9 and 10 April 2022/04. Computer Firm/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _04._Computer_Firm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int computersQuantity = int.Parse(Console.ReadLine());
+            double ratingSum = 0;
+            double sales = 0;
+
+            for (int numComputer = 1; numComputer <= computersQuantity; numComputer++)
+            {
+                int salesAndRating = int.Parse(Console.ReadLine());
+
+                int currentRating = salesAndRating % 10;
+                ratingSum += salesAndRating % 10;
+                double possibleSales = salesAndRating / 10;
+
+                if (currentRating == 2)
+                {
+                    sales += 0;
+                }
+                else if (currentRating == 3)
+                {
+                    sales += possibleSales * 0.5;
+                }
+                else if (currentRating == 4)
+                {
+                    sales += possibleSales * 0.7;
+                }
+                else if (currentRating == 5)
+                {
+                    sales += possibleSales * 0.85;
+                }
+                else if (currentRating == 6)
+                {
+                    sales += possibleSales;
+                }
+            }
+
+            Console.WriteLine($"{sales:f2}");
+            Console.WriteLine($"{ratingSum / computersQuantity:f2}");
+        }
+    }
+}
