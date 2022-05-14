f3f218b9784cafe3441b592d14275a37be5218ea
Petar Belchev <petar9004@gmail.com>
2022-05-14 09:35:00 +0300

    Add a Christmas Preparation task

---
+++ Programing Basics/Programming Basics Online Pre - Exam - 9 and 10 April 2022/01. Christmas Preparation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Christmas_Preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paperRollersQuantity = int.Parse(Console.ReadLine());
+            int fabricRollersQuantity = int.Parse(Console.ReadLine());
+            double glueLitersQuantity = double.Parse(Console.ReadLine());
+            int percentDiscount = int.Parse(Console.ReadLine());
+
+            double paperRollersPrice = paperRollersQuantity * 5.8;
+            double fabricRollersPrice = fabricRollersQuantity * 7.2;
+            double gluePrice = glueLitersQuantity * 1.2;
+
+            double totalPrice = paperRollersPrice + fabricRollersPrice + gluePrice;
+            double totalPriceDiscounted = totalPrice - ((totalPrice * percentDiscount) / 100);
+
+            Console.WriteLine($"{totalPriceDiscounted:f3}");
+        }
+    }
+}
