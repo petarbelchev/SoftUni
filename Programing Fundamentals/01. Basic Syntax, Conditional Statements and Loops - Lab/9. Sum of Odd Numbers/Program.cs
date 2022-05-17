7a208496f7335792f192f3cc5e6da090e57f93a4
Petar Belchev <petar9004@gmail.com>
2022-05-17 12:49:03 +0300

    Add a Sum of Odd Numbers task

---
+++ Programing Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/9. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _9._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int oddNumbersCount = int.Parse(Console.ReadLine());
+            int oddNumbersSum = 0;
+            int currOddNumber = 1;
+
+            for (int i = 1; i <= oddNumbersCount; i++)
+            {
+                Console.WriteLine(currOddNumber);
+                oddNumbersSum += currOddNumber;
+                currOddNumber += 2;
+            }
+
+            Console.WriteLine($"Sum: {oddNumbersSum}");
+        }
+    }
+}
