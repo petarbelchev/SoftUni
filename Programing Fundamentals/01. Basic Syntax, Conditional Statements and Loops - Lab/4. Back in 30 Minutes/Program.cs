e8d40eeea62c4f4d74eacf21c46e552dd16d6b85
Petar Belchev <petar9004@gmail.com>
2022-05-17 12:46:13 +0300

    Add a Back in 30 Minutes task

---
+++ Programing Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/4. Back in 30 Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4._Back_in_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int arriveInMinutes = hours * 60 + minutes;
+            int comeBackInMinutes = arriveInMinutes + 30;
+
+            int hoursToCome = comeBackInMinutes / 60;
+
+            if (hoursToCome > 23)
+            {
+                hoursToCome = 0;
+            }
+
+            int minutesToCome = comeBackInMinutes % 60;
+
+            Console.WriteLine($"{hoursToCome}:{minutesToCome:d2}");
+        }
+    }
+}
