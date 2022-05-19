f610178113366e49ecf94140e152435a26484d18
Petar Belchev <petar9004@gmail.com>
2022-05-19 17:14:57 +0300

    Add a Print and sum task

---
+++ Programing Fundamentals/01.2 Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startSequence = int.Parse(Console.ReadLine());
+            int endSequence = int.Parse(Console.ReadLine());
+            int sumSequenceOfNumbers = 0;
+
+            for (int i = startSequence; i <= endSequence; i++)
+            {
+                Console.Write(i + " ");
+                sumSequenceOfNumbers += i;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sumSequenceOfNumbers}");
+        }
+    }
+}
