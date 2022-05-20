489b860a00bae4c57981f7c769d851c0defeef2e
Petar Belchev <petar9004@gmail.com>
2022-05-20 09:48:28 +0300

    Add a Padawan Equipment task

---
+++ Programing Fundamentals/01.2 Basic Syntax, Conditional Statements and Loops - Exercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountOfMoney = double.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            double priceOfLightsabers = double.Parse(Console.ReadLine());
+            double priceOfRobes = double.Parse(Console.ReadLine());
+            double priceOfBelts = double.Parse(Console.ReadLine());
+
+            double countOfLightsabers = Math.Ceiling(countOfStudents + (countOfStudents * 0.1));
+            double countOfBelts = countOfStudents;
+
+            if (countOfBelts > 5)
+            {
+                countOfBelts -= countOfStudents / 6;
+            }
+
+            double countOfRobes = countOfStudents;
+
+            double totalPriceForLightsabers = priceOfLightsabers * countOfLightsabers;
+            double totalPriceForRobes = priceOfRobes * countOfRobes;
+            double totalPriceForBelts = priceOfBelts * countOfBelts;
+
+            double totalPriceAll = totalPriceForLightsabers + totalPriceForBelts + totalPriceForRobes;
+
+            bool isMoneyEnough = amountOfMoney >= totalPriceAll;
+
+            if (isMoneyEnough)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPriceAll:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($" John will need {totalPriceAll - amountOfMoney:f2}lv more.");
+            }
+        }
+    }
+}
