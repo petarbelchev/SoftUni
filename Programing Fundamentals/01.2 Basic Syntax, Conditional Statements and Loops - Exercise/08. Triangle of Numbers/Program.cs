3815e4da9c849201f264ab875e079eb52231f021
Petar Belchev <petar9004@gmail.com>
2022-05-20 09:47:41 +0300

    Add a Triangle of Numbers task

---
+++ Programing Fundamentals/01.2 Basic Syntax, Conditional Statements and Loops - Exercise/08. Triangle of Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08._Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int colomn = n - row; colomn < n; colomn++)
+                {
+                    Console.Write(row + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
