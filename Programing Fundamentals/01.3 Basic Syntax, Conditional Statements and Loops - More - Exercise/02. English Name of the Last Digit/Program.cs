826d526ed5b326e361600bd797e5d4260ae394b7
Petar Belchev <petar9004@gmail.com>
2022-05-21 15:01:23 +0300

    Add a English Name of the Last Digit task

---
+++ Programing Fundamentals/01.3 Basic Syntax, Conditional Statements and Loops - More - Exercise/02. English Name of the Last Digit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._English_Name_of_the_Last_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int lastDigit = number % 10;
+
+            string spelling = lastDigit == 0 ? "zero" : lastDigit == 1 ? "one" : lastDigit == 2 ? "two" : lastDigit == 3 ? "three" : lastDigit == 4 ? "four" : lastDigit == 5 ? "five" : lastDigit == 6 ? "six" : lastDigit == 7 ? "seven" : lastDigit == 8 ? "eight" : "nine";
+
+            Console.WriteLine(spelling);
+        }
+    }
+}
