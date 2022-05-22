2060e8a526bcfd2490df0f3ea376f0ac2d4dacfb
Petar Belchev <petar9004@gmail.com>
2022-05-22 12:47:49 +0300

    Added a Messages task

---
+++ Programing Fundamentals/01.3 Basic Syntax, Conditional Statements and Loops - More - Exercise/05. Messages/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = string.Empty;
+            int numCount = int.Parse(Console.ReadLine());
+
+            for (int currNum = 1; currNum <= numCount; currNum++)
+            {
+                string inputNum = Console.ReadLine();
+
+                if (inputNum != "0")
+                {
+                    int digitCount = inputNum.Length;
+
+                    int mainDigit = int.Parse(inputNum[0].ToString());
+
+                    int offsetOfNum = (mainDigit - 2) * 3;
+
+                    if (mainDigit == 8 || mainDigit == 9)
+                    {
+                        offsetOfNum += 1;
+                    }
+
+                    int letterIndex = 97 + (offsetOfNum + digitCount - 1);
+
+                    char letter = (char)letterIndex;
+
+                    text += letter;
+                }
+                else
+                {
+                    text += " ";
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
