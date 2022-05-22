e5fce0b9fc22fbf6f9f096f5518e87d81e389490
Petar Belchev <petar9004@gmail.com>
2022-05-22 12:47:03 +0300

    Added a Reverse String task

---
+++ Programing Fundamentals/01.3 Basic Syntax, Conditional Statements and Loops - More - Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputedWord = Console.ReadLine();
+            string reversedWord = string.Empty;
+
+            for (int i = inputedWord.Length - 1; i >= 0; i--)
+            {
+                reversedWord += inputedWord[i];
+            }
+
+            Console.WriteLine(reversedWord);
+        }
+    }
+}
