d594dfe1214579f428bbb26491427408b2e510ed
Petar Belchev <petar9004@gmail.com>
2022-05-23 12:56:43 +0300

    Added a Centuries to Minutes task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04._Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal centuries = decimal.Parse(Console.ReadLine());
+            decimal inYears = centuries * 100;
+            decimal inDays = Math.Floor(inYears * 365.2422M);
+            decimal inHours = inDays * 24;
+            decimal inMinutes = inHours * 60;
+            Console.WriteLine($"{centuries} centuries = {inYears} years = {inDays} days = {inHours} hours = {inMinutes} minutes");
+        }
+    }
+}
