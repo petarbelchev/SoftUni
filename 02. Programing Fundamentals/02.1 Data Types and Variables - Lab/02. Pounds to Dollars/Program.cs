df8553d296c2e616be82b97f8435e334028e5e26
Petar Belchev <petar9004@gmail.com>
2022-05-23 12:55:08 +0300

    Added a Pounds to Dollars task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal britishPounds = decimal.Parse(Console.ReadLine());
+            decimal usDollars = britishPounds * 1.31m;
+            Console.WriteLine($"{usDollars:f3}");
+        }
+    }
+}
