c0a129e09419bdd1b2be8f276f454dae633f19ea
Petar Belchev <petar9004@gmail.com>
2022-05-23 12:51:39 +0300

    Added a Convert Meters to Kilometers task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal distanceInMeters = int.Parse(Console.ReadLine());
+            decimal distanceInKilometers = distanceInMeters / 1000;
+            Console.WriteLine($"{distanceInKilometers:f2}");
+        }
+    }
+}
