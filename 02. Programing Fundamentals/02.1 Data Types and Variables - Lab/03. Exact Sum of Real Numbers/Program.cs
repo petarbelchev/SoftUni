31f11d2e2b6a6fc0ccf856cdb1d99f3de722786d
Petar Belchev <petar9004@gmail.com>
2022-05-23 12:56:03 +0300

    Added a Exact Sum of Real Numbers task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCount = int.Parse(Console.ReadLine());
+            decimal sumNumbers = 0;
+
+            for (int currNum = 1; currNum <= numCount; currNum++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                sumNumbers += num;
+            }
+
+            Console.WriteLine(sumNumbers);
+        }
+    }
+}
