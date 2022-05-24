952af9bc4756169c3cf9d4247def39c7a27a0814
Petar Belchev <petar9004@gmail.com>
2022-05-24 10:32:27 +0300

    Added a Refactor Volume of Pyramid task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal length, width, height = 0;
+            Console.Write("Length: ");
+            length = decimal.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            width = decimal.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            height = decimal.Parse(Console.ReadLine());
+            decimal volume = (length * width * height) / 3;
+            Console.Write($"Pyramid Volume: {volume:f2}");
+
+        }
+    }
+}
