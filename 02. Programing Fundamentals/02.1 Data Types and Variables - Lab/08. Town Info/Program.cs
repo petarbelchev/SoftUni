c68203772cba4ca7f4fec381f71b2c9915751feb
Petar Belchev <petar9004@gmail.com>
2022-05-24 10:30:25 +0300

    Added a Town Info task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/08. Town Info/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08._Town_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            string population = Console.ReadLine();
+            string area = Console.ReadLine();
+
+            Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
+        }
+    }
+}
