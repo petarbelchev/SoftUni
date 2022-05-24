72bbf30bbd821007f491ca07033e87ddb73486d2
Petar Belchev <petar9004@gmail.com>
2022-05-24 19:08:23 +0300

    Update the solution with better and short one

--- 02. Programing Fundamentals/01.3 Basic Syntax, Conditional Statements and Loops - More - Exercise/03. Gaming Store/Program.cs
+++ 02. Programing Fundamentals/01.3 Basic Syntax, Conditional Statements and Loops - More - Exercise/03. Gaming Store/Program.cs
@@ -8,83 +8,43 @@ namespace _03._Gaming_Store
         {
             double currBalance = double.Parse(Console.ReadLine());
             double totalSpent = 0;
+            double price = 0;
             string gameToBuy = Console.ReadLine();
-
             while (gameToBuy != "Game Time")
             {
                 switch (gameToBuy)
                 {
                     case "OutFall 4":
                     case "RoverWatch Origins Edition":
-                        if (currBalance >= 39.99)
-                        {
-                            totalSpent += 39.99;
-                            currBalance -= 39.99;
-                            Console.WriteLine($"Bought {gameToBuy}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Too Expensive");
-                        }
+                        price = 39.99;
                         break;
-
                     case "CS: OG":
-                        if (currBalance >= 15.99)
-                        {
-                            totalSpent += 15.99;
-                            currBalance -= 15.99;
-                            Console.WriteLine($"Bought {gameToBuy}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Too Expensive");
-                        }
+                        price = 15.99;
                         break;
-
                     case "Zplinter Zell":
-                        if (currBalance >= 19.99)
-                        {
-                            totalSpent += 19.99;
-                            currBalance -= 19.99;
-                            Console.WriteLine($"Bought {gameToBuy}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Too Expensive");
-                        }
+                        price = 19.99;
                         break;
-
                     case "Honored 2":
-                        if (currBalance >= 59.99)
-                        {
-                            totalSpent += 59.99;
-                            currBalance -= 59.99;
-                            Console.WriteLine($"Bought {gameToBuy}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Too Expensive");
-                        }
+                        price = 59.99;
                         break;
-
                     case "RoverWatch":
-                        if (currBalance >= 29.99)
-                        {
-                            totalSpent += 29.99;
-                            currBalance -= 29.99;
-                            Console.WriteLine($"Bought {gameToBuy}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Too Expensive");
-                        }
+                        price = 29.99;
                         break;
-
                     default:
                         Console.WriteLine("Not Found");
-                        break;
+                        gameToBuy = Console.ReadLine();
+                        continue;
                 }
-
+                if (currBalance >= price)
+                {
+                    totalSpent += price;
+                    currBalance -= price;
+                    Console.WriteLine($"Bought {gameToBuy}");
+                }
+                else
+                {
+                    Console.WriteLine("Too Expensive");
+                }
                 if (currBalance == 0)
                 {
                     Console.WriteLine("Out of money!");
@@ -92,7 +52,6 @@ namespace _03._Gaming_Store
                 }
                 gameToBuy = Console.ReadLine();
             }
-
             Console.WriteLine($"Total spent: ${totalSpent:f2}. Remaining: ${currBalance:f2}");
         }
     }
