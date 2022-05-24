ef138929a45f5552ca47256d066a02c808ec9fbe
Petar Belchev <petar9004@gmail.com>
2022-05-24 10:31:06 +0300

    Added a Chars to String task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/09. Chars to String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string char1 = Console.ReadLine();
+            string char2 = Console.ReadLine();
+            string char3 = Console.ReadLine();
+
+            string @string = char1 + char2 + char3;
+
+            Console.WriteLine(@string);
+        }
+    }
+}
