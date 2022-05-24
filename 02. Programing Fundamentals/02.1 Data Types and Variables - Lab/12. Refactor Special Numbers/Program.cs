b4268592b4786b79e2624783fa53187f6fd7b314
Petar Belchev <petar9004@gmail.com>
2022-05-24 10:33:04 +0300

    Added a Refactor Special Numbers task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int endOfRange = int.Parse(Console.ReadLine());
+            bool isSpecialNumber = false;
+
+            for (int currNum = 1; currNum <= endOfRange; currNum++)
+            {
+                int sum = 0;
+                int i = currNum;
+
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i /= 10;
+                }
+
+                isSpecialNumber = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine("{0} -> {1}", currNum, isSpecialNumber);
+            }
+        }
+    }
+}
