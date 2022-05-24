84d9320b980ef591b05e00ae48b80f1e847d23ae
Petar Belchev <petar9004@gmail.com>
2022-05-24 10:31:49 +0300

    Added a Lower or Upper task

---
+++ 02. Programing Fundamentals/02.1 Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char @char = char.Parse(Console.ReadLine());
+
+            if (@char >= 65 && @char <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
