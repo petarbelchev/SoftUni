f2dad716f19cf8176a0485f6c2d5bcae7a38de42
Petar Belchev <petar9004@gmail.com>
2022-05-25 10:01:37 +0300

    Added a Snowballs task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBalls = int.Parse(Console.ReadLine());
+            BigInteger highestValue = 0;
+            int snowballSnow = 0;
+            int snowballTime = 0;
+            int snowballQuality = 0;
+
+            for (int i = 1; i <= numberOfBalls; i++)
+            {
+                int currSnowballSnow = int.Parse(Console.ReadLine());
+                int currSnowballTime = int.Parse(Console.ReadLine());
+                int currSnowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger currSnowballValue = BigInteger.Pow(currSnowballSnow / currSnowballTime, currSnowballQuality);
+
+                if (currSnowballValue >= highestValue)
+                {
+                    highestValue = currSnowballValue;
+                    snowballSnow = currSnowballSnow;
+                    snowballQuality = currSnowballQuality;
+                    snowballTime = currSnowballTime;
+                }
+            }
+
+            Console.WriteLine($"{snowballSnow} : {snowballTime} = {highestValue} ({snowballQuality})");
+        }
+    }
+}
