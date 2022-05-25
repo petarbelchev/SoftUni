00b8c1f70018da76df3c780397388a58ad4a9a8d
Petar Belchev <petar9004@gmail.com>
2022-05-25 08:31:53 +0300

    Added a Elevator task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+
+            int neededCourses = peopleCount / elevatorCapacity;
+
+            if (peopleCount % elevatorCapacity != 0)
+            {
+                neededCourses += 1;
+            }
+
+            Console.WriteLine(neededCourses);
+        }
+    }
+}
