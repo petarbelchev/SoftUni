c3e9a47bbe54bd488d1b8f2bebac2bbaf58ff6e5
Petar Belchev <petar9004@gmail.com>
2022-05-25 08:34:52 +0300

    Added a Spice Must Flow task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int sumYield = 0;
+            int daysCount = 0;
+
+            while (startingYield > 99)
+            {
+                int currYield = startingYield;
+                currYield -= 26;
+                sumYield += currYield;
+                daysCount++;
+                startingYield -= 10;
+            }
+
+            if (sumYield >= 26)
+            {
+                sumYield -= 26;
+            }
+            else
+            {
+                sumYield = 0;
+            }
+
+            Console.WriteLine(daysCount);
+            Console.WriteLine(sumYield);
+        }
+    }
+}
