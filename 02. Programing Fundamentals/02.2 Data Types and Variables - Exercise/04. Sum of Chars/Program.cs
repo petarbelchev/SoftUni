80f1615c3a3c0c8723620eb182b0c7cbc5485588
Petar Belchev <petar9004@gmail.com>
2022-05-25 08:32:18 +0300

    Added a Sum of Chars task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int charCount = int.Parse(Console.ReadLine());
+            int sumChars = 0;
+
+            for (int charNum = 1; charNum <= charCount; charNum++)
+            {
+                char currChar = char.Parse(Console.ReadLine());
+                sumChars += currChar;
+            }
+
+            Console.WriteLine($"The sum equals: {sumChars}");
+        }
+    }
+}
