c8a6e2f4d2c60fa003244537fd8633433fe72a60
Petar Belchev <petar9004@gmail.com>
2022-05-25 08:35:17 +0300

    Added a Poke Mon task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokemonPower = int.Parse(Console.ReadLine());
+            int distanceTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int targetsCount = 0;
+            int currPokemonPower = pokemonPower;
+
+            while (currPokemonPower >= distanceTargets)
+            {
+                currPokemonPower -= distanceTargets;
+                targetsCount++;
+
+                if (currPokemonPower == (double)pokemonPower / 2 && exhaustionFactor != 0)
+                {
+                    currPokemonPower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(currPokemonPower);
+            Console.WriteLine(targetsCount);
+        }
+    }
+}
