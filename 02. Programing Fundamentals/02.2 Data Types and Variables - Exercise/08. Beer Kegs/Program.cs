6ef9f8e150c374d02959eabae16b5425d724ebec
Petar Belchev <petar9004@gmail.com>
2022-05-25 08:34:27 +0300

    Added a Beer Kegs task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kegsCount = int.Parse(Console.ReadLine());
+            double volumeBiggestKeg = 0;
+            string modelBiggestKeg = string.Empty;
+
+            for (int currKeg = 1; currKeg <= kegsCount; currKeg++)
+            {
+                string modelOfKeg = Console.ReadLine();
+                double radiusOfKeg = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volumeOfKeg = Math.PI * (radiusOfKeg * radiusOfKeg) * height;
+                if (volumeOfKeg > volumeBiggestKeg)
+                {
+                    volumeBiggestKeg = volumeOfKeg;
+                    modelBiggestKeg = modelOfKeg;
+                }
+            }
+
+            Console.WriteLine(modelBiggestKeg);
+        }
+    }
+}
