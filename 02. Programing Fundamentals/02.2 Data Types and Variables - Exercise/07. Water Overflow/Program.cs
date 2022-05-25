cbbc985cac95ebe956c6577af214b638f55d50f1
Petar Belchev <petar9004@gmail.com>
2022-05-25 08:33:50 +0300

    Added a Water Overflow task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pourCount = int.Parse(Console.ReadLine());
+            int waterTankCapacity = 255;
+
+            for (int i = 1; i <= pourCount; i++)
+            {
+                int currPour = int.Parse(Console.ReadLine());
+
+                if (waterTankCapacity >= currPour)
+                {
+                    waterTankCapacity -= currPour;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(255 - waterTankCapacity);
+        }
+    }
+}
