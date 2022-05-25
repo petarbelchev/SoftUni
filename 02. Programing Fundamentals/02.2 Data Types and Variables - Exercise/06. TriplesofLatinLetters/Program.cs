683d3b46bcaee94496bbe8315ecaab0f6abd9a4f
Petar Belchev <petar9004@gmail.com>
2022-05-25 08:33:24 +0300

    Added a Triple of Latin Letters task

---
+++ 02. Programing Fundamentals/02.2 Data Types and Variables - Exercise/06. TriplesofLatinLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06._TriplesofLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int charsCount = 'a' + int.Parse(Console.ReadLine()) - 1;
+
+            for (char firstPosition = 'a'; firstPosition <= charsCount; firstPosition++)
+            {
+                for (char secondPosition = 'a'; secondPosition <= charsCount; secondPosition++)
+                {
+                    for (char thirdPosition = 'a'; thirdPosition <= charsCount; thirdPosition++)
+                    {
+                        Console.WriteLine($"{firstPosition}{secondPosition}{thirdPosition}");
+                    }
+                }
+            }
+        }
+    }
+}
