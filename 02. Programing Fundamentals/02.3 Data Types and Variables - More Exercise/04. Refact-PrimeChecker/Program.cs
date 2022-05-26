026c799257f47aebb908c844686ca8f176f8b2a1
Petar Belchev <petar9004@gmail.com>
2022-05-26 17:42:05 +0300

    Added a Refactoring: Prime Checker task

---
+++ 02. Programing Fundamentals/02.3 Data Types and Variables - More Exercise/04. Refact-PrimeChecker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04._Refact_PrimeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumber = int.Parse(Console.ReadLine());
+
+            for (int currNumber = 2; currNumber <= lastNumber; currNumber++)
+            {
+                string isPrime = "true";
+
+                for (int divider = 2; divider < currNumber; divider++)
+                {
+                    if (currNumber % divider == 0)
+                    {
+                        isPrime = "false";
+                        break;
+                    }
+                }
+
+                Console.WriteLine("{0} -> {1}", currNumber, isPrime);
+            }
+        }
+    }
+}
