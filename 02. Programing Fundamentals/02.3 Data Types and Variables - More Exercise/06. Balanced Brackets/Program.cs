650192db56dee975c3bc6810fbb9ad39c2f8feb4
Petar Belchev <petar9004@gmail.com>
2022-05-26 17:43:18 +0300

    Added a Balanced Brackets task

---
+++ 02. Programing Fundamentals/02.3 Data Types and Variables - More Exercise/06. Balanced Brackets/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+            int bracketsCounter = 0;
+            bool isBalanced = true;
+
+            for (int numInput = 1; numInput <= inputCount; numInput++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    bracketsCounter++;
+                }
+                else if (input == ")")
+                {
+                    bracketsCounter--;
+                }
+
+                if ((bracketsCounter < 0 || bracketsCounter > 1) && isBalanced == true)
+                {
+                    isBalanced = false;
+                }
+            }
+
+            if (bracketsCounter != 0)
+            {
+                isBalanced = false;
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
