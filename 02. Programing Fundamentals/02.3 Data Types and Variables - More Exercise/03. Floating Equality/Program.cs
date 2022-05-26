0d2735c10636efed19a5ab99d5eacbbe387b60cb
Petar Belchev <petar9004@gmail.com>
2022-05-26 17:41:01 +0300

    Added a Floating Equality task

---
+++ 02. Programing Fundamentals/02.3 Data Types and Variables - More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = Math.Abs(Math.Round(double.Parse(Console.ReadLine()), 6));
+            double numberB = Math.Abs(Math.Round(double.Parse(Console.ReadLine()), 6));
+
+            bool isEqual = numberA - numberB <= 0.000001;
+
+            Console.WriteLine(isEqual);
+        }
+    }
+}
