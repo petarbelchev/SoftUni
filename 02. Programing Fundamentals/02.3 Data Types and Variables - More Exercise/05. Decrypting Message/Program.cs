3d4553c1a8e0d4eea0299c6997214c0159b2b452
Petar Belchev <petar9004@gmail.com>
2022-05-26 17:42:48 +0300

    Added a Decrypting Message task

---
+++ 02. Programing Fundamentals/02.3 Data Types and Variables - More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int charsCount = int.Parse(Console.ReadLine());
+            string decryptedMessage = string.Empty;
+
+            for (int numOfChar = 1; numOfChar <= charsCount; numOfChar++)
+            {
+                char currChar = (char)(char.Parse(Console.ReadLine()) + key);
+                decryptedMessage += currChar;
+            }
+
+            Console.WriteLine(decryptedMessage);
+        }
+    }
+}
