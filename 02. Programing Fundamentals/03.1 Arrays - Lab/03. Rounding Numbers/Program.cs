bbd540783c7c002509906ebb0e9b59ed08aee31f
Petar Belchev <petar9004@gmail.com>
2022-05-27 15:36:14 +0300

    Added a Rounding Numbers task

---
+++ 02. Programing Fundamentals/03.1 Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double roundedNum = Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+
+                Console.WriteLine($"{numbers[i]} => {Convert.ToDecimal(roundedNum)}");
+            }
+        }
+    }
+}
