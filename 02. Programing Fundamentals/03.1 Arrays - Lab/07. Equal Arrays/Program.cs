201d0a3955a49a43cbf662d48e106388447b08ed
Petar Belchev <petar9004@gmail.com>
2022-05-28 11:27:12 +0300

    Added a Equal Arrays task

---
+++ 02. Programing Fundamentals/03.1 Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNums1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arrayOfNums2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOfNums = 0;
+            bool isNotIdentical = false;
+
+            for (int i = 0; i < arrayOfNums1.Length; i++)
+            {
+                if (arrayOfNums1[i] == arrayOfNums2[i])
+                {
+                    sumOfNums += arrayOfNums1[i];
+                }
+                else
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    isNotIdentical = true;
+                    break;
+                }
+            }
+
+            if (!isNotIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sumOfNums}");
+            }
+        }
+    }
+}
