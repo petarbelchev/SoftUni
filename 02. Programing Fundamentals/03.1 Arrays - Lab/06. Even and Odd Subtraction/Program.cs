6c86c1ce9678c4a7581e6270c8db58e0c4711aeb
Petar Belchev <petar9004@gmail.com>
2022-05-28 11:26:47 +0300

    Added a Even and Odd Subtraction task

---
+++ 02. Programing Fundamentals/03.1 Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOfEvens = 0;
+            int sumOfOdds = 0;
+
+            foreach (int currNum in arrayOfNums)
+            {
+                if (currNum % 2 == 0)
+                {
+                    sumOfEvens += currNum;
+                }
+                else
+                {
+                    sumOfOdds += currNum;
+                }
+            }
+
+            int result = sumOfEvens - sumOfOdds;
+
+            Console.WriteLine(result);
+        }
+    }
+}
