985fa903ad2f583eb831427cf9b0a7c6f5c4a7bf
Petar Belchev <petar9004@gmail.com>
2022-05-28 11:26:17 +0300

    Added a Sum Even Numbers task

---
+++ 02. Programing Fundamentals/03.1 Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOfEvens = 0;
+            
+            foreach (int currNum in arrayOfNums)
+            {
+                if (currNum % 2 == 0)
+                {
+                    sumOfEvens += currNum;
+                }
+            }
+
+            Console.WriteLine(sumOfEvens);
+        }
+    }
+}
