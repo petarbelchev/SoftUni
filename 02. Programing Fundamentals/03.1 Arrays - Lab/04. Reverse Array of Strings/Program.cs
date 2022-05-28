098a7f1956668d9eeb15f301c25442c271726ff9
Petar Belchev <petar9004@gmail.com>
2022-05-28 11:25:48 +0300

    Added a Reverse Array of Strings task

---
+++ 02. Programing Fundamentals/03.1 Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfChars = Console.ReadLine().Split().ToArray();
+
+            Array.Reverse(arrayOfChars);
+            
+            foreach (string currChar in arrayOfChars)
+            {
+                Console.Write($"{currChar} ");
+            }
+        }
+    }
+}
