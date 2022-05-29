42f27c73fb4c05ee7758ee3e70d2104768c1648c
Petar Belchev <petar9004@gmail.com>
2022-05-29 15:21:33 +0300

    Added a Arrays Rotation task

---
+++ 02. Programing Fundamentals/03.2 Arrays - Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] rotatedArray = new int[arrayOfNumbers.Length];
+
+            int rotationsCount = int.Parse(Console.ReadLine());
+
+            for (int rotation = 0; rotation < rotationsCount; rotation++)
+            {
+                for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
+                {
+                    rotatedArray[i] = arrayOfNumbers[i + 1];
+                }
+                rotatedArray[rotatedArray.Length - 1] = arrayOfNumbers[0];
+
+                for (int i = 0; i < rotatedArray.Length; i++)
+                {
+                    arrayOfNumbers[i] = rotatedArray[i];
+                }
+            }
+
+            foreach (int number in arrayOfNumbers)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
