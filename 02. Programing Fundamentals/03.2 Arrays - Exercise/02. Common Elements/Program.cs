22847328a747000722dfafb92fc7649ac86b62c8
Petar Belchev <petar9004@gmail.com>
2022-05-29 15:20:28 +0300

    Added a Common Elements task

---
+++ 02. Programing Fundamentals/03.2 Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+
+            foreach (string itemSecondArray in secondArray)
+            {
+                foreach (string itemFirstArray in firstArray)
+                {
+                    if (itemSecondArray == itemFirstArray)
+                    {
+                        Console.Write($"{itemSecondArray} ");
+                    }
+                }
+            }
+        }
+    }
+}
