13776a5ec9b7ebb85b4a5a17e8433be69b4e17d8
Petar Belchev <petar9004@gmail.com>
2022-05-29 15:22:29 +0300

    Added a Equals Sum task

---
+++ 02. Programing Fundamentals/03.2 Arrays - Exercise/06. Equal Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool isHaveEqualsSum = false;
+
+            for (int currElement = 0; currElement < arrayOfNumbers.Length; currElement++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int leftSide = currElement - 1; leftSide >= 0; leftSide--)
+                {
+                    leftSum += arrayOfNumbers[leftSide];
+                }
+
+                for (int rightSide = currElement + 1; rightSide < arrayOfNumbers.Length; rightSide++)
+                {
+                    rightSum += arrayOfNumbers[rightSide];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(currElement);
+                    isHaveEqualsSum = true;
+                }
+            }
+
+            if (!isHaveEqualsSum)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
