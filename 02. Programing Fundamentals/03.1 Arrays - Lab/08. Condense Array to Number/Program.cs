3279c1c1f29c04f1d6e67736e91e7f7119563221
Petar Belchev <petar9004@gmail.com>
2022-05-29 11:35:55 +0300

    Added a Condense Array to Number task

---
+++ 02. Programing Fundamentals/03.1 Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int numCondensation = 0; numCondensation < arrayOfNumbers.Length - 1; numCondensation++)
+            {
+                for (int currIndex = 0; currIndex < arrayOfNumbers.Length - 1; currIndex++)
+                {
+                    arrayOfNumbers[currIndex] = arrayOfNumbers[currIndex] + arrayOfNumbers[currIndex + 1];
+                }
+            }
+
+            Console.WriteLine(arrayOfNumbers[0]);
+        }
+    }
+}
