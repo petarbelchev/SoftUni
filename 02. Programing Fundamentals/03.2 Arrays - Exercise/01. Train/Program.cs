f5b59ba57f532af804f70581b8c9371a9996fd2e
Petar Belchev <petar9004@gmail.com>
2022-05-29 15:19:54 +0300

    Added a Train task

---
+++ 02. Programing Fundamentals/03.2 Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] wagons = new int[int.Parse(Console.ReadLine())];
+            int sumOfPeople = 0;
+
+            for (int currWagon = 0; currWagon < wagons.Length; currWagon++)
+            {
+                wagons[currWagon] = int.Parse(Console.ReadLine());
+                sumOfPeople += wagons[currWagon];
+            }
+
+            foreach (int currWagon in wagons)
+            {
+                Console.Write($"{currWagon} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(sumOfPeople);
+        }
+    }
+}
