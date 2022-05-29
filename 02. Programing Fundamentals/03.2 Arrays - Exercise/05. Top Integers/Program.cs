d33cfeacb09a99da6b4d9971655f7d9879fc83de
Petar Belchev <petar9004@gmail.com>
2022-05-29 15:22:01 +0300

    Added a Top Integers task

---
+++ 02. Programing Fundamentals/03.2 Arrays - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                bool isGreater = true;
+
+                for (int j = i + 1; j < arrayOfNumbers.Length; j++)
+                {
+                    if (arrayOfNumbers[i] <= arrayOfNumbers[j])
+                    {
+                        isGreater = false;
+                        break;
+                    }
+                }
+
+                if (isGreater)
+                {
+                    Console.Write($"{arrayOfNumbers[i]} ");
+                }
+            }
+        }
+    }
+}
