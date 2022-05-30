c53c1ca8810223251b1f9d1db1789c794a3cb7d7
Petar Belchev <petar9004@gmail.com>
2022-05-30 14:28:16 +0300

    Added a Magic Sum task

---
+++ 02. Programing Fundamentals/03.2 Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumResult = int.Parse(Console.ReadLine());
+
+            for (int firstElement = 0; firstElement < arrayOfNumbers.Length - 1; firstElement++)
+            {
+                for (int secondElement = firstElement + 1; secondElement < arrayOfNumbers.Length; secondElement++)
+                {
+                    if (arrayOfNumbers[firstElement] + arrayOfNumbers[secondElement] == sumResult)
+                    {
+                        Console.WriteLine($"{arrayOfNumbers[firstElement]} {arrayOfNumbers[secondElement]}");
+                    }
+                }
+            }
+        }
+    }
+}
