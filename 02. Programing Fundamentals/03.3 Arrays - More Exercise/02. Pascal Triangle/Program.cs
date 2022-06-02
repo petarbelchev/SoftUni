a6bbc359b2c1ab0261093108cf5c9d8a5ac127ac
Petar Belchev <petar9004@gmail.com>
2022-06-02 19:34:28 +0300

    Added a Pascal Triangle task

---
+++ 02. Programing Fundamentals/03.3 Arrays - More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1");
+
+            if (rowsNum == 1)
+            {
+                return;
+            }
+
+            int[] firstArray = new int[2] { 1, 1 };
+            Console.WriteLine(String.Join(" ", firstArray));
+
+            if (rowsNum == 2)
+            {
+                return;
+            }
+            else
+            {
+                for (int row = 3; row <= rowsNum; row++)
+                {
+                    int[] secondArray = new int[firstArray.Length + 1];
+                    secondArray[0] = 1;
+                    secondArray[secondArray.Length - 1] = 1;
+
+                    for (int i = 1; i < secondArray.Length - 1; i++)
+                    {
+                        secondArray[i] = firstArray[i] + firstArray[i - 1];
+                    }
+                    firstArray = secondArray;
+
+                    Console.WriteLine(String.Join(" ", firstArray));
+                }
+            }
+        }
+    }
+}
