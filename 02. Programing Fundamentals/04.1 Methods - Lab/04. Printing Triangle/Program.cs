47e1ff66d98202eaa3b14a4a9b08e2d5420044b8
Petar Belchev <petar9004@gmail.com>
2022-06-04 18:11:22 +0300

    Added a Printing Triangle task

---
+++ 02. Programing Fundamentals/04.1 Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            PrintTriangle(length);
+        }
+
+        static void PrintTriangle(int length)
+        {
+            for (int row = 1; row <= length; row++)
+            {
+                int num = 1;
+
+                for (int col = length - row; col < length; col++)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = 1; row < length; row++)
+            {
+                int num = 1;
+
+                for (int col = row; col < length; col++)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
