e2f78cd66f889d01b90e206f5b48126175962ff6
Petar Belchev <petar9004@gmail.com>
2022-06-04 18:10:23 +0300

    Added a Grades task

---
+++ 02. Programing Fundamentals/04.1 Methods - Lab/02. Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintGrade(double.Parse(Console.ReadLine()));
+        }
+
+        static void PrintGrade(double grade)
+        {
+            if (grade >= 2.00 && grade <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3.00 && grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.5 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 5.5 && grade <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
