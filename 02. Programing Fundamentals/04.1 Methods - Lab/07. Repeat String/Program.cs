041e84f3fb099d01fab7cfe31a2f90815afbb941
Petar Belchev <petar9004@gmail.com>
2022-06-05 16:12:48 +0300

    Added a Repeat String task

---
+++ 02. Programing Fundamentals/04.1 Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int repeatCount = int.Parse(Console.ReadLine());
+
+            string repeatedWord = GetRepeatedWord(word, repeatCount);
+
+            Console.WriteLine(repeatedWord);
+        }
+
+        static string GetRepeatedWord(string word, int repeatCount)
+        {
+            StringBuilder repeatedWord = new StringBuilder();
+
+            for (int i = 0; i < repeatCount; i++)
+            {
+                repeatedWord.Append(word);
+            }
+
+            return repeatedWord.ToString();
+        }
+    }
+}
