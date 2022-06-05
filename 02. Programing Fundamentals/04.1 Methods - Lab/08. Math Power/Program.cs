d5ce3e6e0840a93cb8f78dd4ee957c4091be4ecf
Petar Belchev <petar9004@gmail.com>
2022-06-05 16:13:16 +0300

    Added a Math Power task

---
+++ 02. Programing Fundamentals/04.1 Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double result = raiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+        static double raiseToPower(double number, double power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
