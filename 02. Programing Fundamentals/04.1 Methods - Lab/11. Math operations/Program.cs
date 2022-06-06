8ab7e811478a0d6e543d93311cd8139d27e6e968
Petar Belchev <petar9004@gmail.com>
2022-06-06 12:16:32 +0300

    Added a Math operations task

---
+++ 02. Programing Fundamentals/04.1 Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+            double result = GetCalculate(firstNum, secondNum, @operator);
+
+            Console.WriteLine(result);
+        }
+
+        static double GetCalculate(double num, double num2, char operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case '/':
+                    result = num / num2;
+                    break;
+
+                case '*':
+                    result = num * num2;
+                    break;
+
+                case '+':
+                    result = num + num2;
+                    break;
+
+                case '-':
+                    result = num - num2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
