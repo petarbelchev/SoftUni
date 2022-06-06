8e68f8d5a2ea985f4d826e9079c2d044d6f9432a
Petar Belchev <petar9004@gmail.com>
2022-06-06 12:16:05 +0300

    Added a second version of a Multiply Evens by Odds task

---
+++ 02. Programing Fundamentals/04.1 Methods - Lab/10.2 Multiply Evens by Odds/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _10._2_Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int result = GetMultipleOfEvenAndOdds(number);
+
+            Console.WriteLine(result);
+        }
+
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+        }
+
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+
+                if (digit % 2 == 0)
+                {
+                    sum += digit;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+
+                if (digit % 2 != 0)
+                {
+                    sum += digit;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
