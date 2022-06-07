d48c8dbd1b5b395ba52a04db6dab59dedbc162ba
Petar Belchev <petar9004@gmail.com>
2022-06-07 10:07:12 +0300

    Added a Password Validator task

---
+++ 02. Programing Fundamentals/04.2 Methods - Exercise/04. Password Validator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _04._Password_Validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            if (CheckCharsCount(password)
+                && CheckDigitsCount(password)
+                && CheckLettersAndDigits(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                if (!CheckCharsCount(password))
+                {
+                    Console.WriteLine("Password must be between 6 and 10 characters");
+                }
+
+                if (!CheckLettersAndDigits(password))
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                }
+
+                if (!CheckDigitsCount(password))
+                {
+                    Console.WriteLine("Password must have at least 2 digits");
+                }
+            }
+        }
+
+        static bool CheckCharsCount(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool CheckLettersAndDigits(string password)
+        {
+            char[] chars = password.ToCharArray();
+
+            foreach (char ch in chars)
+            {
+                if (!(ch >= 65 && ch <= 90)
+                    && !(ch >= 97 && ch <= 122)
+                    && !(ch >= 48 && ch <= 57))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool CheckDigitsCount(string password)
+        {
+            char[] chars = password.ToCharArray();
+
+            int digitsCount = 0;
+
+            foreach (char ch in chars)
+            {
+                if (ch >= 48 && ch <= 57)
+                {
+                    digitsCount++;
+                }
+            }
+
+            if (digitsCount >= 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
