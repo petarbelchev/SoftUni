d2c00e69dbf88d050e08789fca4c673d59f476d5
Petar Belchev <petar9004@gmail.com>
2022-06-07 10:06:08 +0300

    Added a Vowels Count task

---
+++ 02. Programing Fundamentals/04.2 Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            Console.WriteLine(GetVowelsCount(word));
+        }
+
+        static int GetVowelsCount(string word)
+        {
+            char[] chars = word.ToCharArray();
+            int sumVowels = 0;
+
+            foreach (char ch in chars)
+            {
+                switch (ch)
+                {
+                    case 'A':
+                    case 'a':
+                    case 'E':
+                    case 'e':
+                    case 'I':
+                    case 'i':
+                    case 'O':
+                    case 'o':
+                    case 'U':
+                    case 'u':
+                        sumVowels++;
+                        break;
+                }
+            }
+
+            return sumVowels;
+        }
+    }
+}
