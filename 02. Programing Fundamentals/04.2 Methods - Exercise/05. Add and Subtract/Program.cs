12f5cffba1978fa9d9ead386cf93f017fe9343f7
Petar Belchev <petar9004@gmail.com>
2022-06-07 10:07:49 +0300

    Added a Add and Subtract task

---
+++ 02. Programing Fundamentals/04.2 Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int sum = GetAddNums(firstNum, secondNum);
+            int subtract = GetSubtract(sum, thirdNum);
+
+            Console.WriteLine(subtract);
+        }
+
+        static int GetAddNums(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int GetSubtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
