83ea8ec563c920f475a2e971d0602abef73ee6e8
Petar Belchev <petar9004@gmail.com>
2022-06-07 10:08:17 +0300

    Added a Middle Characters task

---
+++ 02. Programing Fundamentals/04.2 Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            GetMiddleChars(input);
+        }
+
+        static void GetMiddleChars(string input)
+        {
+            char[] chars = input.ToCharArray();
+            int middleIndex = (int)chars.Length / 2; ;
+
+            if (chars.Length % 2 == 0)
+            {
+                int secondMiddleIndex = middleIndex - 1;
+                Console.Write(chars[secondMiddleIndex]);
+            }
+
+            Console.WriteLine(chars[middleIndex]);
+        }
+    }
+}
