f6a75cb96de312cab6087784225c52f7f015d34f
Petar Belchev <petar9004@gmail.com>
2022-06-08 08:43:49 +0300

    Added a Factorial Division task

---
+++ 02. Programing Fundamentals/04.2 Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+            double facFirstNum = GetFactorial(firstNum);
+            double facSecondNum = GetFactorial(secondNum);
+
+            Console.WriteLine($"{(facFirstNum / facSecondNum):f2}");
+        }
+
+        static double GetFactorial(double number)
+        {
+            for (double i = number - 1; i > 1; i--)
+            {
+                number *= i;
+            }
+
+            return number;
+        }
+    }
+}
