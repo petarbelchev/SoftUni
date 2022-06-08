3cef573bafc1b64f78121b94000c7193354cd21a
Petar Belchev <petar9004@gmail.com>
2022-06-08 08:44:26 +0300

    Added a Palindrome Integers task

---
+++ 02. Programing Fundamentals/04.2 Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string reversedNumber = GetReversedNumber(command);
+
+                if (command == reversedNumber)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        static string GetReversedNumber(string number)
+        {
+            char[] arrOfNumber = number.ToCharArray();
+
+            Array.Reverse(arrOfNumber);
+
+            return new string(arrOfNumber);
+        }
+    }
+}
