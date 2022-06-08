6c9c76f763e057063d8f9a9c0c416b00e4a82c5f
Petar Belchev <petar9004@gmail.com>
2022-06-08 08:43:12 +0300

    Added a NxN Matrix task

---
+++ 02. Programing Fundamentals/04.2 Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsAndCols = int.Parse(Console.ReadLine());
+
+            PrintMatrix(rowsAndCols);
+        }
+
+        static void PrintMatrix(int rowsAndCols)
+        {
+            for (int row = 1; row <= rowsAndCols; row++)
+            {
+                for (int col = 1; col <= rowsAndCols; col++)
+                {
+                    Console.Write(rowsAndCols + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
