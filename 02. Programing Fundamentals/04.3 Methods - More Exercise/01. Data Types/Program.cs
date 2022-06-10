0fc4498c002be4bcc10d4295f7ee43b57bbd3711
Petar Belchev <petar9004@gmail.com>
2022-06-10 15:29:45 +0300

    Added a Data Types task

---
+++ 02. Programing Fundamentals/04.3 Methods - More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            switch (type)
+            {
+                case "int":
+                    int number = int.Parse(Console.ReadLine());
+                    Print(number);
+                    break;
+
+                case "real":
+                    double realNumber = double.Parse(Console.ReadLine());
+                    Print(realNumber);
+                    break;
+
+                case "string":
+                    string input = Console.ReadLine();
+                    Print(input);
+                    break;
+            }
+
+            
+        }
+
+        static void Print(int input)
+        {
+            Console.WriteLine(input * 2);
+        }
+
+        static void Print(double input)
+        {
+            Console.WriteLine($"{input * 1.5:f2}");
+        }
+
+        static void Print(string input)
+        {
+            Console.WriteLine($"${input}$");
+        }
+    }
+}
