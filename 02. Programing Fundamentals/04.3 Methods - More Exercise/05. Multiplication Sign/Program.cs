528ba706d330f12afb189ab2de1f25de55e42c64
Petar Belchev <petar9004@gmail.com>
2022-06-12 16:07:29 +0300

    Added a Multiplication Sign task

---
+++ 02. Programing Fundamentals/04.3 Methods - More Exercise/05. Multiplication Sign/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _05._Multiplication_Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[3];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(CheckProduct(nums));
+        }
+
+        static string CheckProduct(int[] nums)
+        {
+            foreach (int num in nums)
+            {
+                if (num == 0)
+                {
+                    return "zero";
+                }
+            }
+
+            int counter = 0;
+
+            foreach (int num in nums)
+            {
+                if (num < 0)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter % 2 != 0)
+            {
+                return "negative";
+            }
+
+            return "positive";
+        }
+    }
+}
