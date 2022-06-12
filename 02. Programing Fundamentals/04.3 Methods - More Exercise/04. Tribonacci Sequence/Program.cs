df953b726a5cf50aa61fbee0cacd8389953cc42a
Petar Belchev <petar9004@gmail.com>
2022-06-12 16:07:02 +0300

    Added a Tribonacci Sequence task

---
+++ 02. Programing Fundamentals/04.3 Methods - More Exercise/04. Tribonacci Sequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04._Tribonacci_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", TribonacciSequence(int.Parse(Console.ReadLine()))));
+        }
+
+        static int[] TribonacciSequence(int length)
+        {
+            int[] sequence = new int[length];
+
+            sequence[0] = 1;
+
+            if (length < 2)
+            {
+                return sequence;
+            }
+
+            sequence[1] = 1;
+
+            if (length < 3)
+            {
+                return sequence;
+            }
+
+            sequence[2] = 2;
+
+            for (int i = 3; i < length; i++)
+            {
+                sequence[i] = sequence[i - 1] + sequence[i - 2] + sequence[i - 3];
+            }
+
+            return sequence;
+        }
+    }
+}
