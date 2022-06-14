d856cdebc0b416ca7b3f71320ded208f2453bd0a
Petar Belchev <petar9004@gmail.com>
2022-06-14 12:28:54 +0300

    Added a List Manipulation Basics task

---
+++ 02. Programing Fundamentals/05.1 Lists - Lab/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._List_Manipulation_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] command = input.Split();
+
+                if (command[0] == "Add")
+                {
+                    numbers.Add(int.Parse(command[1]));
+                }
+                else if (command[0] == "Remove")
+                {
+                    numbers.Remove(int.Parse(command[1]));
+                }
+                else if (command[0] == "RemoveAt")
+                {
+                    numbers.RemoveAt(int.Parse(command[1]));
+                }
+                else if (command[0] == "Insert")
+                {
+                    numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
