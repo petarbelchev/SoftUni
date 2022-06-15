b18f2c6a07be6927c43ac6373bdde0e36e6167bd
Petar Belchev <petar9004@gmail.com>
2022-06-15 11:58:12 +0300

    Added a Append Arrays task

---
+++ 02. Programing Fundamentals/05.2 Lists - Exercise/07.2 Append Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._2_Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split('|').ToList();
+
+            input.Reverse();
+            
+            List<string> result = new List<string>();
+            
+            foreach (string item in input)
+            {
+                result.AddRange(item.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList());
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
