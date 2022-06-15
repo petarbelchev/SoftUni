af10b6ba8987b6af0d1ae1daa0682c0abd8a612a
Petar Belchev <petar9004@gmail.com>
2022-06-15 11:55:38 +0300

    Added a House Party task

---
+++ 02. Programing Fundamentals/05.2 Lists - Exercise/03. House Party/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfInvitations = int.Parse(Console.ReadLine());
+
+            List<string> guestList = new List<string>();
+
+            for (int invitation = 1; invitation <= numOfInvitations; invitation++)
+            {
+                string[] answer = Console.ReadLine().Split();
+                string nameGuest = answer[0];
+                string goingOrNot = answer[2];
+
+                if (goingOrNot == "going!")
+                {
+                    if (guestList.Contains(nameGuest))
+                    {
+                        Console.WriteLine($"{nameGuest} is already in the list!");
+                    }
+                    else
+                    {
+                        guestList.Add(nameGuest);
+                    }
+                }
+                else if (goingOrNot == "not")
+                {
+                    if (guestList.Contains(nameGuest))
+                    {
+                        guestList.Remove(nameGuest);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{nameGuest} is not in the list!");
+                    }
+                }
+            }
+
+            foreach (string name in guestList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
