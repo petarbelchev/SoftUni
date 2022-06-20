c43a3cc15fd143beb1b1e4a38a2b7ba6b93192b7
Petar Belchev <petar9004@gmail.com>
2022-06-20 14:53:02 +0300

    Added a Drum Set task

---
+++ 02. Programing Fundamentals/05.3 Lists - More Exercise/05. Drum Set/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Drum_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+
+            List<int> drumSet = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] initialDrumSet = new int[drumSet.Count];
+
+            drumSet.CopyTo(initialDrumSet);
+
+            string input = Console.ReadLine();
+
+            while (input != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(input);
+
+                for (int i = 0; i < drumSet.Count; i++)
+                {
+                    drumSet[i] -= hitPower;
+
+                    if (drumSet[i] <= 0)
+                    {
+                        if (savings >= initialDrumSet[i] * 3)
+                        {
+                            savings -= initialDrumSet[i] * 3;
+                            drumSet[i] = initialDrumSet[i];
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", drumSet.FindAll(x => x > 0)));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
