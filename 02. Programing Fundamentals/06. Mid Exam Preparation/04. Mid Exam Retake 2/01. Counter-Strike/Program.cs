b54995dac399ce12d86b602c73414f10f1b51668
Petar Belchev <petar9004@gmail.com>
2022-06-20 13:24:03 +0300

    Added a Counter-Strike task

---
+++ 02. Programing Fundamentals/06. Mid Exam Preparation/04. Mid Exam Retake 2/01. Counter-Strike/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int wonsCount = 0;
+
+            while (input != "End of battle")
+            {
+                int distance = int.Parse(input);
+
+                if (distance <= initialEnergy)
+                {
+                    wonsCount++;
+                    initialEnergy -= distance;
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wonsCount} won battles and {initialEnergy} energy");
+                    return;
+                }
+
+                if (wonsCount % 3 == 0)
+                {
+                    initialEnergy += wonsCount;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Won battles: {wonsCount}. Energy left: {initialEnergy}");
+        }
+    }
+}
