52f228be31299f9f58dc424c820190735439e226
Petar Belchev <petar9004@gmail.com>
2022-06-20 13:25:07 +0300

    Added a Moving Target task

---
+++ 02. Programing Fundamentals/06. Mid Exam Preparation/04. Mid Exam Retake 2/03. Moving Target/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._MovTarget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] cmdArgs = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string mainCmd = cmdArgs[0];
+                int index = int.Parse(cmdArgs[1]);
+
+                switch (mainCmd)
+                {
+                    case "Shoot":
+                        int power = int.Parse(cmdArgs[2]);
+                        if (index >= 0 && index < targets.Count)
+                        {
+                            targets[index] -= power;
+                            if (targets[index] <= 0)
+                            {
+                                targets.RemoveAt(index);
+                            }
+                        }
+                        break;
+                    case "Add":
+                        int value = int.Parse(cmdArgs[2]);
+                        if (index >= 0 && index < targets.Count)
+                        {
+                            targets.Insert(index, value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid placement!");
+                        }
+                        break;
+                    case "Strike":
+                        int radius = int.Parse(cmdArgs[2]);
+                        if (IsStrikeMissed(targets, index, radius))
+                        {
+                            Console.WriteLine("Strike missed!");
+                        }
+                        else
+                        {
+                            targets.RemoveRange(index - radius, (radius * 2) + 1);
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join("|", targets));
+        }
+
+        static bool IsStrikeMissed(List<int> targets, int index, int radius)
+        {
+            if (index - radius >= 0 && index + radius < targets.Count)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
