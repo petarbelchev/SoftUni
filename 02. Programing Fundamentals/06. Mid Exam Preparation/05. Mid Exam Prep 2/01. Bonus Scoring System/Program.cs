a64d086e55a9279da796b760d674241b152db2d0
Petar Belchev <petar9004@gmail.com>
2022-06-23 12:04:01 +0300

    Added a Bonus Scoring System task

---
+++ 02. Programing Fundamentals/06. Mid Exam Preparation/05. Mid Exam Prep 2/01. Bonus Scoring System/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Bonus_Scoring_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStudents = int.Parse(Console.ReadLine());
+            int numLectures = int.Parse(Console.ReadLine());
+            int adBonus = int.Parse(Console.ReadLine());
+            double biggestBonus = 0;
+            int maxAtt = 0;
+
+            for (int currStudent = 1; currStudent <= numStudents; currStudent++)
+            {
+                int attCurrStudent = int.Parse(Console.ReadLine());
+                double currBonus = (double)attCurrStudent / numLectures * (5 + adBonus);
+
+                if (currBonus > biggestBonus)
+                {
+                    biggestBonus = currBonus;
+                    maxAtt = attCurrStudent;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(biggestBonus)}.");
+            Console.WriteLine($"The student has attended {maxAtt} lectures.");
+        }
+    }
+}
