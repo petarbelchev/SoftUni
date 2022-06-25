3e0a1b3b06226a5bad5745679a80668fbc149428
Petar Belchev <petar9004@gmail.com>
2022-06-25 13:02:44 +0300

    Added a Black Flag task

---
+++ 02. Programing Fundamentals/06. Mid Exam Preparation/06. Mid Exam Prep Retake 3/01. Black Flag/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalDays = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double addPlunder = dailyPlunder * 0.5;
+            double expectedPlunder = double.Parse(Console.ReadLine());
+            double gatheredQuantity = 0;
+
+            for (int day = 1; day <= totalDays; day++)
+            {
+                gatheredQuantity += dailyPlunder;
+
+                if (day % 3 == 0)
+                {
+                    gatheredQuantity += addPlunder;
+                }
+
+                if (day % 5 == 0)
+                {
+                    gatheredQuantity -= gatheredQuantity * 0.3;
+                }
+            }
+
+            if (gatheredQuantity >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {gatheredQuantity:f2} plunder gained.");
+            }
+            else
+            {
+                double percentage = (gatheredQuantity / expectedPlunder) * 100;
+
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+        }
+    }
+}
