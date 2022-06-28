4efbdc5438b8f2733ec8063554a3e7cc26f2ed7e
Petar Belchev <petar9004@gmail.com>
2022-06-28 17:14:04 +0300

    Added a Second task

---
+++ 02. Programing Fundamentals/07. Mid Exam/SecondTask/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecondTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            string cmd = Console.ReadLine();
+
+            while (cmd != "Finish")
+            {
+                string[] cmdArgs = cmd.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                string mainCmd = cmdArgs[0];
+                int value = int.Parse(cmdArgs[1]);
+                switch (mainCmd)
+                {
+                    case "Add":
+                        numbers.Add(value);
+                        break;
+
+                    case "Remove":
+                        if (numbers.Contains(value))
+                        {
+                            numbers.Remove(value);
+                        }
+                        break;
+
+                    case "Replace":
+                        if (numbers.Contains(value))
+                        {
+                            int replacement = int.Parse(cmdArgs[2]);
+                            int indexValue = numbers.IndexOf(value);
+                            numbers.Remove(value);
+                            numbers.Insert(indexValue, replacement);                            
+                        }
+                        break;
+
+                    case "Collapse":
+                        numbers.RemoveAll(num => num < value);
+                        break;
+                }
+
+                cmd = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
