d6d3d32fd20b40091d9a63a05836dfb0a11b9e4e
Petar Belchev <petar9004@gmail.com>
2022-06-29 11:20:05 +0300

    Added a Big Factorial task

---
+++ 02. Programing Fundamentals/08.1 Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = n;
+
+            for (int i = n - 1; i > 1; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
