ea8b7baef044fa35d392d420ebb0e9caa5eaacd0
Petar Belchev <petar9004@gmail.com>
2022-07-02 08:58:45 +0300

    Added a Oldest Family Member task

---
+++ 02. Programing Fundamentals/08.3 Objects and Classes - More Exercise/02. Oldest Family Member/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Oldest_Family_Member
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int numberOfPersons = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPersons; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personDetails[0];
+                int age = int.Parse(personDetails[1]);
+                Person newPerson = new Person(name, age);
+                family.AddMember(newPerson);
+            }
+
+            Console.WriteLine(family.GetOldestPerson());
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+
+    class Family
+    {
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person person)
+        {
+            this.People.Add(person);
+        }
+
+        public Person GetOldestPerson()
+        {
+            this.People = this.People.OrderByDescending(m => m.Age).ToList();
+
+            return People[0];
+        }
+    }
+}
