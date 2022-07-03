e48060dbf0177d194a4698ca770c9e96def0e45e
Petar Belchev <petar9004@gmail.com>
2022-07-03 19:20:30 +0300

    Added a Word Filter task

---
+++ 02. Programing Fundamentals/09.1 Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordsLength = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                wordsLength.Add(word, word.Length);
+            }
+
+            string[] evenWords = wordsLength
+                .Where(word => word.Value % 2 == 0)
+                .Select(word => word.Key)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, evenWords));
+        }
+    }
+}
