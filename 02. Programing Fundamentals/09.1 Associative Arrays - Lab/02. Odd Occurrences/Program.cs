a212979931971ad1c701802c7a91e2c51452eada
Petar Belchev <petar9004@gmail.com>
2022-07-03 19:19:40 +0300

    Added a Odd Occurrences task

---
+++ 02. Programing Fundamentals/09.1 Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string lowerWord = word.ToLower();
+
+                if (result.ContainsKey(lowerWord))
+                {
+                    result[lowerWord]++;
+                }
+                else
+                {
+                    result.Add(lowerWord, 1);
+                }
+            }
+
+            words = result
+                .Where(word => word.Value % 2 != 0)
+                .Select(word => word.Key)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", words));
+        }
+    }
+}
