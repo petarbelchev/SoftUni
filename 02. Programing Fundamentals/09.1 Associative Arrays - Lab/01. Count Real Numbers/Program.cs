9668867505f2ff4205d519af51fbd06f6daf78aa
Petar Belchev <petar9004@gmail.com>
2022-07-03 19:19:10 +0300

    Added a Count Real Numbers task

---
+++ 02. Programing Fundamentals/09.1 Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            SortedDictionary<int, int> result = new SortedDictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (result.ContainsKey(number))
+                {
+                    result[number] += 1;
+                }
+                else
+                {
+                    result.Add(number, 1);
+                }
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
