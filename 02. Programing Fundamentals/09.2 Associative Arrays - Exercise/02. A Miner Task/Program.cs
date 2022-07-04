b1f0996a9c64ccb678861aa73e7b7897b78a99b0
Petar Belchev <petar9004@gmail.com>
2022-07-04 17:20:09 +0300

    Added A Miner Task

---
+++ 02. Programing Fundamentals/09.2 Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            
+            string input = Console.ReadLine();
+            
+            while (input != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(input))
+                {
+                    resources[input] += quantity;
+                }
+                else
+                {
+                    resources.Add(input, quantity);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
