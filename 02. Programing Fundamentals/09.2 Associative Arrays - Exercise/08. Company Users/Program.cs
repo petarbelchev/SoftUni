7f53644f673cc678edfc2bc03297c9d07e3e66ef
Petar Belchev <petar9004@gmail.com>
2022-07-05 09:49:58 +0300

    Added a Company Users task

---
+++ 02. Programing Fundamentals/09.2 Associative Arrays - Exercise/08. Company Users/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = cmd.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string company = cmdArgs[0];
+                string employeeId = cmdArgs[1];
+                if (companies.ContainsKey(company))
+                {
+                    if (!companies[company].Contains(employeeId))
+                    {
+                        companies[company].Add(employeeId);
+                    }
+                }
+                else
+                {
+                    companies.Add(company, new List<string>() { employeeId });
+                }
+            }
+
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Key);
+                foreach (var employeeId in company.Value)
+                {
+                    Console.WriteLine($"-- {employeeId}");
+                }
+            }
+        }
+    }
+}
