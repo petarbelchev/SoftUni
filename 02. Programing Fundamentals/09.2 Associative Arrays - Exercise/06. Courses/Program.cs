43969516e24060e46372db4a46e57144737bd30a
Petar Belchev <petar9004@gmail.com>
2022-07-05 09:49:05 +0300

    Added a Courses task

---
+++ 02. Programing Fundamentals/09.2 Associative Arrays - Exercise/06. Courses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = cmd.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+                string courseName = cmdArgs[0];
+                string studentName = cmdArgs[1];
+
+                if (courses.ContainsKey(courseName))
+                {
+                    courses[courseName].Add(studentName);
+                }
+                else
+                {
+                    courses.Add(courseName, new List<string>() { studentName });
+                }
+            }
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+
+                foreach (var student in course.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
