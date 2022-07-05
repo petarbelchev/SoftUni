47ff733e9c2f7888e47f4bf01b97b5cbc812e532
Petar Belchev <petar9004@gmail.com>
2022-07-05 09:49:31 +0300

    Added a Student Academy task

---
+++ 02. Programing Fundamentals/09.2 Associative Arrays - Exercise/07. Student Academy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            int pairsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < pairsCount; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (students.ContainsKey(student))
+                {
+                    students[student].Add(grade);
+                }
+                else
+                {
+                    students.Add(student, new List<double>() { grade });
+                }
+            }
+
+            foreach (var student in students)
+            {
+                double averageGrade = student.Value.Average();
+
+                if (averageGrade >= 4.5)
+                {
+                    Console.WriteLine($"{student.Key} -> {averageGrade:f2}");
+                }
+            }
+        }
+    }
+}
