dc5113496ce97a444df5cb597239e9ede48f9c6c
Petar Belchev <petar9004@gmail.com>
2022-07-06 17:22:41 +0300

    Added a Snowwhite task

---
+++ 02. Programing Fundamentals/09.3 Associative Arrays - More Exercise/04. Snowwhite/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Snowwhite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dwarfs = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Once upon a time")
+            {
+                string[] inputArgs = input.Split(" <:> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string dwarfId = inputArgs[0] + ':' + inputArgs[1];
+                int dwarfPhysics = int.Parse(inputArgs[2]);
+
+                if (!dwarfs.ContainsKey(dwarfId))
+                {
+                    dwarfs.Add(dwarfId, dwarfPhysics);
+                }
+                else
+                {
+                    if (dwarfs[dwarfId] < dwarfPhysics)
+                    {
+                        dwarfs[dwarfId] = dwarfPhysics;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var dwarf in dwarfs
+                .OrderByDescending(d => d.Value)
+                .ThenByDescending(x => dwarfs.Where(y => y.Key.Split(':')[1] == x.Key.Split(':')[1]).Count()))
+            {
+                Console.WriteLine
+                    ($"({dwarf.Key.Split(':')[1]}) {dwarf.Key.Split(':')[0]} <-> {dwarf.Value}");
+            }
+        }
+    }
+}
