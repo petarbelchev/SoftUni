b04e4facd96fc78e9b53cd14f066502717e63dc4
Petar Belchev <petar9004@gmail.com>
2022-07-07 16:11:17 +0300

    Added a Substring task

---
+++ 02. Programing Fundamentals/10.1 Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string textToEdit = Console.ReadLine();
+
+            while (textToEdit.Contains(wordToRemove))
+            {
+                textToEdit = textToEdit.Replace(wordToRemove, "");
+            }
+
+            Console.WriteLine(textToEdit);
+        }
+    }
+}
