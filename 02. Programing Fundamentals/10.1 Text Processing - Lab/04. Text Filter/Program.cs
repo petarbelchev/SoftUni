b078487798a244e37447bca225e1eabdc64ef3bc
Petar Belchev <petar9004@gmail.com>
2022-07-07 16:11:38 +0300

    Added a Text Filter task

---
+++ 02. Programing Fundamentals/10.1 Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            char ch = '*';
+
+            foreach (var word in bannedWords)
+            {
+                text = text.Replace(word, new string(ch, word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
