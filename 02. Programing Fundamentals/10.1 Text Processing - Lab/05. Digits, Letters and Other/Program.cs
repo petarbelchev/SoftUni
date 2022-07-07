6b2bb1b0a3cc69015e22f2d72061a1fd15a4909b
Petar Belchev <petar9004@gmail.com>
2022-07-07 16:12:08 +0300

    Added a Digits, Letters and Others task

---
+++ 02. Programing Fundamentals/10.1 Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string allDigits = new string(input
+                .Where(ch => char.IsDigit(ch) == true)
+                .ToArray());
+            
+            string allLetters = new string(input
+                .Where(ch => char.IsLetter(ch) == true)
+                .ToArray());
+           
+            string otherChars = new string(input
+                .Where(ch => char.IsLetterOrDigit(ch) == false)
+                .ToArray());
+
+            Console.WriteLine(allDigits);
+            Console.WriteLine(allLetters);
+            Console.WriteLine(otherChars);
+        }
+    }
+}
