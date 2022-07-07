8edad00c10cdbaad49ca09eca05e61818dca0284
Petar Belchev <petar9004@gmail.com>
2022-07-07 16:10:31 +0300

    Added a Reverse Strings task

---
+++ 02. Programing Fundamentals/10.1 Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            while (word != "end")
+            {
+                char[] wordCharArr = word.ToCharArray();
+                Array.Reverse(wordCharArr);
+                string reversedWord = new string(wordCharArr);
+                Console.WriteLine($"{word} = {reversedWord}");
+
+                word = Console.ReadLine();
+            }
+        }
+    }
+}
