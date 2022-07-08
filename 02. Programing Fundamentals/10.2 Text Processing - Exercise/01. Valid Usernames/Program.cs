e6dd5e278d183eabb8b7acb52ce327896b6a74e1
Petar Belchev <petar9004@gmail.com>
2022-07-08 18:45:09 +0300

    Added a Valid Username task

---
+++ 02. Programing Fundamentals/10.2 Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string name in names)
+            {
+                if (name.Length >= 3 
+                    && name.Length <= 16 
+                    && !name.Any(ch => char.IsLetterOrDigit(ch) == false && ch != '-' && ch != '_'))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
