1c06a9178f4a9cef147e35994891f12e3e899e5f
Petar Belchev <petar9004@gmail.com>
2022-07-08 18:46:28 +0300

    Added a Caesar Cipher task

---
+++ 02. Programing Fundamentals/10.2 Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int newChar = chars[i] + 3;
+                chars[i] = (char)newChar;
+            }
+
+            Console.WriteLine(new string (chars));
+        }
+    }
+}
