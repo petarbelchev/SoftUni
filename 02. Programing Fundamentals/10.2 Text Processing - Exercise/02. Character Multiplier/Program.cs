bc1f624b2fef0556a2416e74afc1a0b063a191ca
Petar Belchev <petar9004@gmail.com>
2022-07-08 18:45:34 +0300

    Added a Character Multiplier task

---
+++ 02. Programing Fundamentals/10.2 Text Processing - Exercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int result = CharacterMultiplier(strings[0], strings[1]);
+
+            Console.WriteLine(result);
+        }
+
+        static int CharacterMultiplier(string firstString, string secondString)
+        {
+            int sum = 0;
+
+            List<char> firstChars = firstString.ToCharArray().ToList();
+            List<char> secondChars = secondString.ToCharArray().ToList();
+
+            while (firstChars.Count > 0 && secondChars.Count > 0)
+            {
+                sum += firstChars[0] * secondChars[0];
+                firstChars.RemoveAt(0);
+                secondChars.RemoveAt(0);
+            }
+
+            if (firstChars.Count > 0)
+            {
+                foreach (char ch in firstChars)
+                {
+                    sum += ch;
+                }
+            }
+            else
+            {
+                foreach (char ch in secondChars)
+                {
+                    sum += ch;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
