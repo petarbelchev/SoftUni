e8e0698d4ecc4589373cc88dd7930139ecf91f38
Petar Belchev <petar9004@gmail.com>
2022-07-08 18:45:56 +0300

    Added a Extract File task

---
+++ 02. Programing Fundamentals/10.2 Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filePath = Console.ReadLine()
+                .Split('\\', StringSplitOptions.RemoveEmptyEntries);
+
+            string fileName = filePath[filePath.Length - 1].Split('.')[0];
+            string fileExtension = filePath[filePath.Length - 1].Split('.')[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
