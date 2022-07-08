56bb11320934e0c0453a1c7e02894ec50550b554
Petar Belchev <petar9004@gmail.com>
2022-07-08 18:47:14 +0300

    Added a Replace Repeating Chars task

---
+++ 02. Programing Fundamentals/10.2 Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> chars = Console.ReadLine().ToList();
+
+            int originalCount = chars.Count;
+
+            int index = 0;
+
+            for (int i = 0; i < originalCount - 1; i++)
+            {
+                if (chars[index] == chars[index + 1])
+                {
+                    chars.RemoveAt(index);
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            Console.WriteLine(string.Join("", chars));
+        }
+    }
+}
