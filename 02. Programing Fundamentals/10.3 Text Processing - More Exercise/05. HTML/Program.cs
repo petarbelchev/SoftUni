30d61b5500a663de4616f07d0edecc887a12ade0
Petar Belchev <petar9004@gmail.com>
2022-07-10 14:19:16 +0300

    Added a HTML task

---
+++ 02. Programing Fundamentals/10.3 Text Processing - More Exercise/05. HTML/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _05._HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder htmlArticle = new StringBuilder();
+
+            htmlArticle.AppendLine("<h1>");
+            htmlArticle.AppendLine($"    {Console.ReadLine()}");
+            htmlArticle.AppendLine("</h1>");
+            htmlArticle.AppendLine("<article>");
+            htmlArticle.AppendLine($"    {Console.ReadLine()}");
+            htmlArticle.AppendLine("</article>");
+
+            string comment = Console.ReadLine();
+
+            while (comment != "end of comments")
+            {
+                htmlArticle.AppendLine("<div>");
+                htmlArticle.AppendLine($"    {comment}");
+                htmlArticle.AppendLine("</div>");
+
+                comment = Console.ReadLine();
+            }
+
+            Console.WriteLine(htmlArticle.ToString().TrimEnd());
+        }
+    }
+}
