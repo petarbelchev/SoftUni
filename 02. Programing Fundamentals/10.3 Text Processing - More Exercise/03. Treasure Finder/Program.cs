3f67bfccb9f4fc58b7d74214e279d38a82cbccf9
Petar Belchev <petar9004@gmail.com>
2022-07-10 14:18:12 +0300

    Added a Treasure Finder task

---
+++ 02. Programing Fundamentals/10.3 Text Processing - More Exercise/03. Treasure Finder/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Treasure_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] keys = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            string input = Console.ReadLine();
+
+            while (input != "find")
+            {
+                char[] cryptedMessage = input.ToCharArray();
+
+                int keyIndex = 0;
+
+                for (int i = 0; i < cryptedMessage.Length; i++)
+                {
+                    cryptedMessage[i] -= (char)keys[keyIndex];
+                    keyIndex++;
+
+                    if (keyIndex >= keys.Length)
+                    {
+                        keyIndex = 0;
+                    }
+                }
+
+                string decyptedMessage = new string(cryptedMessage);
+
+                int startIndexType = decyptedMessage.IndexOf('&') + 1;
+                int endIndexType = decyptedMessage.LastIndexOf('&');
+                string type = decyptedMessage.Substring(startIndexType, endIndexType - startIndexType);
+
+                int startIndexCoordinates = decyptedMessage.IndexOf('<') + 1;
+                int endIndexCoordinates = decyptedMessage.IndexOf('>');
+                string coordinates = decyptedMessage.Substring(startIndexCoordinates, endIndexCoordinates - startIndexCoordinates);
+
+                Console.WriteLine($"Found {type} at {coordinates}");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
