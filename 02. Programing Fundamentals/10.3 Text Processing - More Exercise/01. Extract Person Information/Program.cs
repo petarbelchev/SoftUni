45496db3abf43ccaa512533afb39478d8648c1c8
Petar Belchev <petar9004@gmail.com>
2022-07-10 14:17:11 +0300

    Added a Extract Person Information task

---
+++ 02. Programing Fundamentals/10.3 Text Processing - More Exercise/01. Extract Person Information/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfTexts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfTexts; i++)
+            {
+                string text = Console.ReadLine();
+
+                int startIndexName = text.IndexOf('@') + 1;
+                int endIndexName = text.IndexOf('|');
+                string name = text.Substring(startIndexName, endIndexName - startIndexName);
+
+                int startIndexAge = text.IndexOf('#') + 1;
+                int endIndexAge = text.IndexOf('*');
+                string age = text.Substring(startIndexAge, endIndexAge - startIndexAge);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
