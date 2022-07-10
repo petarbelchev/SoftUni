30ec7f788be24d38b32685f54e6e176cf15913a3
Petar Belchev <petar9004@gmail.com>
2022-07-10 14:17:39 +0300

    Added a Ascii Sumator task

---
+++ 02. Programing Fundamentals/10.3 Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            string stringInput = Console.ReadLine();
+            int sum = 0;
+
+            foreach (char ch in stringInput)
+            {
+                if (ch > firstChar && ch < secondChar)
+                {
+                    sum += ch;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
