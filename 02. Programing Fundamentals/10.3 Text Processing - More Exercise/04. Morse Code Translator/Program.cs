2ce09e861af0ab1b5e92b8582369dca234052370
Petar Belchev <petar9004@gmail.com>
2022-07-10 14:18:53 +0300

    Added a Morse Code Translator task

---
+++ 02. Programing Fundamentals/10.3 Text Processing - More Exercise/04. Morse Code Translator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04._Morse_Code_Translator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, char> morsecode = new Dictionary<string, char>()
+            {
+                {".-", 'A'},
+                {"-...", 'B'},
+                {"-.-.", 'C'},
+                {"-..", 'D'},
+                {".", 'E'},
+                {"..-.", 'F'},
+                {"--.", 'G'},
+                {"....", 'H'},
+                {"..", 'I'},
+                {".---", 'J'},
+                {"-.-", 'K'},
+                {".-..", 'L'},
+                {"--", 'M'},
+                {"-.", 'N'},
+                {"---", 'O'},
+                {".--.", 'P'},
+                {"--.-", 'Q'},
+                {".-.", 'R'},
+                {"...", 'S'},
+                {"-", 'T'},
+                {"..-", 'U'},
+                {"...-", 'V'},
+                {".--", 'W'},
+                {"-..-", 'X'},
+                {"-.--", 'Y'},
+                {"--..", 'Z'}
+            };
+
+            string cryptedMessage = Console.ReadLine();
+
+            StringBuilder currCryptedChar = new StringBuilder();
+            StringBuilder decryptedMessage = new StringBuilder();
+
+            foreach (char symbol in cryptedMessage)
+            {
+                if (symbol == '.' || symbol == '-')
+                {
+                    currCryptedChar.Append(symbol);
+                }
+                else if (symbol == ' ' && currCryptedChar.Length > 0)
+                {
+                    decryptedMessage.Append(morsecode[currCryptedChar.ToString()]);
+                    currCryptedChar.Clear();
+                }
+                else if (symbol == '|')
+                {
+                    decryptedMessage.Append(' ');
+                }
+            }
+
+            if (currCryptedChar.Length > 0)
+            {
+                decryptedMessage.Append(morsecode[currCryptedChar.ToString()]);
+            }
+
+            Console.WriteLine(decryptedMessage.ToString().TrimEnd());
+        }
+    }
+}
