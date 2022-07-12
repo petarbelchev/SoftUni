2eca2e04fe7a261691cfa3c9a2929acdcc7ca3d5
Petar Belchev <petar9004@gmail.com>
2022-07-12 16:41:33 +0300

    Added a Match Phone Number task

---
+++ 02. Programing Fundamentals/11.1 Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(\+359)(( )|(-))2\2\d{3}\2\d{4}\b");
+
+            string phoneNumbers = Console.ReadLine();
+
+            MatchCollection matchedPhones = regex.Matches(phoneNumbers);
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
