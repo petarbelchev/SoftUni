3b12e3b3e09a2a05050b5dddaa3fadf75106c357
Petar Belchev <petar9004@gmail.com>
2022-07-12 16:41:06 +0300

    Added a Match Full Name task

---
+++ 02. Programing Fundamentals/11.1 Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Regex regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+");
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine(string.Join(' ', matches));
+        }
+    }
+}
