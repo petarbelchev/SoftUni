b7b5410d709363fb7ed95eace2fe8c8b2ad940fd
Petar Belchev <petar9004@gmail.com>
2022-07-12 16:42:02 +0300

    Added a Match Dates task

---
+++ 02. Programing Fundamentals/11.1 Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dates = Console.ReadLine();
+
+            Regex regex = new Regex(@"\b(\d{2})([-\.\/])([A-Z][a-z]{2})\2(\d{4})\b");
+
+            MatchCollection matchedDates = regex.Matches(dates);
+
+            foreach (Match date in matchedDates)
+            {
+                Console.WriteLine($"Day: {date.Groups[1]}, Month: {date.Groups[3]}, Year: {date.Groups[4]}");
+            }
+        }
+    }
+}
