51a4d63c5da9f3f1e777f9a47f4078eb3beb2152
Petar Belchev <petar9004@gmail.com>
2022-07-15 15:40:07 +0300

    Added a Extract Emails task

---
+++ 02. Programing Fundamentals/11.2 Regular Expressions - Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Regex regex = new Regex(@"\s(?<user>[a-z0-9]+[.\-_]?[a-z0-9]+)@(?<host>[a-z]{2,}-?[a-z]{2,}\.[a-z]{2,}(?:\.[a-z]{2,})*)");
+
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
