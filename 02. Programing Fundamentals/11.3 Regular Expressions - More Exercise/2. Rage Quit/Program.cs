05f34c1f185bb8001c0c77cf49bd08ee69985963
Petar Belchev <petar9004@gmail.com>
2022-07-16 18:27:31 +0300

    Added a Rage Quit task

---
+++ 02. Programing Fundamentals/11.3 Regular Expressions - More Exercise/2. Rage Quit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2._Rage_Quit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(@"(\D+)(\d+)");
+
+            MatchCollection matches = regex.Matches(input);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                string str = match.Groups[1].Value;
+                int repeatCount = int.Parse(match.Groups[2].Value);
+
+                for (int i = 0; i < repeatCount; i++)
+                {
+                    sb.Append(str);
+                }
+            }
+
+            string output = sb.ToString().ToUpper();
+            int uniqueSymbolsCount = output.Distinct().Count();
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymbolsCount}");
+            Console.WriteLine(output);
+        }
+    }
+}
