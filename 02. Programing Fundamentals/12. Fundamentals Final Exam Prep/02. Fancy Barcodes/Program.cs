53a46b7125301e1946c057d8e04938da9b809984
Petar Belchev <petar9004@gmail.com>
2022-07-18 14:16:50 +0300

    Added a Fancy Barcodes task

---
+++ 02. Programing Fundamentals/12. Fundamentals Final Exam Prep/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfBarcodes = int.Parse(Console.ReadLine());
+
+            Regex regexForBarcodes = new Regex(@"(@#+)(?<barcode>[A-Z][A-Za-z0-9]{4,}[A-Z])(@#+)");
+            Regex regexForDigits = new Regex(@"\d+");
+
+            for (int i = 0; i < numOfBarcodes; i++)
+            {
+                string currBarcode = Console.ReadLine();
+
+                Match matchBarcode = regexForBarcodes.Match(currBarcode);
+
+                if (!matchBarcode.Success)
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+                }
+
+                string productGroup = string.Empty;
+
+                if (regexForDigits.IsMatch(matchBarcode.Groups["barcode"].Value))
+                {
+                    MatchCollection matchesDigits = regexForDigits.Matches(matchBarcode.Groups["barcode"].Value);
+
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach (Match match in matchesDigits)
+                    {
+                        sb.Append(match.Value);
+                    }
+
+                    productGroup = sb.ToString().TrimEnd();
+                }
+                else
+                {
+                    productGroup = "00";
+                }
+
+                Console.WriteLine($"Product group: {productGroup}");
+            }
+        }
+    }
+}
