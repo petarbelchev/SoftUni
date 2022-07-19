5ecec12f773e65c43bc7360a0bf9cd3cf7d15d7c
Petar Belchev <petar9004@gmail.com>
2022-07-19 12:11:08 +0300

    Added a Simple Calculator task

---
+++ 03. C# Advanced/01.1 Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            Array.Reverse(input);
+
+            Stack<string> stack = new Stack<string>(input);
+
+            int result = int.Parse(stack.Pop());
+
+            while (stack.Count > 0)
+            {
+                string @operator = stack.Pop();
+
+                if (@operator == "+")
+                {
+                    result += int.Parse(stack.Pop());
+                }
+                else
+                {
+                    result -= int.Parse(stack.Pop());
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
