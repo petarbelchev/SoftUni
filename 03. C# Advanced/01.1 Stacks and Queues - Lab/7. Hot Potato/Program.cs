8d065555a8f4b23dfde7ec8db7190588b5db88a9
Petar Belchev <petar9004@gmail.com>
2022-07-19 12:12:44 +0300

    Added a HotPotato task

---
+++ 03. C# Advanced/01.1 Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split(' ');
+
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>(kids);
+
+            int counter = 0;
+
+            while (queue.Count > 1)
+            {
+                counter++;
+                
+                string currKid = queue.Dequeue();
+
+                if (counter == n)
+                {
+                    Console.WriteLine($"Removed {currKid}");
+                    counter = 0;
+                }
+                else
+                {
+                    queue.Enqueue(currKid);
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Peek()}");
+        }
+    }
+}
