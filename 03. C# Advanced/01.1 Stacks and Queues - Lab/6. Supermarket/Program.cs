ff893f63d7aa5b56435cc6333b0b2ce9f1f9c637
Petar Belchev <petar9004@gmail.com>
2022-07-19 12:12:15 +0300

    Added a Supermarket task

---
+++ 03. C# Advanced/01.1 Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> costumers = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                if (input == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, costumers));
+                    costumers.Clear();
+                }
+                else
+                {
+                    costumers.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{costumers.Count} people remaining.");
+        }
+    }
+}
