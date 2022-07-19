eee3b24e9d5761179443b71be071f70bf9ce1965
Petar Belchev <petar9004@gmail.com>
2022-07-19 12:11:29 +0300

    Added a Matching Brackets task

---
+++ 03. C# Advanced/01.1 Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] expression = Console.ReadLine().ToCharArray();
+
+            Stack<int> stackOfIndexes = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stackOfIndexes.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    for (int j = stackOfIndexes.Pop(); j <= i; j++)
+                    {
+                        Console.Write(expression[j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
