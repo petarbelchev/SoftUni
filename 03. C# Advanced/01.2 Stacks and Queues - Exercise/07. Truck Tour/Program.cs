5750bc5e9ed8ca2b2b423c4635b7fffd1be9fca7
Petar Belchev <petar9004@gmail.com>
2022-07-20 11:17:22 +0300

    Added a Truck Tour task

---
+++ 03. C# Advanced/01.2 Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+
+            Queue<int> circleOfPumps = new Queue<int>();
+
+            for (int currPump = 0; currPump < numberOfPumps; currPump++)
+            {
+                int[] currPumpLittersAndDistance = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                int currPumpPetrolLeft = currPumpLittersAndDistance[0] - currPumpLittersAndDistance[1];
+
+                circleOfPumps.Enqueue(currPumpPetrolLeft);
+            }
+
+            int kilometersCounter = 0;
+
+            for (int currPumpIndex = 0; currPumpIndex < circleOfPumps.Count; currPumpIndex++)
+            {
+                Queue<int> testCircle = new Queue<int>(circleOfPumps);
+
+                while (testCircle.Count > 0)
+                {
+                    kilometersCounter += testCircle.Dequeue();
+
+                    if (kilometersCounter < 0)
+                    {
+                        int currPump = circleOfPumps.Dequeue();
+                        circleOfPumps.Enqueue(currPump);
+                        break;
+                    }
+                }
+
+                if (kilometersCounter >= 0)
+                {
+                    Console.WriteLine(currPumpIndex);
+                    break;
+                }
+
+                kilometersCounter = 0;
+            }
+        }
+    }
+}
