0a681c2e3ae2ce0eeace707096d5f57e871f1c40
Petar Belchev <petar9004@gmail.com>
2022-07-20 11:19:33 +0300

    Added a Cups and Bottles task

---
+++ 03. C# Advanced/01.2 Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cups = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> cupsQueue = new Queue<int>(cups);
+
+            int[] bottles = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> bottlesStack = new Stack<int>(bottles);
+
+            int wastedWater = 0;
+
+            while (cupsQueue.Count > 0 && bottlesStack.Count > 0)
+            {
+                int currBottle = bottlesStack.Pop();
+
+                if (currBottle >= cupsQueue.Peek())
+                {
+                    wastedWater += currBottle - cupsQueue.Dequeue();
+                }
+                else
+                {
+                    var newCupsQueue = cupsQueue.ToArray();
+                    newCupsQueue[0] -= currBottle;
+                    cupsQueue = new Queue<int>(newCupsQueue);
+                }
+            }
+
+            if (cupsQueue.Count > 0 && bottlesStack.Count == 0)
+            {
+                Console.Write($"Cups: ");
+                Console.WriteLine(string.Join(' ', cupsQueue));
+            }
+            else
+            {
+                Console.Write($"Bottles: ");
+                Console.WriteLine(string.Join(' ', bottlesStack));
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
