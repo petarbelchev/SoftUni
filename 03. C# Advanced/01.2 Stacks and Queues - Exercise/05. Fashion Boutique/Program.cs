729451371c0b52f328950d3aa8040f90d446792a
Petar Belchev <petar9004@gmail.com>
2022-07-20 11:16:39 +0300

    Added a Fashion Boutique task

---
+++ 03. C# Advanced/01.2 Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesOnTheBox = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int capacityOfSingleRack = int.Parse(Console.ReadLine());
+
+            if (capacityOfSingleRack == 0)
+            {
+                return;
+            }
+
+            Stack<int> stack = new Stack<int>(clothesOnTheBox);
+
+            int racksCounter = 1;
+            int currRackCapLeft = capacityOfSingleRack;
+
+            while (stack.Count > 0)
+            {
+                if (currRackCapLeft >= stack.Peek())
+                {
+                    currRackCapLeft -= stack.Pop();
+                }
+                else
+                {
+                    racksCounter++;
+                    currRackCapLeft = capacityOfSingleRack;
+                }
+            }
+
+            Console.WriteLine(racksCounter);
+        }
+    }
+}
