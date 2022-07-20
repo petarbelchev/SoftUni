8643235e5aeb010a4df1394b3bc196dff8a0f465
Petar Belchev <petar9004@gmail.com>
2022-07-20 11:18:20 +0300

    Added a Simple Text Editor task

---
+++ 03. C# Advanced/01.2 Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfOperations = int.Parse(Console.ReadLine());
+            Stack<string> stack = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < numOfOperations; i++)
+            {
+                string[] cmd = Console.ReadLine().Split(' ');
+                string mainCmd = cmd[0];
+                if (mainCmd == "1")
+                {
+                    string textToAppend = cmd[1];
+                    stack.Push(text.ToString());
+                    text.Append(textToAppend);
+                }
+                else if (mainCmd == "2")
+                {
+                    int countForRemove = int.Parse(cmd[1]);
+                    int startIndex = text.Length - countForRemove;
+                    stack.Push(text.ToString());
+                    text.Remove(startIndex, countForRemove);
+                }
+                else if (mainCmd == "3")
+                {
+                    int positionToShow = int.Parse(cmd[1]) - 1;
+                    Console.WriteLine(text[positionToShow]);
+                }
+                else if (mainCmd == "4")
+                {
+                    text.Clear();
+                    text.Append(stack.Pop());
+                }
+            }
+        }
+    }
+}
