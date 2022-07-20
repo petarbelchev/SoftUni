7455103c4dd889e9ce2c0df62e79c7a9b3feca99
Petar Belchev <petar9004@gmail.com>
2022-07-20 11:17:56 +0300

    Added a Balanced Parenthesis task

---
+++ 03. C# Advanced/01.2 Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();
+
+            if (sequence.Length == 1 || sequence.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in sequence)
+            {
+                if (ch == '(' || ch == '{' || ch == '[')
+                {
+                    stack.Push(ch);
+                }
+                else
+                {
+                    if (ch == ')' && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else if (ch == '}' && stack.Peek() == '{')
+                    {
+                        stack.Pop();
+                    }
+                    else if (ch == ']' && stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
