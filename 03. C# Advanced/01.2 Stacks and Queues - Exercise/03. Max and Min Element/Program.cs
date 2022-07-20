3e383e56775ff08a95169b19c4e55d80112c17ec
Petar Belchev <petar9004@gmail.com>
2022-07-20 11:15:44 +0300

    Added a Max and Min Element task

---
+++ 03. C# Advanced/01.2 Stacks and Queues - Exercise/03. Max and Min Element/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Max_and_Min_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int[] currQuery = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                if (currQuery[0] == 1)
+                {
+                    stack.Push(currQuery[1]);
+                }
+                else if (stack.Count > 0)
+                {
+                    if (currQuery[0] == 2)
+                    {
+                        stack.Pop();
+                    }
+                    else if (currQuery[0] == 3)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                    else if (currQuery[0] == 4)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+
+            Console.Write(string.Join(", ", stack));
+        }
+    }
+}
