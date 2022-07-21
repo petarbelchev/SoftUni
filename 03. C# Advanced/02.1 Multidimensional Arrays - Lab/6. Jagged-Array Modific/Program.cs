8dbf1fcc25bf6ae1da1fe5846a05ae87c11a7de6
Petar Belchev <petar9004@gmail.com>
2022-07-21 08:34:09 +0300

    Added a Jagged-Array Modification task

---
+++ 03. C# Advanced/02.1 Multidimensional Arrays - Lab/6. Jagged-Array Modific/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modific
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixRows = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[matrixRows][];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRowValues = Console.ReadLine()
+                    .Split().Select(int.Parse).ToArray();
+
+                matrix[row] = new int[currRowValues.Length];
+
+                for (int col = 0; col < currRowValues.Length; col++)
+                {
+                    matrix[row][col] = currRowValues[col];
+                }
+            }
+            string cmd = Console.ReadLine();
+
+            while (cmd != "END")
+            {
+                string[] cmdArgs = cmd.Split();
+
+                int row = int.Parse(cmdArgs[1]);
+                int col = int.Parse(cmdArgs[2]);
+                int value = int.Parse(cmdArgs[3]);
+
+                if (row >= 0 && row < matrix.GetLength(0) 
+                    && col >= 0 && col < matrix[row].Length)
+                {
+                    if (cmdArgs[0] == "Add")
+                    {
+                        matrix[row][col] += value;
+                    }
+                    else if (cmdArgs[0] == "Subtract")
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid coordinates");
+                }
+
+                cmd = Console.ReadLine();
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                Console.WriteLine(string.Join(' ', matrix[row]));
+            }
+        }
+    }
+}
