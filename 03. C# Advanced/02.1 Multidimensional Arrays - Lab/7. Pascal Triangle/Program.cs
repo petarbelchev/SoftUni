3e6b65e1cc49fe37a505447c48ee6ef55cb5b0a6
Petar Belchev <petar9004@gmail.com>
2022-07-21 08:34:32 +0300

    Added a Pascal Triangle task

---
+++ 03. C# Advanced/02.1 Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+
+            long[][] triangle = new long[rowsCount][];
+
+            triangle[0] = new long[1] { 1 };
+
+            for (int row = 1; row < triangle.GetLength(0); row++)
+            {
+                triangle[row] = new long[triangle[row - 1].Length + 1];
+                
+                triangle[row][0] = 1;
+                triangle[row][triangle[row].Length - 1] = 1;
+
+                for (int col = 1; col < triangle[row].Length - 1; col++)
+                {
+                    triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
+                }
+            }
+
+            for (int row = 0; row < triangle.GetLength(0); row++)
+            {
+                Console.WriteLine(string.Join(' ', triangle[row]));
+            }
+        }
+    }
+}
