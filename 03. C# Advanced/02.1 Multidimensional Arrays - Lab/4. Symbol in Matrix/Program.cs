7b66e0fbdf574a4ea04c3ad5969be322799c87da
Petar Belchev <petar9004@gmail.com>
2022-07-21 08:33:01 +0300

    Added a Symbol in Matrix task

---
+++ 03. C# Advanced/02.1 Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] currRowChars = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRowChars[col];
+                }
+            }
+            char specialSymbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == specialSymbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{specialSymbol} does not occur in the matrix");
+        }
+    }
+}
