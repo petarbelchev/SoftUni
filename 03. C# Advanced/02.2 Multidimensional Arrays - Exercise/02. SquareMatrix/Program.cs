77c9ead556e50ebba294a89bc3b8a5293eab5a80
Petar Belchev <petar9004@gmail.com>
2022-07-22 10:15:51 +0300

    Edited folders names

---
+++ 03. C# Advanced/02.2 Multidimensional Arrays - Exercise/02. SquareMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._SquareMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsCols = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            char[,] matrix = new char[rowsCols[0], rowsCols[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] currRowChars = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRowChars[col];
+                }
+            }
+
+            int squaresCounter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] 
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        squaresCounter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squaresCounter);
+        }
+    }
+}
---
+++ 03. C# Advanced/02.2 Multidimensional Arrays - Exercise/03. Maximal Sum/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Maximal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsCols = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[rowsCols[0], rowsCols[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRowValues = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRowValues[col];
+                }
+            }
+
+            int[,] bestMatrix = new int[3, 3];
+            int bestMatrixSum = 0;
+            int[,] testMatrix = new int[3, 3];
+            int testMatrixSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    for (int testRow = 0; testRow < testMatrix.GetLength(0); testRow++)
+                    {
+                        for (int testCol = 0; testCol < testMatrix.GetLength(1); testCol++)
+                        {
+                            testMatrix[testRow, testCol] = matrix[row + testRow, col + testCol];
+
+                            testMatrixSum += testMatrix[testRow, testCol];
+                        }
+                    }
+
+                    if (testMatrixSum > bestMatrixSum)
+                    {
+                        bestMatrixSum = testMatrixSum;
+
+                        for (int testMatrixRow = 0; testMatrixRow < testMatrix.GetLength(0); testMatrixRow++)
+                        {
+                            for (int testMatrixCol = 0; testMatrixCol < testMatrix.GetLength(1); testMatrixCol++)
+                            {
+                                bestMatrix[testMatrixRow, testMatrixCol] = testMatrix[testMatrixRow, testMatrixCol];
+                            }
+                        }
+                    }
+
+                    testMatrixSum = 0;
+                }
+            }
+
+            Console.WriteLine($"Sum = {bestMatrixSum}");
+
+            for (int row = 0; row < bestMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < bestMatrix.GetLength(1); col++)
+                {
+                    Console.Write(bestMatrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
