bcfe22821e27bf8b5a5e3a1473dd035920ad7960
Petar Belchev <petar9004@gmail.com>
2022-07-23 17:59:49 +0300

    Added a Cities by Continent and Country task

---
+++ 03. C# Advanced/03.1 Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._CitCoCo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfCities = int.Parse(Console.ReadLine());
+            var continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < countOfCities; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string continent = data[0];
+                string country = data[1];
+                string city = data[2];
+
+                if (continents.ContainsKey(continent))
+                {
+                    if (continents[continent].ContainsKey(country))
+                    {
+                        continents[continent][country].Add(city);
+                    }
+                    else
+                    {
+                        continents[continent][country] = new List<string>() { city };
+                    }
+                }
+                else
+                {
+                    continents[continent] = new Dictionary<string, List<string>>();
+                    continents[continent].Add(country, new List<string>() { city });
+                }
+            }
+
+            foreach (var (continent, country) in continents)
+            {
+                Console.WriteLine($"{continent}:");
+                foreach (var kvp in country)
+                {
+                    Console.WriteLine($"  {kvp.Key} -> " + string.Join(", ", kvp.Value));
+                }
+            }
+        }
+    }
+}
