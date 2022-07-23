1a476ac813b193effe4d4fdbe142885d8e5b2f54
Petar Belchev <petar9004@gmail.com>
2022-07-23 18:00:22 +0300

    Added a Record Unique Names task

---
+++ 03. C# Advanced/03.1 Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._ReUnNam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNames = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < countOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
