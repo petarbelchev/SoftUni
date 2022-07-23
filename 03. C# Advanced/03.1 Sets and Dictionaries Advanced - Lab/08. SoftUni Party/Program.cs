6980f53db2b81f2f9893fa39ac88b8cdf1ebfdea
Petar Belchev <petar9004@gmail.com>
2022-07-23 18:01:26 +0300

    Added a SoftUni Party task

---
+++ 03. C# Advanced/03.1 Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._SoUnPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regularReservations = new HashSet<string>();
+            HashSet<string> vipReservations = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (char.IsDigit(input[0]))
+                    vipReservations.Add(input);
+                else
+                    regularReservations.Add(input);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (char.IsDigit(input[0]))
+                    vipReservations.Remove(input);
+                else
+                    regularReservations.Remove(input);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(vipReservations.Count + regularReservations.Count);
+
+            if (vipReservations.Count > 0)
+                Console.WriteLine(string.Join(Environment.NewLine, vipReservations));
+
+            if (regularReservations.Count > 0)
+                Console.WriteLine(string.Join(Environment.NewLine, regularReservations));
+        }
+    }
+}
