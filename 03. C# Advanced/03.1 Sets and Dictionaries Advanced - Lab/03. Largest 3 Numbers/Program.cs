e392ff809fbab1704da11660a7a47babc5354df9
Petar Belchev <petar9004@gmail.com>
2022-07-23 17:58:07 +0300

    Added a Largest 3 Numbers task

---
+++ 03. C# Advanced/03.1 Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Lar3Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split().Select(int.Parse)
+                .OrderByDescending(n => n)
+                .Take(3).ToArray();
+
+            Console.WriteLine(string.Join(' ', nums));
+        }
+    }
+}
