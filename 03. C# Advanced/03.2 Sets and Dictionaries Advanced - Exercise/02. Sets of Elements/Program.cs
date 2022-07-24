96cc099b1b7b639b5c41c7d8916743ed01f9ea4c
Petar Belchev <petar9004@gmail.com>
2022-07-24 21:48:10 +0300

    Added a Sets of Elements task

---
+++ 03. C# Advanced/03.2 Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+            int[] length = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            for (int i = 0; i < length[0]; i++)
+                set1.Add(int.Parse(Console.ReadLine()));
+            for (int i = 0; i < length[1]; i++)
+                set2.Add(int.Parse(Console.ReadLine()));
+            foreach (var elem in set1)
+                if (set2.Contains(elem))
+                    Console.Write(elem + " ");
+        }
+    }
+}
