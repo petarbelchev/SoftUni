8ce76445c5ce6f346fff961bfbc030a719d5ee6f
Petar Belchev <petar9004@gmail.com>
2022-07-24 21:47:38 +0300

    Added a Unique Usernames task

---
+++ 03. C# Advanced/03.2 Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> usernames = new HashSet<string>();
+        int countOfUsernames = int.Parse(Console.ReadLine());
+        for (int i = 0; i < countOfUsernames; i++)
+        {
+            string name = Console.ReadLine();
+            usernames.Add(name);
+        }
+        Console.WriteLine(string.Join(Environment.NewLine, usernames));
+    }
+}
