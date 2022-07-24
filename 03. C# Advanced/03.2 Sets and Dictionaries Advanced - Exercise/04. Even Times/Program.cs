0190afaac16c95f0816e4848668851c6630df346
Petar Belchev <petar9004@gmail.com>
2022-07-24 21:49:05 +0300

    Added a Even Times task

---
+++ 03. C# Advanced/03.2 Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new Dictionary<string, int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string number = Console.ReadLine();
+
+                if (numbers.ContainsKey(number))
+                    numbers[number]++;
+                else
+                    numbers[number] = 1;
+            }
+            Console.WriteLine(numbers.Where(kvp => kvp.Value % 2 == 0).Select(key => key.Key).First());
+        }
+    }
+}
