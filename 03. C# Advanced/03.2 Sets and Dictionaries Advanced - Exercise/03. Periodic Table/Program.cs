888c6cf5ef7a00f91c26c9fb3fd9fcebc08b8304
Petar Belchev <petar9004@gmail.com>
2022-07-24 21:48:39 +0300

    Added a Periodic Table task

---
+++ 03. C# Advanced/03.2 Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> elements = new HashSet<string>();
+            int countOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfLines; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in line)
+                    elements.Add(item);
+            }
+            Console.WriteLine(string.Join(' ', elements.OrderBy(el => el)));
+        }
+    }
+}
