d280e7cd6c0d228f15b1121bc113c7befb2a5a36
Petar Belchev <petar9004@gmail.com>
2022-07-25 14:20:09 +0300

    Added a Word Count task

---
+++ 03. C# Advanced/04.1 Streams, Files and Directories - Lab/03. Word Count/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            string[] words;
+
+            using (var reader = new StreamReader(wordsFilePath))
+                words = reader.ReadToEnd().ToLower().Split();
+
+            string text;
+
+            using (var reader = new StreamReader(textFilePath))
+                text = reader.ReadToEnd().ToLower();
+
+            var wordsAndMatchesCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                var regex = new Regex(@$"\b{word}\b");
+                MatchCollection matches = regex.Matches(text);
+                wordsAndMatchesCount[word] = matches.Count();
+            }
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in wordsAndMatchesCount.OrderByDescending(x => x.Value))
+                    writer.WriteLine(word.Key + " - " + word.Value);
+            }
+        }
+    }
+}
+
