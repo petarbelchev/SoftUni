8048f72ec4165fab71a0c41453ec0d7d6d4f5b10
Petar Belchev <petar9004@gmail.com>
2022-07-25 14:18:23 +0300

    Added a Odd Lines task

---
+++ 03. C# Advanced/04.1 Streams, Files and Directories - Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var read = new StreamReader(inputFilePath);
+            var write = new StreamWriter(outputFilePath);
+            int row = 0;
+
+            using (read)
+            {
+                string line = read.ReadLine();
+
+                using (write)
+                {
+                    while (line != null)
+                    {
+                        if (row % 2 != 0)
+                        {
+                            write.WriteLine(line);
+                        }
+                        row++;
+
+                        line = read.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
