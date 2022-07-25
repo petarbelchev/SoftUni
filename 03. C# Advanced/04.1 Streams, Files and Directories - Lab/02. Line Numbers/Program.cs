0824582b1e5af925af62bacf1b975587a29c06d9
Petar Belchev <petar9004@gmail.com>
2022-07-25 14:19:21 +0300

    Added a Line Numbers task

---
+++ 03. C# Advanced/04.1 Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\input.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            var writer = new StreamWriter(outputFilePath);
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+
+                using (writer)
+                {
+                    int row = 1;
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{row++}. {line}");
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
+
