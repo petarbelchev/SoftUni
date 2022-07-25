b6f3df2885a9a0eef5e5f1234c240954db64e683
Petar Belchev <petar9004@gmail.com>
2022-07-25 14:23:43 +0300

    Added a Extract Special Bytes task

---
+++ 03. C# Advanced/04.1 Streams, Files and Directories - Lab/05. Extract Special Bytes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExtractBytes
+{
+    public class ExtractBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            List<byte> lookedBytes = new List<byte>();
+            using (StreamReader bytesReader = new StreamReader(bytesFilePath))
+            {
+                string line = bytesReader.ReadLine();
+
+                while (line != null)
+                {
+                    lookedBytes.Add(byte.Parse(line));
+
+                    line = bytesReader.ReadLine();
+                }
+            }
+
+            using (FileStream imageReader = new FileStream(binaryFilePath, FileMode.Open))
+            {
+                using (FileStream writer = new FileStream(outputPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+                    int countBuffer = imageReader.Read(buffer, 0, buffer.Length);
+
+                    while (countBuffer != 0)
+                    {
+                        byte[] mathesBytes = buffer.Where(b => lookedBytes.Contains(b)).ToArray();
+
+                        writer.Write(mathesBytes, 0, mathesBytes.Length);
+
+                        countBuffer = imageReader.Read(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
