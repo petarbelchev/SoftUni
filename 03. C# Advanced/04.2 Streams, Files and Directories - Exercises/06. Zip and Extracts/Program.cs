83f512324be0e7bd9735d4ff1eddbe04fa986df2
Petar Belchev <petar9004@gmail.com>
2022-07-26 09:53:26 +0300

    Added a Zip and Extracts task

---
+++ 03. C# Advanced/04.2 Streams, Files and Directories - Exercises/06. Zip and Extracts/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string zipArchiveFilePath = @"..\..\..\zipArchive.zip";
+            string outputFilePath = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFilePath, zipArchiveFilePath);
+
+            var fileName = Path.GetFileName(inputFilePath);
+            
+            ExtractFileFromArchive(zipArchiveFilePath, fileName, outputFilePath);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using (var archive = ZipFile.Open(zipArchiveFilePath,ZipArchiveMode.Create))
+            {
+                var fileName = Path.GetFileName(inputFilePath);
+                archive.CreateEntryFromFile(inputFilePath, fileName);
+            }
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            var archive = ZipFile.OpenRead(zipArchiveFilePath);
+            var fileForExtract = archive.GetEntry(fileName);
+            fileForExtract.ExtractToFile(outputFilePath);
+        }
+    }
+}
+
