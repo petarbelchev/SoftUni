4d80750e7fee8b56f733a74da8642a1b1eaebee2
Petar Belchev <petar9004@gmail.com>
2022-07-26 09:50:14 +0300

    Added a Line Numbers task

---
+++ 03. C# Advanced/04.2 Streams, Files and Directories - Exercises/02. Line Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int row = 1;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        char[] punctuations = new char[] { '.', ',', '!', '?', '-', '\'' };
+                        int countOfPunc = line.Where(ch => char.IsPunctuation(ch)).ToArray().Count();
+                        int countOfLetters = line.Where(ch => char.IsLetter(ch)).ToArray().Count();
+
+                        writer.WriteLine($"Line {row++}: {line} ({countOfLetters})({countOfPunc})");
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
