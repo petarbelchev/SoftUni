85f4961b7df0a196bdd4fbfae4640d9d52cccdb8
Petar Belchev <petar9004@gmail.com>
2022-07-26 09:49:33 +0300

    Added a Even Lines task

---
+++ 03. C# Advanced/04.2 Streams, Files and Directories - Exercises/01. Even Lines/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace EvenLines
+{
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+                int row = 0;
+
+                while (line != null)
+                {
+                    if (row % 2 == 0)
+                    {
+                        char[] chars = new char[] { '-', ',', '.', '!', '?' };
+
+                        foreach (char ch in chars)
+                        {
+                            line = line.Replace(ch, '@');
+                        }
+
+                        string[] reversedLine = line.Split();
+                        Array.Reverse(reversedLine);
+
+                        sb.AppendLine(string.Join(' ', reversedLine));
+
+                    }
+                    row++;
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
+
