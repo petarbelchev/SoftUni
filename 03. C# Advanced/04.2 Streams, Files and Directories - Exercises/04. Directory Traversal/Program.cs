7e92dce4065be0739f907b58e9d634f69c38a08c
Petar Belchev <petar9004@gmail.com>
2022-07-26 09:52:04 +0300

    Added a Directory Traversal task

---
+++ 03. C# Advanced/04.2 Streams, Files and Directories - Exercises/04. Directory Traversal/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DirectoryTraversal
+{
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(inputFolderPath);
+            FileInfo[] files = dir.GetFiles();
+
+            var sortedFiles = new Dictionary<string, Dictionary<string, long>>();
+
+            foreach (var file in files)
+            {
+                if (!sortedFiles.ContainsKey(file.Extension))
+                {
+                    sortedFiles[file.Extension] = new Dictionary<string, long>();
+                }
+
+                sortedFiles[file.Extension][file.Name] = file.Length;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var extension in sortedFiles
+                .OrderByDescending(x => x.Value.Count))
+            {
+                sb.AppendLine(extension.Key);
+                foreach (var file in extension.Value.OrderBy(x => x.Value))
+                {
+                    sb.AppendLine($"--{file.Key} - {file.Value / (double)1000:f3}kb");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string filePath = @"..\..\..\..\..\..\.." + reportFileName;
+
+            using (File.Create(filePath))
+            {
+
+            }
+
+            using (var writer = new StreamWriter(filePath))
+            {
+                writer.Write(textContent);
+            }
+        }
+    }
+}
+
