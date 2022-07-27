1b52871a96cea2170b1b09df5bad82317995a14e
Petar Belchev <petar9004@gmail.com>
2022-07-27 17:33:19 +0300

    Added a Sort Even Numbers task

---
+++ 03. C# Advanced/05.1 Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .Where(n => n % 2 == 0)
+            .OrderBy(n => n)
+            .ToArray();
+
+        Console.WriteLine(string.Join(", ", nums));
+    }
+}
