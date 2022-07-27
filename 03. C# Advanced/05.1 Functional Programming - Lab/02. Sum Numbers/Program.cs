7385d6e47d125640b7557bdc41b8b19783efc6c4
Petar Belchev <petar9004@gmail.com>
2022-07-27 17:33:46 +0300

    Added a Sum Numbers task

---
+++ 03. C# Advanced/05.1 Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Console.WriteLine(nums.Count());
+        Console.WriteLine(nums.Sum());
+    }
+}
