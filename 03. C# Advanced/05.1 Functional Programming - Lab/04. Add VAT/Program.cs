e928cba0ded5a0866b39bcea68709ad283d8deb3
Petar Belchev <petar9004@gmail.com>
2022-07-27 17:34:44 +0300

    Added a Add VAT task

---
+++ 03. C# Advanced/05.1 Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Func<double, double> addVAT = price => price += price * 0.2;
+
+        double[] prices = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .Select(price => addVAT(price))
+            .ToArray();
+
+        Action<double> PrintOnTheConsole = price => Console.WriteLine($"{price:f2}");
+
+        Array.ForEach(prices, PrintOnTheConsole);
+    }
+}
