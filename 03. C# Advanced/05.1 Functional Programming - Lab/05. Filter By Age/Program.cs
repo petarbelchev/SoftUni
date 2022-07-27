9fe44fd355c22c3ea606d9f9d8e651b313de17e5
Petar Belchev <petar9004@gmail.com>
2022-07-27 17:35:05 +0300

    Added a Filter By Age task

---
+++ 03. C# Advanced/05.1 Functional Programming - Lab/05. Filter By Age/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<Person> persons = ReadPersons();
+
+        FilterPersonsByAgeThreshold(ref persons);
+
+        Print(persons);
+    }
+
+    static void Print(List<Person> persons)
+    {
+        string[] formatOfPrint = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        
+        Action<Person> print = person => Console.WriteLine($"{person.Name} - {person.Age}");
+        
+        if (formatOfPrint.Length == 1)
+        {
+            if (formatOfPrint[0] == "name")
+                print = person => Console.WriteLine($"{person.Name}");
+
+            else if (formatOfPrint[0] == "age")
+                print = person => Console.WriteLine($"{person.Age}");
+        }
+
+        persons.ForEach(print);
+    }
+
+    static void FilterPersonsByAgeThreshold(ref List<Person> persons)
+    {
+        string conditionInput = Console.ReadLine();
+        int ageThreshold = int.Parse(Console.ReadLine());
+
+        Func<Person, bool> condition = person => person.Age >= ageThreshold;
+
+        if (conditionInput == "younger")
+            condition = person => person.Age < ageThreshold;
+
+        persons = persons.Where(condition).ToList();
+    }
+
+    static List<Person> ReadPersons()
+    {
+        List<Person> persons = new List<Person>();
+
+        int countOfPersons = int.Parse(Console.ReadLine());
+        for (int i = 0; i < countOfPersons; i++)
+        {
+            string[] personData = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            persons.Add(new Person(personData[0], int.Parse(personData[1])));
+        }
+
+        return persons;
+    }
+}
+
+class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
