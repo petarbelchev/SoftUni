addce30f35092e31366fbf60f2a2c14d6e10e1a3
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:56:27 +0300

    Added a Find Evens or Odds task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] range = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        string command = Console.ReadLine();
+
+        Predicate<int> isWantedNum = null;
+
+        if (command.Contains("even"))
+            isWantedNum = num => num % 2 == 0;
+        else if (command.Contains("odd"))
+            isWantedNum = num => num % 2 != 0;
+
+        int start = range[0];
+        int end = range[1];
+
+        List<int> wantedNums = new List<int>();
+
+        for (int i = start; i <= end; i++)
+        {
+            if (isWantedNum(i))
+                wantedNums.Add(i);
+        }
+
+        Console.WriteLine(string.Join(' ', wantedNums));
+    }
+}
