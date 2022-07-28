76945008f9450c1f261f4fc47517a0c4795e1cd7
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:54:35 +0300

    Added a Action Print task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Action<string> printFormat = dataForPrint => Console.WriteLine(dataForPrint);
+
+        Array.ForEach(names, printFormat);
+    }
+}
