fdc7d320b62661142ead8a45e174b9b86345c24d
Petar Belchev <petar9004@gmail.com>
2022-07-28 09:01:49 +0300

    Added a TriFunction task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int sumChars = int.Parse(Console.ReadLine());
+
+        string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        //Func<string, int, bool> checker = (name, neededSum)  =>
+        //{
+        //    int sumChars = 0;
+        //    foreach (char ch in name)
+        //    {
+        //        sumChars += ch;
+        //    }
+
+        //    if(sumChars >= neededSum)
+        //        return true;
+        //    else
+        //        return false;
+        //};
+
+        //Console.WriteLine(names.Where(name => checker(name, sumChars)).First());
+
+        Console.WriteLine(names.First(name => name.Select(ch => (int)ch).Sum() >= sumChars));
+    }
+}
