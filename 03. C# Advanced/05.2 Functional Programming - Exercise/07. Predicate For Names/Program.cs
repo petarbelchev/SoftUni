400cc10ed6b96f3f8cce7d8179d828055ea23a55
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:58:40 +0300

    Added a Predicate For Names task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(string.Join(Environment.NewLine, names.Where(name => name.Length <= length)));
+        }
+    }
+}
