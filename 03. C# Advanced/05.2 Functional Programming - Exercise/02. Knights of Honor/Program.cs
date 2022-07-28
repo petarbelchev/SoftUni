b61289f01152b9cb7ec51e71d48509fda1f40a6a
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:55:09 +0300

    Added a Knights of Honor task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Action<string> printFormat = dataForPrint => Console.WriteLine($"Sir {dataForPrint}");
+
+        Array.ForEach(names, printFormat);
+    }
+}
