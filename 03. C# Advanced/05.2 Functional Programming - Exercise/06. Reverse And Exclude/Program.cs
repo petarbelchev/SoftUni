54f145f4b742954f8e9386f338ec8442776ca648
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:57:48 +0300

    Added a Reverse And Exclude task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int n = int.Parse(Console.ReadLine());
+
+        Func<int, bool> isNotDivisible = num => num % n != 0;
+
+        Console.WriteLine(string.Join(' ', nums.Where(isNotDivisible).Reverse()));
+    }
+}
