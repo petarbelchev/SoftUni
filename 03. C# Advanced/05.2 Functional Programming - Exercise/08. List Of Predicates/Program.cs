326623097d98294cadc8fb82be20228dbdf21567
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:59:40 +0300

    Added a List of Predicates task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int endOfRange = int.Parse(Console.ReadLine());
+            List<int> nums = new List<int>();
+
+            for (int i = 1; i <= endOfRange; i++)
+                nums.Add(i);
+
+            int[] dividers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Predicate<int> isNotDivisible = num =>
+            {
+                foreach (int divider in dividers)
+                {
+                    if (num % divider != 0)
+                        return true;
+                }
+                return false;
+            };
+
+            Console.WriteLine(string.Join(' ', nums.Where(num => !isNotDivisible(num))));
+        }
+    }
+}
