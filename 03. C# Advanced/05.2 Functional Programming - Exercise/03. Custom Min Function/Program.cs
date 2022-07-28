fc97d8808a303eab545e97dc085290b44609c907
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:55:37 +0300

    Added a Custom Min Function task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine()
+            .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Func<int[], int> smallestNum = num => num.Min();
+
+        Console.WriteLine(smallestNum(nums));
+    }
+}
