8cf2dbd9085a74c65dbc9f12c9eff2d48a7ab848
Petar Belchev <petar9004@gmail.com>
2022-07-28 08:57:13 +0300

    Added a Applied Arithmetics task

---
+++ 03. C# Advanced/05.2 Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        string cmd;
+
+        while ((cmd = Console.ReadLine()) != "end")
+        {
+            if (cmd == "add")
+            {
+                DoArithmetic add = nums => nums.Select(num => num += 1).ToArray();
+                nums = add(nums);
+            }
+            else if (cmd == "multiply")
+            {
+                DoArithmetic multiply = nums => nums.Select(num => num *= 2).ToArray();
+                nums = multiply(nums);
+            }
+            else if (cmd == "subtract")
+            {
+                DoArithmetic subtract = nums => nums.Select(num => num -= 1).ToArray();
+                nums = subtract(nums);
+            }
+            else if (cmd == "print")
+            {
+                Console.WriteLine(string.Join(' ', nums));
+            }
+        }
+    }
+    private delegate int[] DoArithmetic(int[] nums);
+}
