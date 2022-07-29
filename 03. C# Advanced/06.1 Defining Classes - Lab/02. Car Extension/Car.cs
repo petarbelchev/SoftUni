c4b89d40cc55c4871240d46884d1ae926efee212
Petar Belchev <petar9004@gmail.com>
2022-07-29 15:44:40 +0300

    Added a Car Extension task

---
+++ 03. C# Advanced/06.1 Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,60 @@
+﻿namespace CarManufacturer
+{
+    using System;
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumpsion;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumpsion; }
+            set { fuelConsumpsion = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (fuelQuantity - (distance * fuelConsumpsion / 100) >= 0)
+            {
+                fuelQuantity -= distance * fuelConsumpsion / 100;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {make}\nModel: {model}\nYear: {year}\nFuel: {fuelQuantity}";
+        }
+    }
+}
---
+++ 03. C# Advanced/06.1 Defining Classes - Lab/02. Car Extension/StartUp .cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var myCar = new Car()
+            {
+                Make = "VW",
+                Model = "Passat",
+                Year = 1999,
+                FuelConsumption = 5,
+                FuelQuantity = 50
+            };
+
+            myCar.Drive(200);
+            Console.WriteLine(myCar.WhoAmI());
+        }
+    }
+}
