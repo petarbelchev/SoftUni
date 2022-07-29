5d0fa66b133aa0f23c5d891f6ed52c813a20f389
Petar Belchev <petar9004@gmail.com>
2022-07-29 15:45:35 +0300

    Added a Car Constructors task

---
+++ 03. C# Advanced/06.1 Defining Classes - Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumpsion;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumpsion) 
+            : this(make, model, year)
+        {
+            FuelConsumption = fuelConsumpsion;
+            FuelQuantity = fuelQuantity;
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumpsion; }
+            set { fuelConsumpsion = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (fuelQuantity - (distance * fuelConsumpsion / 100) >= 0)
+            {
+                fuelQuantity -= distance * fuelConsumpsion / 100;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {make}\nModel: {model}\nYear: {year}\nFuel: {fuelQuantity}";
+        }
+    }
+}
---
+++ 03. C# Advanced/06.1 Defining Classes - Lab/03. Car Constructors/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            var firstCar = new Car();
+            var secondCar = new Car(make, model, year);
+            var thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+            Console.WriteLine(firstCar.WhoAmI());
+            Console.WriteLine(secondCar.WhoAmI());
+            Console.WriteLine(thirdCar.WhoAmI());
+        }
+    }
+}
