5da2ebfddc970fdf95efff594beeaa23f1052426
Petar Belchev <petar9004@gmail.com>
2022-07-29 15:44:11 +0300

    Added a Car task

---
+++ 03. C# Advanced/06.1 Defining Classes - Lab/01. Car/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var newCar = new Car()
+            {
+                Make = "VW",
+                Model = "Passat",
+                Year = 1992
+            };
+
+            Console.WriteLine($"Make: {newCar.Make}\nModel: {newCar.Model}\nYear: {newCar.Year}");
+        }
+    }
+}
+
