8895a6eec3f0dc19fa06826115e77e316bd8f10c
Petar Belchev <petar9004@gmail.com>
2022-07-29 15:49:26 +0300

    Added a Creating Constructors task

---
+++ 03. C# Advanced/06.2 Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var firstPerson = new Person();
+            var secondPerson = new Person(15);
+            var thirdPerson = new Person("Petar", 32);
+        }
+    }
+}
