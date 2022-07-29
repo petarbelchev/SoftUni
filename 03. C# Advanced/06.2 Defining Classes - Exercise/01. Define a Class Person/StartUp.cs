0077c996050f4bbfc3f354f36af7a3f7b4e0ec12
Petar Belchev <petar9004@gmail.com>
2022-07-29 15:48:51 +0300

    Added a Define a Class Person task

---
+++ 03. C# Advanced/06.2 Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var firstPerson = new Person("Petar", 32);
+
+            var secondPerson = new Person()
+            {
+                Name = "Tsvetelina",
+                Age = 33
+            };
+        }
+    }
+}
