2204f6592fbfc137588f38d21c3c5daca7f1c1e2
Petar Belchev <petar9004@gmail.com>
2022-07-30 10:48:12 +0300

    Added a Opinion Poll task

---
+++ 03. C# Advanced/06.2 Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int numOfPersons = int.Parse(Console.ReadLine());
+
+            var persons = new List<Person>();
+
+            for (int i = 0; i < numOfPersons; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                persons.Add(new Person(name, age));
+            }
+
+            Console.WriteLine(
+                string.Join(Environment.NewLine, persons
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name)));
+        }
+    }
+}
