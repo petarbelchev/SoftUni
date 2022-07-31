f2eba895ec7acf11637297b0337383fce05a50d7
Petar Belchev <petar9004@gmail.com>
2022-07-31 16:21:26 +0300

    Added a Custom Stack task

---
+++ 03. C# Advanced/07. Implementing Linked List, List, Stack and Queue/02. Implement the CustomStack Class/MyStack.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _02._Implement_the_CustomStack_Class
+{
+    public class MyStack
+    {
+        private int[] items;
+        private const int initialCapacity = 4;
+
+        public MyStack()
+        {
+            items = new int[initialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        private void Resize()
+        {
+            var resizedStack = new int[items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                resizedStack[i] = items[i];
+            }
+
+            items = resizedStack;
+        }
+
+        public void Push(int item)
+        {
+            ShiftToRight();
+            items[0] = item;
+            Count++;
+        }
+
+        public void Pop()
+        {
+            ShiftToLeft();
+            Count--;
+        }
+
+        public int Peek()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException("Stack empty!");
+            else
+                return items[0];
+        }
+
+        private void ShiftToRight()
+        {
+            if (Count + 1 > items.Length)
+            {
+                Resize();
+            }
+
+            for (int i = Count; i > 0; i--)
+            {
+                items[i] = items[i - 1];
+            }
+        }
+
+        private void ShiftToLeft()
+        {
+            for (int i = 0; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            items[Count - 1] = default;
+
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+        }
+
+        private void Shrink()
+        {
+            var shrinkedStack = new int[items.Length / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                shrinkedStack[i] = items[i];
+            }
+
+            items = shrinkedStack;
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(items[i]);
+            }
+        }
+    }
+}
---
+++ 03. C# Advanced/07. Implementing Linked List, List, Stack and Queue/02. Implement the CustomStack Class/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Implement_the_CustomStack_Class
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var stack = new Stack<int>();
+
+            var myStack = new MyStack();
+
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+
+            myStack.Pop();
+
+            myStack.Peek();
+
+            myStack.ForEach(el => Console.WriteLine(el));
+        }
+    }
+}
