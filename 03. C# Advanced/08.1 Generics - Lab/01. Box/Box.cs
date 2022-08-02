3388e877a2a709a3318dab884faea8faf24b2a28
Petar Belchev <petar9004@gmail.com>
2022-08-02 12:46:17 +0300

    Added a Box task

---
+++ 03. C# Advanced/08.1 Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Stack<T> Elements { get; set; } = new Stack<T>();
+        public int Count { get { return Elements.Count; } }
+
+        public void Add(T element)
+        {
+            Elements.Push(element);
+        }
+
+        public T Remove()
+        {
+            return Elements.Pop();
+        }
+    }
+}
