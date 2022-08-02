ed17dcdbd4df44c25de574a4b6c2cb63296e9253
Petar Belchev <petar9004@gmail.com>
2022-08-02 12:47:35 +0300

    Added a Generic Scale task

---
+++ 03. C# Advanced/08.1 Generics - Lab/03. GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            EqualityScale<int> numbers = new EqualityScale<int>(5, 5);
+            
+            Console.WriteLine(numbers.AreEqual());
+        }
+    }
+}
