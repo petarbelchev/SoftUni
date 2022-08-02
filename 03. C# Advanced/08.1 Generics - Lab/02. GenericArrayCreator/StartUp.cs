46d5d2b75910c79af861720e7e5f730b1af3d907
Petar Belchev <petar9004@gmail.com>
2022-08-02 12:46:55 +0300

    Added a Generic Array Creator task

---
+++ 03. C# Advanced/08.1 Generics - Lab/02. GenericArrayCreator/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            string[] people = ArrayCreator.Create(5, "Petar");
+            int[] number = ArrayCreator.Create(10, 4);
+        }
+    }
+}
