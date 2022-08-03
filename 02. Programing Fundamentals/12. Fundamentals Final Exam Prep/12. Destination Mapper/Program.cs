9622525d53142b96c9928a6e1c6afb6d17ccfffb
Petar Belchev <petar9004@gmail.com>
2022-08-03 10:22:01 +0300

    Added a Destination Mapper task

---
+++ 02. Programing Fundamentals/12. Fundamentals Final Exam Prep/12. Destination Mapper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string places = Console.ReadLine();
+
+        var regex = new Regex(@"((=)|(\/))(?<place>[A-Z][A-Za-z]{2,})(\1)");
+
+        MatchCollection matches = regex.Matches(places);
+
+        string[] destinations = matches.Select(x => x.Groups["place"].Value).ToArray();
+        int travelPoints = destinations.Select(x => x.Length).Sum();
+
+        Console.WriteLine("Destinations: " + string.Join(", ", destinations));
+        Console.WriteLine($"Travel Points: {travelPoints}");
+    }
+}
