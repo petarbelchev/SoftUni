21ebe5380f977e929467b51b7f5067daa8f8910a
Petar Belchev <petar9004@gmail.com>
2022-08-04 11:37:03 +0300

    Added a Generic Box Of Integer task

---
+++ 03. C# Advanced/08.2 Generics - Exercise/GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var box = new Box<int>(int.Parse(Console.ReadLine()));
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
