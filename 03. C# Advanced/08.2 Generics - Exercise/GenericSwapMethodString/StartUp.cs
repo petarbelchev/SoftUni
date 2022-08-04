65ac29a258b12ba70cab9d96990eaa62bd717309
Petar Belchev <petar9004@gmail.com>
2022-08-04 11:37:42 +0300

    Added a Generic Swap Method String task

---
+++ 03. C# Advanced/08.2 Generics - Exercise/GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var list = new List<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                list.Add(Console.ReadLine());
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            Swap(list, indexes[0], indexes[1]);
+        }
+
+        static void Swap<T>(List<T> list, int firstIndex, int secondIndex)
+        {
+            var firstItem = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = firstItem;
+            foreach (var item in list)
+                Console.WriteLine($"{typeof(T)}: {item}");
+        }
+    }
+}
