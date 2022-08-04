bc59e783d181f50983fca91ec13437f7c2d1c495
Petar Belchev <petar9004@gmail.com>
2022-08-04 11:38:49 +0300

    Added a Tuple task

---
+++ 03. C# Advanced/08.2 Generics - Exercise/Tuple/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string name = firstInput[0] + ' ' + firstInput[1];
+            string address = firstInput[2];
+            var firstTuple = new Tuple<string, string>(name, address);
+            Console.WriteLine(firstTuple);
+
+            string[] secondInput = Console.ReadLine().Split();
+            name = secondInput[0];
+            int littersOfBeer = int.Parse(secondInput[1]);
+            var secondTuple = new Tuple<string, int>(name, littersOfBeer);
+            Console.WriteLine(secondTuple);
+
+            string[] thirdInput = Console.ReadLine().Split();
+            int integer = int.Parse(thirdInput[0]);
+            double @double = double.Parse(thirdInput[1]);
+            var thirdTuple = new Tuple<int, double>(integer, @double);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ 03. C# Advanced/08.2 Generics - Exercise/Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿namespace Tuple
+{
+    internal class Tuple<TFirst, TSecond>
+    {
+        private TFirst item1;
+        private TSecond item2;
+        
+        public Tuple(TFirst item1,TSecond item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public TFirst Item1 { get { return item1; } }
+
+        public TSecond Item2 { get { return item2; } }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
