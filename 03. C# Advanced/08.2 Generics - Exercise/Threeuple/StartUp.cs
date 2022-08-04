8df97973df86ab14d2cf8ada315e770ec91c0bce
Petar Belchev <petar9004@gmail.com>
2022-08-04 11:39:21 +0300

    Added a Threeuple task

---
+++ 03. C# Advanced/08.2 Generics - Exercise/Threeuple/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string name = firstInput[0] + ' ' + firstInput[1];
+            string address = firstInput[2];
+            var town = new StringBuilder();
+            for (int i = 3; i < firstInput.Length; i++)
+            {
+                town.Append(firstInput[i] + ' ');
+            }
+            var firstTuple = new Threeuple<string, string, string>(name, address, town.ToString().TrimEnd());
+            Console.WriteLine(firstTuple);
+
+            string[] secondInput = Console.ReadLine().Split();
+            name = secondInput[0];
+            int littersOfBeer = int.Parse(secondInput[1]);
+            bool drunkOrNot = secondInput[2] == "drunk";
+            var secondTuple = new Threeuple<string, int, bool>(name, littersOfBeer, drunkOrNot);
+            Console.WriteLine(secondTuple);
+
+            string[] thirdInput = Console.ReadLine().Split();
+            name = thirdInput[0];
+            double accBalance = double.Parse(thirdInput[1]);
+            string bankName = thirdInput[2];
+            var thirdTuple = new Threeuple<string, double, string>(name, accBalance, bankName);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ 03. C# Advanced/08.2 Generics - Exercise/Threeuple/Threeuple.cs
@@ -0,0 +1,23 @@
+﻿namespace Threeuple
+{
+    internal class Threeuple<TFirst, TSecond, TThird>
+    {
+        public Threeuple(TFirst item1, TSecond item2, TThird item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+
+        public TFirst Item1 { get; set; }
+
+        public TSecond Item2 { get; set; }
+
+        public TThird Item3 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2} -> {Item3}";
+        }
+    }
+}
