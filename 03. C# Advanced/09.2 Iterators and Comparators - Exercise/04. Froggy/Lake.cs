2a7848fbbe531b4603f055cde1819b6bf12b4e71
Petar Belchev <petar9004@gmail.com>
2022-08-05 12:04:02 +0300

    Added a Froggy task

---
+++ 03. C# Advanced/09.2 Iterators and Comparators - Exercise/04. Froggy/Lake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public Lake(int[] stones)
+        {
+            Stones = stones;
+        }
+        public int[] Stones { get; private set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Stones.Length; i += 2)
+            {
+                yield return Stones[i];
+            }
+            
+            for (int i = Stones.Length - 1; i >= 1; i--)
+            {
+                if (i % 2 != 0)
+                    yield return Stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 03. C# Advanced/09.2 Iterators and Comparators - Exercise/04. Froggy/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int[] stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
