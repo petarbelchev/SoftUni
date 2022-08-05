e21537575787c8a6480bf73e5c889e1554250c3c
Petar Belchev <petar9004@gmail.com>
2022-08-05 12:01:23 +0300

    Added a Listy Iterator task

---
+++ 03. C# Advanced/09.2 Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class ListyIterator <T>
+    {
+        private List<T> elements;
+        private int currentIndex;
+
+        public void Create(params T[] elements)
+        {
+            this.elements = elements.ToList();
+            this.currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                return ++this.currentIndex < elements.Count;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return currentIndex + 1 < elements.Count;
+        }
+
+        public void Print()
+        {
+            if (this.elements.Count > 0)
+            {
+                Console.WriteLine(this.elements[currentIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+    }
+}
---
+++ 03. C# Advanced/09.2 Iterators and Comparators - Exercise/01. ListyIterator/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            string[] firstCmd = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var elements = new List<string>();
+
+            if (firstCmd.Length > 1)
+            {
+                for (int i = 1; i < firstCmd.Length; i++)
+                {
+                    elements.Add(firstCmd[i]);
+                }
+            }
+
+            var listIterator = new ListyIterator<string>();
+            listIterator.Create(elements.ToArray());
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                if (cmd == "Move")
+                {
+                    Console.WriteLine(listIterator.Move());
+                }
+                else if (cmd == "Print")
+                {
+                    listIterator.Print();
+                }
+                else if (cmd == "HasNext")
+                {
+                    Console.WriteLine(listIterator.HasNext());
+                }
+            }
+        }
+    }
+}
