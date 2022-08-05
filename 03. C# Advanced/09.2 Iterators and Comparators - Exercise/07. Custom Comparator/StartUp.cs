0a45addbc4f9c04b8b460126c4b3e9f33ca86a79
Petar Belchev <petar9004@gmail.com>
2022-08-05 12:05:53 +0300

    Added a Custom Comparator task

---
+++ 03. C# Advanced/09.2 Iterators and Comparators - Exercise/07. Custom Comparator/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomComparator
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).OrderBy(n => n).ToArray();
+
+            Array.Sort(nums, new Comparator());
+
+            Console.WriteLine(string.Join(' ', nums));
+        }
+    }
+
+    public class Comparator : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+            else if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
