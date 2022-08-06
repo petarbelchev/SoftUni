5d09bd9eb933427578f58aaae63faffe93da6474
Petar Belchev <petar9004@gmail.com>
2022-08-06 14:13:21 +0300

    Added a Mirror Words task

---
+++ 02. Programing Fundamentals/12. Fundamentals Final Exam Prep/14. Mirror Words/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        var regex = new Regex(@"(@|#)(?<word1>[A-Za-z]{3,})\1\1(?<word2>[A-Za-z]{3,})\1");
+
+        MatchCollection matches = regex.Matches(text);
+
+        var mirrorWords = new List<string>();
+
+        foreach (Match match in matches)
+        {
+            string firstWord = match.Groups["word1"].Value;
+            char[] secondWordToChars = match.Groups["word2"].Value.ToCharArray();
+            Array.Reverse(secondWordToChars);
+            string secondWord = new string(secondWordToChars);
+            if (firstWord == secondWord)
+            {
+                mirrorWords.Add($"{firstWord} <=> {match.Groups["word2"].Value}");
+            }
+        }
+
+        if (matches.Count == 0)
+            Console.WriteLine("No word pairs found!");
+        else
+            Console.WriteLine($"{matches.Count} word pairs found!");
+
+        if (mirrorWords.Count == 0)
+            Console.WriteLine("No mirror words!");
+        else
+        {
+            Console.WriteLine($"The mirror words are:");
+            Console.WriteLine(string.Join(", ", mirrorWords));
+        }
+    }
+}
