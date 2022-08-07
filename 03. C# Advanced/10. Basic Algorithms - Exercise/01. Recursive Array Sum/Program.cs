8938db1c3c136fc8d5f4589abfddb48488841e1c
Petar Belchev <petar9004@gmail.com>
2022-08-07 19:15:56 +0300

    Added a Recursive Array Sum task

---
+++ 03. C# Advanced/10. Basic Algorithms - Exercise/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine(Sum(nums));
+    }
+
+    static int Sum(int[] array, int index = 0)
+    {
+        if (index == array.Length - 1)
+        {
+            return array[index];
+        }
+
+        return array[index] + Sum(array, index + 1);
+    }
+}
