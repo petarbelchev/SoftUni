a2e9c43eb8d4b61d29d67c78f2c2dc389712b556
Petar Belchev <petar9004@gmail.com>
2022-08-07 19:13:18 +0300

    Added a Boss Rush task

---
+++ 02. Programing Fundamentals/13. Final Exam Fundamentals/T02.BossRush/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        int numsOfLines = int.Parse(Console.ReadLine());
+
+        var regex = new Regex(@"\|(?<boss>[A-Z]{4,})\|:#(?<title>[A-Za-z]+\s[A-Za-z]+)#");
+
+        for (int i = 0; i < numsOfLines; i++)
+        {
+            string input = Console.ReadLine();
+
+            Match match = regex.Match(input);
+
+            if (match.Success)
+            {
+                Console.WriteLine($"{match.Groups["boss"].Value}, The {match.Groups["title"].Value}");
+                Console.WriteLine($">> Strength: {match.Groups["boss"].Value.Length}");
+                Console.WriteLine($">> Armor: {match.Groups["title"].Value.Length}");
+            }
+            else
+            {
+                Console.WriteLine("Access denied!");
+            }
+        }
+    }
+}
