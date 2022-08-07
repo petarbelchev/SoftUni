f9ac39f453d0bba7d1e169f80bc3819b5d28787f
Petar Belchev <petar9004@gmail.com>
2022-08-07 19:16:27 +0300

    Added a Recursive Factorial task

---
+++ 03. C# Advanced/10. Basic Algorithms - Exercise/02. Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int result = GetFactorial(n);
+
+        Console.WriteLine(result);
+    }
+
+    private static int GetFactorial(int n)
+    {
+        if (n == 1)
+        {
+            return n;
+        }
+
+        return n * GetFactorial(n - 1);
+    }
+}
