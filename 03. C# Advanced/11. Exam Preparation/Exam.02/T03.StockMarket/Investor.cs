278d2d04389bbe5a0f28340bb8e9723c20ff2650
Petar Belchev <petar9004@gmail.com>
2022-08-10 11:11:35 +0300

    Added a Stock Market task

---
+++ 03. C# Advanced/11. Exam Preparation/Exam.02/T03.StockMarket/Investor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            this.FullName = fullName;
+            this.EmailAddress = emailAddress;
+            this.MoneyToInvest = moneyToInvest;
+            this.BrokerName = brokerName;
+            this.Portfolio = new List<Stock>();
+        }
+        public List<Stock> Portfolio { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => this.Portfolio.Count;
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization <= 10_000 &&
+                this.MoneyToInvest < stock.MarketCapitalization)
+            {
+                return;
+            }
+
+            this.MoneyToInvest -= stock.MarketCapitalization;
+            this.Portfolio.Add(stock);
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            Stock currStock = this.Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+
+            if (currStock == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+
+            if (sellPrice < currStock.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+
+            this.MoneyToInvest += sellPrice;
+            this.Portfolio.Remove(currStock);
+
+            return $"{companyName} was sold.";
+        }
+
+        public Stock FindStock(string companyName)
+        {
+            return this.Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+        }
+
+        public Stock FindBiggestCompany()
+        {
+            return this.Portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+        }
+
+        public string InvestorInformation()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"The investor {this.FullName} with a broker {this.BrokerName} has stocks:");
+            foreach (var stock in this.Portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03. C# Advanced/11. Exam Preparation/Exam.02/T03.StockMarket/Stock.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            this.CompanyName = companyName;
+            this.Director = director;
+            this.PricePerShare = pricePerShare;
+            this.TotalNumberOfShares = totalNumberOfShares;
+            this.MarketCapitalization = this.PricePerShare * this.TotalNumberOfShares;
+        }
+        public string CompanyName { get; set; }
+        public string Director { get; set; }
+        public decimal PricePerShare { get; set; }
+        public int TotalNumberOfShares { get; set; }
+        public decimal MarketCapitalization { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Company: {this.CompanyName}");
+            sb.AppendLine($"Director: {this.Director}");
+            sb.AppendLine($"Price per share: ${this.PricePerShare}");
+            sb.AppendLine($"Market capitalization: ${this.MarketCapitalization}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
