2a63a97b8034f779fed33f0f746d0dc066e7abdf
Petar Belchev <petar9004@gmail.com>
2022-09-11 14:00:46 +0300

    Added a Generic Count Method Doubles task

---
+++ 03. C# Advanced/08.2 Generics - Exercise/GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericCountMethodDoubles
+{
+    public class Box<T> : IComparable<T> where T : IComparable<T>
+    {
+        public Box(T item)
+        {
+            Item = item;
+        }
+
+        public T Item { get; set; }
+
+        public int CompareTo(T other)
+        {
+            return Item.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Item}";
+        }
+    }
+}
---
+++ 03. C# Advanced/08.2 Generics - Exercise/GenericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDoubles
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new List<Box<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                box.Add(new Box<double>(double.Parse(Console.ReadLine())));
+            }
+            double doubleToCompare = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Compare(box, doubleToCompare));
+        }
+
+        private static int Compare(List<Box<double>> box, double doubleToCompare)
+        {
+            int counter = 0;
+            for (int i = 0; i < box.Count; i++)
+            {
+                if (box[i].CompareTo(doubleToCompare) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
