48b4544be144422dedf69898dacb60560b43ee4d
Petar Belchev <petar9004@gmail.com>
2022-09-11 14:00:19 +0300

    Added a Generic Count Method Strings task

---
+++ 03. C# Advanced/08.2 Generics - Exercise/Generic Count Method Strings/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodStrings
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new List<Box<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                box.Add(new Box<string>(Console.ReadLine()));
+            }
+            string strToCompare = Console.ReadLine();
+
+            Console.WriteLine(Compare(box, strToCompare));
+        }
+
+        private static int Compare(List<Box<string>> box, string strToCompare)
+        {
+            int counter = 0;
+            for (int i = 0; i < box.Count; i++)
+            {
+                if (box[i].CompareTo(strToCompare) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
