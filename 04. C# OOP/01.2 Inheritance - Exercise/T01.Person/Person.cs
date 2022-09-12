4b82100ecebb8678f98fb3426361a53570ac2cf8
Petar Belchev <petar9004@gmail.com>
2022-09-12 17:35:30 +0300

    Added a Person task

---
+++ 04. C# OOP/01.2 Inheritance - Exercise/T01.Person/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace T01.Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
---
+++ 04. C# OOP/01.2 Inheritance - Exercise/T01.Person/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T01.Person
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+        }
+    }
+}
