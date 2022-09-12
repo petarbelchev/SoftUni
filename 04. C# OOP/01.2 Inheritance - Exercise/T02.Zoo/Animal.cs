91d2182037633ec06d97da3a7e3f078945a279bc
Petar Belchev <petar9004@gmail.com>
2022-09-12 17:35:57 +0300

    Added a Zoo task

---
+++ 04. C# OOP/01.2 Inheritance - Exercise/T02.Zoo/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T02.Zoo
+{
+    public class Animal
+    {
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ 04. C# OOP/01.2 Inheritance - Exercise/T02.Zoo/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T02.Zoo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
