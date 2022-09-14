5f79c06d01be5397d7fbfdb217a58ff247f02932
Petar Belchev <petar9004@gmail.com>
2022-09-14 19:29:06 +0300

    Added a Food Shortage task

---
+++ 04. C# OOP/03.2 Interfaces And Abstraction - Exercise/FoodShortage/IBuyer.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodShortage
+{
+    public interface IBuyer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Food { get; set; }
+        public void BuyFood();
+    }
+}
---
+++ 04. C# OOP/03.2 Interfaces And Abstraction - Exercise/FoodShortage/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodShortage
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<IBuyer> people = new List<IBuyer>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (personData.Length == 4)
+                {
+                    var citizen = new Citizen(personData[0], int.Parse(personData[1]), personData[2], personData[3]);
+                    people.Add(citizen);
+                }
+                else if (personData.Length == 3)
+                {
+                    var rebel = new Rebel(personData[0], int.Parse(personData[1]), personData[2]);
+                    people.Add(rebel);
+                }
+            }
+
+            while (true)
+            {
+                string buyerOfFood = Console.ReadLine();
+
+                if (buyerOfFood == "End") break;
+
+                IBuyer person = people.Find(p => p.Name == buyerOfFood);
+                
+                if (person != null) person.BuyFood();
+            }
+
+            int totalAmountOfFood = 0;
+
+            people.ForEach(p => totalAmountOfFood += p.Food);
+
+            Console.WriteLine(totalAmountOfFood);
+        }
+    }
+}
