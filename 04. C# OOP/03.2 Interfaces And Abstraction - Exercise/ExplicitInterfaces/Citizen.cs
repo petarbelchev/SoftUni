8fd5229d48a495fd7cea66cd73098202704c8c1c
Petar Belchev <petar9004@gmail.com>
2022-09-15 19:55:20 +0300

    Added a Explicit Interfaces task

---
+++ 04. C# OOP/03.2 Interfaces And Abstraction - Exercise/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,32 @@
+﻿namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        private string name;
+        private int age;
+        private string country;
+
+        public Citizen(string name, string country, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.country = country;
+        }
+
+        public string Name => this.name;
+
+        public int Age => this.age;
+
+        public string Country => this.country;
+
+        string IPerson.GetName()
+        {
+            return this.Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
---
+++ 04. C# OOP/03.2 Interfaces And Abstraction - Exercise/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                
+                if (cmd == "End") break;
+
+                string[] citizenData = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var citizen = new Citizen(citizenData[0], citizenData[1], int.Parse(citizenData[2]));
+
+                IPerson person = citizen;
+                IResident resident = citizen;
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
