9923c145055dfc4eb97491c210f2aba704996723
Petar Belchev <petar9004@gmail.com>
2022-09-19 15:59:03 +0300

    Added a Sum of Integers task

---
+++ 04. C# OOP/05. Exceptions and Error Handling - Lab/T04.SumOfIntegers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace T04.SumOfIntegers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] inputs = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int intSum = 0;
+
+            foreach (var input in inputs)
+            {
+                try
+                {
+                    int num = int.Parse(input);
+                    intSum += num;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{input}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{input}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{input}' processed - current sum: {intSum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {intSum}");
+        }
+    }
+}
