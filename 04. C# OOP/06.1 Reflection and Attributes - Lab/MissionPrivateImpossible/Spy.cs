5f8fe44273ecb922222c4668eb81dd6a7b177ca8
Petar Belchev <petar9004@gmail.com>
2022-09-23 18:59:16 +0300

    Added a Mission Private Impossible task

---
+++ 04. C# OOP/06.1 Reflection and Attributes - Lab/MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string className)
+        {
+            var sb = new StringBuilder();
+
+            Type investigatedClass = Type.GetType(className);
+
+            sb.AppendLine($"All Private Methods of Class: {className}");
+            sb.AppendLine($"Base Class: {investigatedClass.BaseType.Name}");
+
+            MethodInfo[] methods = investigatedClass.GetMethods(
+                BindingFlags.Instance | 
+                BindingFlags.Static | 
+                BindingFlags.NonPublic);
+
+            foreach (MethodInfo method in methods)
+            {
+                sb.AppendLine($"{method.Name}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
