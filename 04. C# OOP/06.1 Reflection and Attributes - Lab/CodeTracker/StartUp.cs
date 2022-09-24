097046d6d974125bef809ac94388556683385da4
Petar Belchev <petar9004@gmail.com>
2022-09-24 13:24:27 +0300

    Added a Code Tracker task

---
+++ 04. C# OOP/06.1 Reflection and Attributes - Lab/CodeTracker/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Petar")]
+    public class StartUp
+    {
+        [Author("Stoyan")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ 04. C# OOP/06.1 Reflection and Attributes - Lab/CodeTracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            MethodInfo[] methods = Type.GetType("AuthorProblem.StartUp")
+                .GetMethods(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(m => m.CustomAttributes
+                .Any(a => a.AttributeType.Name == "AuthorAttribute"))
+                .ToArray();
+
+            foreach (MethodInfo method in methods)
+            {
+                var attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attr in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                }
+            }
+        }
+    }
+}
