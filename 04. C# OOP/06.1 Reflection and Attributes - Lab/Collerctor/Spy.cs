34452468d0dfea9c18881760c97bd04912e3e092
Petar Belchev <petar9004@gmail.com>
2022-09-24 13:23:36 +0300

    Added a Collector task

---
+++ 04. C# OOP/06.1 Reflection and Attributes - Lab/Collerctor/Spy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string nameOfInvestigatedClass)
+        {
+            var sb = new StringBuilder();
+
+            Type investigatedClass = Type.GetType(nameOfInvestigatedClass);
+
+            MethodInfo[] getMethods = investigatedClass.GetMethods(
+                BindingFlags.Instance | 
+                BindingFlags.Static | 
+                BindingFlags.Public | 
+                BindingFlags.NonPublic)
+                .Where(m => m.Name.StartsWith("get_"))
+                .ToArray();
+
+            MethodInfo[] setMethods = investigatedClass.GetMethods(
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.NonPublic)
+                .Where(m => m.Name.StartsWith("set_"))
+                .ToArray();
+
+            foreach (MethodInfo method in getMethods)
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in setMethods)
+            {
+                ParameterInfo[] parameters = method.GetParameters();
+
+                sb.AppendLine($"{method.Name} will set field of {parameters[0].ParameterType}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 04. C# OOP/06.1 Reflection and Attributes - Lab/Collerctor/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
