9969945ad75075fbcc65b8c1ffe5619971564500
Petar Belchev <petar9004@gmail.com>
2022-09-24 13:24:02 +0300

    Added a Create Attribute task

---
+++ 04. C# OOP/06.1 Reflection and Attributes - Lab/CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Petar")]
+    public class StartUp
+    {
+        [Author("Stoyan")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
