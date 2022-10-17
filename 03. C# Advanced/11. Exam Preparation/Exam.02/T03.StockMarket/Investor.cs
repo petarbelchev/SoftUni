f1eb1eecbcf8d7a849921fd3f3511aeb618c7a14
Petar Belchev <petar9004@gmail.com>
2022-10-17 12:18:45 +0300

    Update Investor.cs

--- 03. C# Advanced/11. Exam Preparation/Exam.02/T03.StockMarket/Investor.cs
+++ 03. C# Advanced/11. Exam Preparation/Exam.02/T03.StockMarket/Investor.cs
@@ -6,36 +6,35 @@ namespace StockMarket
 {
     public class Investor
     {
+        private List<Stock> portfolio;
         public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
         {
             this.FullName = fullName;
             this.EmailAddress = emailAddress;
             this.MoneyToInvest = moneyToInvest;
             this.BrokerName = brokerName;
-            this.Portfolio = new List<Stock>();
+            this.portfolio = new List<Stock>();
         }
-        public List<Stock> Portfolio { get; set; }
         public string FullName { get; set; }
         public string EmailAddress { get; set; }
         public decimal MoneyToInvest { get; set; }
         public string BrokerName { get; set; }
-        public int Count => this.Portfolio.Count;
+        public int Count => this.portfolio.Count;
 
         public void BuyStock(Stock stock)
         {
-            if (stock.MarketCapitalization <= 10_000 &&
-                this.MoneyToInvest < stock.MarketCapitalization)
+            if (stock.MarketCapitalization > 10_000 &&
+                this.MoneyToInvest >= stock.PricePerShare)
             {
-                return;
+                this.MoneyToInvest -= stock.PricePerShare;
+                this.portfolio.Add(stock);
             }
 
-            this.MoneyToInvest -= stock.MarketCapitalization;
-            this.Portfolio.Add(stock);
         }
 
         public string SellStock(string companyName, decimal sellPrice)
         {
-            Stock currStock = this.Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+            Stock currStock = this.portfolio.FirstOrDefault(s => s.CompanyName == companyName);
 
             if (currStock == null)
             {
@@ -48,26 +47,26 @@ namespace StockMarket
             }
 
             this.MoneyToInvest += sellPrice;
-            this.Portfolio.Remove(currStock);
+            this.portfolio.Remove(currStock);
 
             return $"{companyName} was sold.";
         }
 
         public Stock FindStock(string companyName)
         {
-            return this.Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+            return this.portfolio.FirstOrDefault(s => s.CompanyName == companyName);
         }
 
         public Stock FindBiggestCompany()
         {
-            return this.Portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+            return this.portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
         }
 
         public string InvestorInformation()
         {
             var sb = new StringBuilder();
             sb.AppendLine($"The investor {this.FullName} with a broker {this.BrokerName} has stocks:");
-            foreach (var stock in this.Portfolio)
+            foreach (var stock in this.portfolio)
             {
                 sb.AppendLine(stock.ToString());
             }
