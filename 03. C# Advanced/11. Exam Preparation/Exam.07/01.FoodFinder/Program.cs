5d0184562d15f7059f4391b5f86b0f2f50b223bf
Petar Belchev <petar9004@gmail.com>
2022-10-18 11:55:10 +0300

    Added a Food Finder task

---
+++ 03. C# Advanced/11. Exam Preparation/Exam.07/01.FoodFinder/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.FoodFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> vowels = new Queue<char>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToList());
+
+            Stack<char> consonants = new Stack<char>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToList());
+
+            var words = new Dictionary<string, HashSet<char>>()
+            {
+                { "pear", new HashSet<char>() },
+                { "flour", new HashSet<char>() },
+                { "pork", new HashSet<char>() },
+                { "olive", new HashSet<char>() },
+            };
+
+            while (consonants.Count > 0)
+            {
+                char currVowel = vowels.Dequeue();
+                char currConsonant = consonants.Pop();
+
+                foreach (var word in words)
+                {
+                    if (word.Key.Contains(currVowel))
+                    {
+                        word.Value.Add(currVowel);
+                    }
+
+                    if (word.Key.Contains(currConsonant))
+                    {
+                        word.Value.Add(currConsonant);
+                    }
+                }
+
+                vowels.Enqueue(currVowel);
+            }
+
+            string[] foundedWords = words
+                .Where(w => w.Key.Length == w.Value.Count)
+                .Select(w => w.Key)
+                .ToArray();
+
+            Console.WriteLine($"Words found: {foundedWords.Length}");
+
+            Console.WriteLine(string.Join(Environment.NewLine, foundedWords));
+        }
+    }
+}
