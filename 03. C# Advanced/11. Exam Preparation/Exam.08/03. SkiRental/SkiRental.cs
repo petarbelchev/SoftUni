ad9a17e02ebc04565068498c58619c0212e3c305
Petar Belchev <petar9004@gmail.com>
2022-10-19 11:00:54 +0300

    Added a Ski Rental task

---
+++ 03. C# Advanced/11. Exam Preparation/Exam.08/03. SkiRental/SkiRental.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        private List<Ski> data;
+
+        public SkiRental(string name, int capacity)
+        {
+            this.data = new List<Ski>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public void Add(Ski ski)
+        {
+            if (Capacity > data.Count)
+                data.Add(ski);
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            var ski = data.FirstOrDefault(s =>
+                    s.Manufacturer == manufacturer &&
+                    s.Model == model);
+
+            if (ski != null)
+            {
+                data.Remove(ski);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Ski GetNewestSki()
+         => data.OrderByDescending(s => s.Year).FirstOrDefault();
+
+        public Ski GetSki(string manufacturer, string model)
+         => data.FirstOrDefault(s => s.Manufacturer == manufacturer && s.Model == model);
+
+        public int Count => data.Count;
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {Name}:");
+            data.ForEach(s => sb.AppendLine(s.ToString()));
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
