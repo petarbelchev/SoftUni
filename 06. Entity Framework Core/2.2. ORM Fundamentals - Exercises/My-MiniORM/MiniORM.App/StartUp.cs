565c5b4cc8d326cd9e5c6f1262e3e9b65d0165bf
Petar Belchev <petar9004@gmail.com>
2022-12-23 12:55:39 +0200

    Added implementation of custom ORM

---
+++ 06. Entity Framework Core/2.2. ORM Fundamentals - Exercises/My-MiniORM/MiniORM.App/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=.;Database=MiniORM;Integrated Security=true;";
+
+            var context = new SoftUniDbContextClass(connectionString);
+
+            context.Employees.Add(new Employee()
+            {
+                FirstName = "Petar",
+                LastName = "Petrov",
+                DepartmentId = 1
+            });
+
+            context.SaveChanges();
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
