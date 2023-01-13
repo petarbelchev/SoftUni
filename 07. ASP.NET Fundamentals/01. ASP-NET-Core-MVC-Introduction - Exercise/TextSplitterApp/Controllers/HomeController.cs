86e5b82bf851b85b2b2b765303fcab5d71b01e1b
Petar Belchev <petar9004@gmail.com>
2023-01-13 14:39:11 +0200

    Added a TextSplitterApp

---
+++ 07. ASP.NET Fundamentals/01. ASP-NET-Core-MVC-Introduction - Exercise/TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitterApp.Models;
+
+namespace TextSplitterApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index(TextViewModel model)
+		{
+			return View(model);
+		}
+
+		public IActionResult Split(TextViewModel model)
+		{
+			string[] splitedText = model.Text
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+			model.SplitText = string.Join(Environment.NewLine, splitedText);
+
+			return RedirectToAction("Index", model);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
