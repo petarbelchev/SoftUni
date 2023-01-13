7b1a954998f811f38752da3b74d228611e17bed4
Petar Belchev <petar9004@gmail.com>
2023-01-13 14:10:12 +0200

    Added a ChatApp

---
+++ 07. ASP.NET Fundamentals/01. ASP-NET-Core-MVC-Introduction - Exercise/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,41 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+	public class ChatController : Controller
+	{
+		private static ICollection<KeyValuePair<string, string>> Messages =
+			new List<KeyValuePair<string, string>>();
+
+		public IActionResult Show()
+		{
+			var chatHistory = new ChatViewModel();
+
+			if (!Messages.Any())
+			{
+				return View(chatHistory);
+			}
+
+			chatHistory.Messages = Messages
+				.Select(m => new MessageViewModel
+				{
+					Sender = m.Key,
+					MessageText = m.Value
+				})
+				.ToList();
+
+			return View(chatHistory);
+		}
+
+		[HttpPost]
+		public IActionResult Send(ChatViewModel model)
+		{
+			Messages.Add(new KeyValuePair<string, string>(
+				model.CurrentMessage.Sender,
+				model.CurrentMessage.MessageText));
+
+			return RedirectToAction("Show");
+		}
+	}
+}
